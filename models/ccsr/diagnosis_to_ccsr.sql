b84d114e690e91f719c7438b0d1becfe6a8245f6
aneiderhiser <anneiderhiser@gmail.com>
2021-11-20 12:38:57 -0700

    added ccsr seed tables, logic, and documentation

---
+++ models/ccsr/diagnosis_to_ccsr.sql
@@ -0,0 +1,23 @@
+{{ config(materialized='view') }}
+
+select 
+    a.encounter_id
+,   case
+        when a.encounter_type = 'inpatient' then c.default_ccsr_inpatient
+        else null
+    end default_ccsr_inpatient
+,   case
+        when a.encounter_type = 'outpatient' then c.default_ccsr_outpatient
+        else null
+    end default_ccsr_outpatient
+,   c.ccsr_1
+,   c.ccsr_2
+,   c.ccsr_3
+,   c.ccsr_4
+,   c.ccsr_5
+,   c.ccsr_6
+from {{ ref('stg_encounters') }} a
+left join {{ ref('stg_diagnoses') }} b
+    on a.encounter_id = b.encounter_id
+left join {{ ref('ccsr_dx_mapping') }} c
+    on b.diagnosis_code = c.diagnosis_code
