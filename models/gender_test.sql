6cbae9985eabac67491002d21eb9317834899820
aneiderhiser <anneiderhiser@gmail.com>
2022-01-17 21:10:00 -0700

    updated data quality tests

---
+++ models/gender_test.sql
@@ -0,0 +1,11 @@
+{{ config(materialized='view') }}
+
+select 
+    patient_id
+,   'invalid_gender_code' as test_name
+,   1 as test_flag
+from {{ ref('stg_eligibility') }} a
+left join {{ ref('gender_codes') }} b
+    on a.gender_code = b.code
+where a.gender_code is not null
+    and b.code is null
