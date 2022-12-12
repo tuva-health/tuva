5d0a6ee489930f6778a4695db8244cb736516233
Forrest Beckwith <100245109+tuvaforrest@users.noreply.github.com>
2022-12-12 13:57:11 -0500

    Modularization (#25)
    * typo adjustment, integration tests
    
    * update readme
    
    updating readme to clarify sections.  general instructions have not changed
    
    * updated generate_schema_name.sql
    
    * update readme
    
    * update readme
    
    adding a few tweaks by Aaron
    
    * updated generate_schema_name.sql
    added integration tests
    added docs generate
    
    * cleaned integration tests dbt_project.yml
    
    * Delete integration_tests/saved directory
    
    * cleaned integration tests dbt_project.yml, removed generate schema name
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    Co-authored-by: thutuva <101137626+thutuva@users.noreply.github.com>
    Co-authored-by: Aaron Neiderhiser <anneiderhiser@gmail.com>

---
+++ docs/docs_generate/models/pharmacy_claim.sql
@@ -0,0 +1,17 @@
+select
+    claim_id
+    , claim_line_number
+    , patient_id
+    , member_id
+    , prescribing_provider_npi
+    , dispensing_provider_npi
+    , dispensing_date
+    , ndc_code
+    , quantity
+    , days_supply
+    , refills
+    , paid_date
+    , paid_amount
+    , allowed_amount
+    , data_source
+from tuva.claims_input.pharmacy_claim
