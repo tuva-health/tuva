4c97d44446efe2e4232c5bbe6a628343d522f0cf
thuxuan@tuvahealth.com <thuxuan@tuvahealth.com>
2023-04-12 16:15:30 +0000

    added DP macros

---
+++ macros/pharmacy_claim_missing_column_check.sql
@@ -0,0 +1,32 @@
+{#
+    Required variable input: relation and column_list
+
+
+#}
+
+
+{% macro pharmacy_claim_missing_column_check(relation, column_list) %}
+
+
+    {%- set all_columns = adapter.get_columns_in_relation(
+        relation
+    ) -%}
+
+    {%- for column_item in all_columns
+        if column_item.name.lower() in column_list %}
+
+        select
+            claim_id
+            , '{{ column_item.name|lower }}' as column_checked
+        from {{ relation }}
+        where {{ column_item.name }} is null
+
+
+
+        {% if not loop.last -%}
+            union all
+        {%- endif -%}
+
+        {%- endfor -%}
+
+{% endmacro %}
