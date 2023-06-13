7a0e5dcb834c1f7402dd5e9196110746f4f1db64
thuxuan@tuvahealth.com <thuxuan@tuvahealth.com>
2023-06-13 20:18:35 +0000

    save spot

--- models/pmpm/intermediate/pmpm__patient_spend_with_service_categories.sql
+++ models/pmpm/intermediate/pmpm__patient_spend_with_service_categories.sql
@@ -67,5 +67,6 @@ select
 , service_category_2
 , sum(paid_amount) as total_paid
 , sum(allowed_amount) as total_allowed
+, '{{ run_started_at.astimezone(modules.pytz.timezone("utc")) }}' as last_update
 from combine_medical_and_rx
 group by 1,2,3,4
