760720a9641a7b7d192bc0cd24d34ba8ab73f98f
sarah-tuva <sarah@tuvahealth.com>
2023-08-15 17:11:54 -0600

    Add logic to use seeds or source

--- integration_tests/models/medical_claim.sql
+++ integration_tests/models/medical_claim.sql
@@ -1,2 +1,11 @@
-select *
-from {{source('claims_input','medical_claim')}}
+{# logic to use the seed data or source input data -#}
+
+{% if var('test_data_override') == true -%}
+
+select * from {{ ref('medical_claim_seed') }}
+
+{%- else -%}
+
+select * from {{ source('claims_input', 'medical_claim') }}
+
+{%- endif %}
--- integration_tests/models/pharmacy_claim.sql
+++ integration_tests/models/pharmacy_claim.sql
@@ -1,2 +1,11 @@
-select *
-from {{source('claims_input','pharmacy_claim')}}
+{# logic to use the seed data or source input data -#}
+
+{% if var('test_data_override') == true -%}
+
+select * from {{ ref('pharmacy_claim_seed') }}
+
+{%- else -%}
+
+select * from {{ source('claims_input', 'pharmacy_claim') }}
+
+{%- endif %}
