63ee0cdfe372ffa027ae3b49182c16ddca63fc1a
Sarah Morgan <sarah@tuvahealth.com>
2023-08-18 11:29:12 -0600

    Add demo synthetic dataset as part of integration_tests in core package (#179)
    * Add synthetic data seeds
    * Add logic to use seeds or source
    * Consolidate load_seed macros and add arguments for compression, headers and null markers
    * Update documentation for using the new test_data_override variable
    * Remove unnecessary tests from ccsr mart
    * Bump package version and update dbt docs

--- macros/load_seed.sql
+++ macros/load_seed.sql
@@ -1,17 +1,33 @@
-{% macro load_seed(uri,pattern) %}
-{{ return(adapter.dispatch('load_seed')(uri,pattern)) }}
+{#
+    This macro includes options for compression, headers, and null markers.
+    Default options are set to FALSE. When set to TRUE, the appropriate
+    adapter-specific syntax will be used.
+
+    Argument examples:
+    compression=false
+    compression=true
+    headers=false
+    headers=true
+    null_marker=false
+    null_marker=true
+#}
+
+{% macro load_seed(uri,pattern,compression=false,headers=false,null_marker=false) %}
+{{ return(adapter.dispatch('load_seed', 'the_tuva_project')(uri,pattern,compression,headers,null_marker)) }}
 {% endmacro %}
 
 
 
-{% macro redshift__load_seed(uri,pattern) %}
+{% macro redshift__load_seed(uri,pattern,compression,headers,null_marker) %}
 {% set sql %}
 copy  {{ this }}
   from 's3://{{ uri }}/{{ pattern }}'
   access_key_id 'AKIA2EPVNTV4FLAEBFGE'
   secret_access_key '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'
   csv
-  gzip
+  {% if compression == true %} gzip {% else %} {% endif %}
+  {% if headers == true %} ignoreheader 1 {% else %} {% endif %}
+  emptyasnull
   region 'us-east-1'
 
 {% endset %}
@@ -31,12 +47,14 @@ copy  {{ this }}
 
 
 
-{% macro snowflake__load_seed(uri,pattern) %}
+{% macro snowflake__load_seed(uri,pattern,compression,headers,null_marker) %}
 {% set sql %}    
 copy into {{ this }}
     from s3://{{ uri }}
     file_format = (type = CSV
-    compression = 'GZIP'
+    {% if compression == true %} compression = 'GZIP' {% else %} compression = 'none' {% endif %}
+    {% if headers == true %} skip_header = 1 {% else %} {% endif %}
+    empty_field_as_null = true
     field_optionally_enclosed_by = '"'
 )
 pattern = '.*\/{{pattern}}.*';
@@ -57,7 +75,7 @@ pattern = '.*\/{{pattern}}.*';
 
 
 
-{% macro bigquery__load_seed(uri,pattern) %}
+{% macro bigquery__load_seed(uri,pattern,compression,headers,null_marker) %}
 {%- set columns = adapter.get_columns_in_relation(this) -%}
 {%- set collist = [] -%}
 
@@ -71,9 +89,10 @@ pattern = '.*\/{{pattern}}.*';
 load data into {{ this }} ( {{collist|join(',')}} )
 from files (format = 'csv',
     uris = ['gs://{{ uri }}/{{ pattern }}*'],
-    compression = 'GZIP',
-    quote = '"',
-    null_marker = '\\N'
+    {% if compression == true %} compression = 'GZIP', {% else %} {% endif %}
+    {% if headers == true %} skip_leading_rows = 1, {% else %} {% endif %}
+    {% if null_marker == true %} null_marker = '\\N', {% else %} {% endif %}
+    quote = '"'
     )
 {% endset %}
 
@@ -90,7 +109,7 @@ from files (format = 'csv',
 
 {% endmacro %}
 
-{% macro default__load_seed(uri,pattern) %}
+{% macro default__load_seed(uri,pattern,compression,headers,null_marker) %}
 {% if execute %}
 {% do log('No adapter found, seed not loaded',info = True) %}
 {% endif %}
