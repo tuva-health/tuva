a1bc4cb5eb0b7da0ca64df0675bd2ea764f13ebc
thutuva <thuxuan@tuvahealth.com>
2023-08-28 17:42:36 +0000

    edited enabled config for hcc snapshots

--- snapshots/cms_hcc/cms_hcc__patient_risk_scores_snapshot.sql
+++ snapshots/cms_hcc/cms_hcc__patient_risk_scores_snapshot.sql
@@ -12,7 +12,7 @@
     , "strategy": "timestamp"
     , "updated_at": "tuva_last_run"
     , "unique_key": "patient_id||model_version||payment_year||tuva_last_run"
-    , "enabled": var('cms_hcc_enabled',var('tuva_marts_enabled',True))
+    , "enabled": var('cms_hcc_enabled',var('claims_enabled',var('tuva_marts_enabled',False)))
   })
 }}
 
