d2a397fcea0803b0d0e13f15e375ff283c010b2a
thutuva <thuxuan@tuvahealth.com>
2023-08-29 12:53:11 +0000

    filled in integration test models

---
+++ integration_tests/models/encounter.sql
@@ -0,0 +1,27 @@
+select 
+null as encounter_id
+, null as patient_id
+, null as encounter_type
+, null as encounter_start_date
+, null as encounter_end_date
+, null as length_of_stay
+, null as admit_source_code
+, null as admit_source_description
+, null as admit_type_code
+, null as admit_type_description
+, null as discharge_disposition_code
+, null as discharge_disposition_description
+, null as attending_provider_id
+, null as facility_npi
+, null as primary_diagnosis_code
+, null as primary_diagnosis_description
+, null as ms_drg_code
+, null as ms_drg_description
+, null as apr_drg_code
+, null as apr_drg_description
+, null as paid_amount
+, null as allowed_amount
+, null as charge_amount
+, null as data_source
+, null as tuva_last_run
+where 1=0
