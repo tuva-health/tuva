1e14f6a17eceff76d44ef87c4cd8dab87fd61484
Forrest Beckwith <100245109+tuvaforrest@users.noreply.github.com>
2023-09-05 19:35:03 -0400

    Docs update (#200)
    * updated files to fix docs issue
    
    * updated files for docs issues, updated logic for enabled vars
    
    * updated catalog+manifest for testing
    
    * updated column descriptions for missing columns for documentation. fixed typo in stage claims patient
    
    * updated enabled logic
    
    * updated enabled logic
    
    * added missing column details for core models
    
    * more column description updates

---
+++ integration_tests/docs_generate/models/observation.sql
@@ -0,0 +1,23 @@
+select
+null as observation_id
+, null as patient_id
+, null as encounter_id
+, null as panel_id
+, null as observation_date
+, null as observation_type
+, null as source_code_type
+, null as source_code
+, null as source_description
+, null as normalized_code_type
+, null as normalized_code
+, null as normalized_description
+, null as result
+, null as source_units
+, null as normalized_units
+, null as source_reference_range_low
+, null as source_reference_range_high
+, null as normalized_reference_range_low
+, null as normalized_reference_range_high
+, null as data_source
+, null as tuva_last_run
+limit 0
