76a291a4d809588ffb8a84f928a2e0a95479049e
tuvaforrest <forrest@tuvahealth.com>
2023-09-05 14:51:14 -0400

    updated files for docs issues, updated logic for enabled vars

---
+++ integration_tests/docs_generate/models/procedure.sql
@@ -0,0 +1,21 @@
+select
+ null as procedure_id
+, null as patient_id
+, null as encounter_id
+, null as claim_id
+, null as procedure_date
+, null as source_code_type
+, null as source_code
+, null as source_description
+, null as normalized_code_type
+, null as normalized_code
+, null as normalized_description
+, null as modifier_1
+, null as modifier_2
+, null as modifier_3
+, null as modifier_4
+, null as modifier_5
+, null as practitioner_id
+, null as data_source
+, null as tuva_last_run
+limit 0
--- models/core/final/core__condition.sql
+++ models/core/final/core__condition.sql
@@ -4,17 +4,17 @@
 }}
 
 
-{% if var('clinical_enabled', false) == true and var('claims_enabled', false) == true -%}
+{% if var('clinical_enabled', var('tuva_marts_enabled',False)) == true and var('claims_enabled', var('tuva_marts_enabled',False)) == true -%}
 
 select * from {{ ref('core__stg_claims_condition') }}
 union all
 select * from {{ ref('core__stg_clinical_condition') }}
 
-{% elif var('clinical_enabled', false) == true -%}
+{% elif var('clinical_enabled', var('tuva_marts_enabled',False)) == true -%}
 
 select * from {{ ref('core__stg_clinical_condition') }}
 
-{% elif var('claims_enabled', false) == true -%}
+{% elif var('claims_enabled', var('tuva_marts_enabled',False)) == true -%}
 
 select * from {{ ref('core__stg_claims_condition') }}
 
