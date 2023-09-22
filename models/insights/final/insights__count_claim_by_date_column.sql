72ba9d2787e38c89a73ed93928bec7289a6ce088
thutuva <thuxuan@tuvahealth.com>
2023-09-22 13:20:52 +0000

    added pivot to date analysis

--- models/insights/final/insights__count_claim_by_date_column.sql
+++ models/insights/final/insights__count_claim_by_date_column.sql
@@ -1,78 +1,100 @@
-select
-    'claim_start_date' as date_field
-    , cast(year(claim_start_date) as varchar) || right('0'||cast(month(claim_start_date) as varchar),2) as year_month
-    , count(distinct claim_id)
-from {{ ref('core__medical_claim') }}
-group by 
-    year(claim_start_date)
-    , month(claim_start_date)
+with date_stage as(
 
-union all
+    select
+        'claim_start_date' as date_field
+        , cast(year(claim_start_date) as varchar) || right('0'||cast(month(claim_start_date) as varchar),2) as year_month
+        , count(distinct claim_id) as distinct_count
+    from {{ ref('core__medical_claim') }}
+    group by 
+        year(claim_start_date)
+        , month(claim_start_date)
 
-select
-    'claim_end_date' as date_field
-    , cast(year(claim_end_date) as varchar) || right('0'||cast(month(claim_end_date) as varchar),2) as year_month
-    , count(distinct claim_id)
-from {{ ref('core__medical_claim') }}
-group by 
-    year(claim_end_date)
-    , month(claim_end_date)
+    union all
 
-union all
+    select
+        'claim_end_date' as date_field
+        , cast(year(claim_end_date) as varchar) || right('0'||cast(month(claim_end_date) as varchar),2) as year_month
+        , count(distinct claim_id) as distinct_count
+    from {{ ref('core__medical_claim') }}
+    group by 
+        year(claim_end_date)
+        , month(claim_end_date)
 
-select
-    'admission_date' as date_field
-    , cast(year(admission_date) as varchar) || right('0'||cast(month(admission_date) as varchar),2) as year_month
-    , count(distinct claim_id)
-from {{ ref('core__medical_claim') }}
-group by 
-    year(admission_date)
-    , month(admission_date)
+    union all
 
-union all
+    select
+        'admission_date' as date_field
+        , cast(year(admission_date) as varchar) || right('0'||cast(month(admission_date) as varchar),2) as year_month
+        , count(distinct claim_id) as distinct_count
+    from {{ ref('core__medical_claim') }}
+    group by 
+        year(admission_date)
+        , month(admission_date)
 
-select
-    'discharge_date' as date_field
-    , cast(year(discharge_date) as varchar) || right('0'||cast(month(discharge_date) as varchar),2) as year_month
-    , count(distinct claim_id)
-from {{ ref('core__medical_claim') }}
-group by 
-    year(discharge_date)
-    , month(discharge_date)
+    union all
 
-union all
+    select
+        'discharge_date' as date_field
+        , cast(year(discharge_date) as varchar) || right('0'||cast(month(discharge_date) as varchar),2) as year_month
+        , count(distinct claim_id) as distinct_count
+    from {{ ref('core__medical_claim') }}
+    group by 
+        year(discharge_date)
+        , month(discharge_date)
 
-select
-    'medical paid_date' as date_field
-    , cast(year(paid_date) as varchar) || right('0'||cast(month(paid_date) as varchar),2) as year_month
-    , count(distinct claim_id)
-from {{ ref('core__medical_claim') }}
-group by 
-    year(paid_date)
-    , month(paid_date)
+    union all
 
-union all
+    select
+        'medical paid_date' as date_field
+        , cast(year(paid_date) as varchar) || right('0'||cast(month(paid_date) as varchar),2) as year_month
+        , count(distinct claim_id) as distinct_count
+    from {{ ref('core__medical_claim') }}
+    group by 
+        year(paid_date)
+        , month(paid_date)
 
-select
-    'dispensing_date' as date_field
-    , cast(year(dispensing_date) as varchar) || right('0'||cast(month(dispensing_date) as varchar),2) as year_month
-    , count(distinct claim_id)
-from {{ ref('core__pharmacy_claim') }}
-group by 
-    year(dispensing_date)
-    , month(dispensing_date)
+    union all
 
-union all
+    select
+        'dispensing_date' as date_field
+        , cast(year(dispensing_date) as varchar) || right('0'||cast(month(dispensing_date) as varchar),2) as year_month
+        , count(distinct claim_id) as distinct_count
+    from {{ ref('core__pharmacy_claim') }}
+    group by 
+        year(dispensing_date)
+        , month(dispensing_date)
 
-select
-    'pharmacy paid_date' as date_field
-    , cast(year(paid_date) as varchar) || right('0'||cast(month(paid_date) as varchar),2) as year_month
-    , count(distinct claim_id)
-from {{ ref('core__pharmacy_claim') }}
-group by 
-    year(paid_date)
-    , month(paid_date)
+    union all
+
+    select
+        'pharmacy paid_date' as date_field
+        , cast(year(paid_date) as varchar) || right('0'||cast(month(paid_date) as varchar),2) as year_month
+        , count(distinct claim_id) as distinct_count
+    from {{ ref('core__pharmacy_claim') }}
+    group by 
+        year(paid_date)
+        , month(paid_date)
+
+    union all 
+
+    select
+        'member_months' as date_field
+        , year_month
+        , count(*) as distinct_count
+    from {{ ref('financial_pmpm__member_months') }}
+    group by
+        year_month
+)
 
 
-
-
+select * from
+    (select
+        year_month
+        , date_field
+        , distinct_count
+        from date_stage
+        ) s
+        pivot
+        (max(distinct_count) for date_field in ('claim_start_date','claim_end_date','admission_date'
+        ,'discharge_date','medical paid_date','dispensing_date','pharmacy paid_date','member_months')
+    )piv
