0fda0bf32a5a7392af60be76631f6066e493335e
thutuva <thuxuan@tuvahealth.com>
2023-09-27 17:10:02 +0000

    added orphan claims insight

---
+++ models/insights/final/insights__orphan_claim_by_claim_type.sql
@@ -0,0 +1,42 @@
+with medical_claim_orphan as(
+select
+    'medical_claim' as claim_type
+    , claim_id
+    , patient_id
+    , cast({{ date_part("year", "claim_end_date") }} as {{ dbt.type_string() }}) || right('0'||cast({{ date_part("month", "claim_end_date") }} as {{ dbt.type_string() }}),2) as year_month
+from {{ ref('core__medical_claim') }}
+)
+
+, pharmacy_claim_orphan as(
+select
+    'pharmacy_claim' as claim_type
+    , claim_id
+    , patient_id
+    , cast({{ date_part("year", "dispensing_date") }} as {{ dbt.type_string() }}) || right('0'||cast({{ date_part("month", "dispensing_date") }} as {{ dbt.type_string() }}),2) as year_month
+from {{ ref('core__pharmacy_claim') }}
+)
+, union_orphans as(
+  select 
+    med.* 
+  from medical_claim_orphan med
+  left join {{ ref('financial_pmpm__member_months') }} months
+      on med.patient_id = months.patient_id
+      and med.year_month = months.year_month
+  where months.patient_id is null
+
+  union all
+
+  select 
+    med.* 
+  from pharmacy_claim_orphan med
+  left join {{ ref('financial_pmpm__member_months') }} months
+      on med.patient_id = months.patient_id
+      and med.year_month = months.year_month
+  where months.patient_id is null
+)
+
+select
+    claim_type
+    , count(distinct claim_id) as distinct_claim_count
+from union_orphans
+group by claim_type
