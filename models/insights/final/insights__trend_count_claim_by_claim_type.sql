2f19c5850005e6e922117060f6d781b7ad1f7671
thutuva <thuxuan@tuvahealth.com>
2023-09-28 15:31:20 +0000

    changed column alias

--- models/insights/final/insights__trend_count_claim_by_claim_type.sql
+++ models/insights/final/insights__trend_count_claim_by_claim_type.sql
@@ -18,7 +18,7 @@ with trend_by_claim_type as(
     year_month
     , claim_type
     , distinct_claim_count
-    , lag(distinct_claim_count) over (partition by claim_type order by year_month) as previous_claim_count
+    , lag(distinct_claim_count) over (partition by claim_type order by year_month) as previous_distinct_claim_count
  from trend_by_claim_type
 )
 
@@ -26,7 +26,7 @@ select
     year_month
     , claim_type
     , distinct_claim_count
-    , distinct_claim_count - previous_claim_count as distinct_claim_count_change
+    , distinct_claim_count - previous_distinct_claim_count as distinct_claim_count_change
     , case
         when distinct_claim_count <> 0 then ((distinct_claim_count-previous_distinct_claim_count) / distinct_claim_count) * 100 
             else distinct_claim_count
