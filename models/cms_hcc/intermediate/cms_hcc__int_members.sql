70bb564746441acbbf5c49753a9228fb56d7a4a2
sarah-tuva <sarah@tuvahealth.com>
2023-10-09 15:13:42 -0600

    Fix string casting and update dbt docs

--- models/cms_hcc/intermediate/cms_hcc__int_members.sql
+++ models/cms_hcc/intermediate/cms_hcc__int_members.sql
@@ -201,7 +201,7 @@ with stg_eligibility as (
         /*
            Defaulting everyone to non-institutional until logic is added
         */
-        , cast('No' as TEXT) as institutional_status
+        , cast('No' as {{ dbt.type_string() }}) as institutional_status
         , enrollment_status_default
         , case
             when dual_status_code is null then TRUE
