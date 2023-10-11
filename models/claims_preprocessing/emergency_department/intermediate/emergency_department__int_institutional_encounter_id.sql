3d7dbac28c17e12a0086fb5d09be4184bff200f0
thutuva <thuxuan@tuvahealth.com>
2023-10-11 16:52:20 +0000

    removed case statment logic to merge adjacent claims

--- models/claims_preprocessing/emergency_department/intermediate/emergency_department__int_institutional_encounter_id.sql
+++ models/claims_preprocessing/emergency_department/intermediate/emergency_department__int_institutional_encounter_id.sql
@@ -39,13 +39,6 @@ select
     -- Claims with same end_date and same facility_npi should be merged:
     when (aa.end_date = bb.end_date
           and aa.facility_npi = bb.facility_npi) then 1
-
-    -- Claims with different end_date and start_date that are
-    -- adjacent (i.e. separated by 1 day) should be merged:
-    when (aa.end_date + 1 = bb.start_date
-          and aa.facility_npi = bb.facility_npi
-	  and aa.discharge_disposition_code = '30') then 1
-
     -- Claims with different end_date 
     -- should be merged if they overlap:
     when ( (aa.end_date <> bb.end_date) and 
