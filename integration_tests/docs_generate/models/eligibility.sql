58089a31605ef4914151e7ac038264f7efe11156
sarah-tuva <sarah@tuvahealth.com>
2023-11-28 15:53:21 -0700

    Fix formatting and seed references for claim input layer

--- integration_tests/docs_generate/models/eligibility.sql
+++ integration_tests/docs_generate/models/eligibility.sql
@@ -1,24 +1,25 @@
-select patient_id,
-       member_id,
-       gender,
-       race,
-       birth_date,
-       death_date,
-       death_flag,
-       enrollment_start_date,
-       enrollment_end_date,
-       payer,
-       payer_type,
-       plan,
-       original_reason_entitlement_code,
-       dual_status_code,
-       medicare_status_code,
-       first_name,
-       last_name,
-       address,
-       city,
-       state,
-       zip_code,
-       phone,
-       data_source
-from tuva._tuva_synthetic.eligibility_seed
+select
+      patient_id
+    , member_id
+    , gender
+    , race
+    , birth_date
+    , death_date
+    , death_flag
+    , enrollment_start_date
+    , enrollment_end_date
+    , payer
+    , payer_type
+    , plan
+    , original_reason_entitlement_code
+    , dual_status_code
+    , medicare_status_code
+    , first_name
+    , last_name
+    , address
+    , city
+    , state
+    , zip_code
+    , phone
+    , data_source
+from {{ ref('eligibility_seed') }}
