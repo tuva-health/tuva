17dba20c39ec5e7ee941b232322c3e439391290a
thutuva <thuxuan@tuvahealth.com>
2023-12-11 19:43:02 +0000

    added query to normalize/validate eligibility dates

--- models/claims_preprocessing/normalized_input_layer/final/normalized_input__eligibility.sql
+++ models/claims_preprocessing/normalized_input_layer/final/normalized_input__eligibility.sql
@@ -1,26 +1,28 @@
 select
-    patient_id
-    , member_id
-    , gender
-    , race
-    , birth_date
-    , death_date
-    , death_flag
-    , enrollment_start_date
-    , enrollment_end_date
-    , payer
-    , payer_type
-    , plan
-    , original_reason_entitlement_code
-    , dual_status_code
-    , medicare_status_code
-    , first_name
-    , last_name
-    , address
-    , city
-    , state
-    , zip_code
-    , phone
-    , data_source
+    elig.patient_id
+    , elig.member_id
+    , elig.gender
+    , elig.race
+    , date_norm.birth_date
+    , date_norm.death_date
+    , elig.death_flag
+    , date_norm.enrollment_start_date
+    , date_norm.enrollment_end_date
+    , elig.payer
+    , elig.payer_type
+    , elig.plan
+    , elig.original_reason_entitlement_code
+    , elig.dual_status_code
+    , elig.medicare_status_code
+    , elig.first_name
+    , elig.last_name
+    , elig.address
+    , elig.city
+    , elig.state
+    , elig.zip_code
+    , elig.phone
+    , elig.data_source
 from {{ ref('eligibility') }} elig
+left join {{ ref('normalized_input__int_eligibility_dates_normalize') }} date_norm
+    on elig.patient_id = date_norm.patient_id
 
---
+++ models/claims_preprocessing/normalized_input_layer/intermediate/normalized_input__int_eligibility_dates_normalize.sql
@@ -0,0 +1,16 @@
+
+select distinct
+  elig.patient_id
+  , cal_dob.full_date as normalized_birth_date
+  , cal_death.full_date as normalized_death_date
+  , cal_enroll_start.full_date as normalized_enrollment_start_date
+  , cal_enroll_end.full_date as normalized_enrollment_end_date
+from {{ ref('eligibility') }} elig
+left join {{ ref('terminology__calendar') }} cal_dob
+    on elig.birth_date = cal_dob.full_date
+left join {{ ref('terminology__calendar') }} cal_death
+    on elig.death_date = cal_death.full_date
+left join {{ ref('terminology__calendar') }} cal_enroll_start
+    on elig.enrollment_start_date = cal_enroll_start.full_date
+left join {{ ref('terminology__calendar') }} cal_enroll_end
+    on elig.enrollment_end_date = cal_enroll_end.full_date
