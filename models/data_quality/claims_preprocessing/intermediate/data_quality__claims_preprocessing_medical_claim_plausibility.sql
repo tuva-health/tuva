b8783fedfb14ead9759dbae9f6292ea2640c845b
sarah-tuva <sarah@tuvahealth.com>
2023-12-11 18:10:26 -0700

    Add claim date plausibility checks

--- models/data_quality/claims_preprocessing/intermediate/data_quality__claims_preprocessing_medical_claim_plausibility.sql
+++ models/data_quality/claims_preprocessing/intermediate/data_quality__claims_preprocessing_medical_claim_plausibility.sql
@@ -21,6 +21,13 @@ with medical_claim as (
 
 )
 
+, calendar as (
+
+    select full_date
+    from {{ ref('terminology__calendar') }}
+
+)
+
 , claim_start_date_after_claim_end_date as (
 
     select
@@ -74,6 +81,194 @@ with medical_claim as (
 
 )
 
+, valid_claim_start_date as (
+
+    select
+          test_catalog.test_name
+        , test_catalog.pipeline_test
+        , test_catalog.source_table
+        , 'all' as claim_type
+        , test_catalog.test_category
+        , 'claim_id' as grain
+        , medical_claim.claim_id
+        , count(*) as counts
+        , '{{ var('tuva_last_run')}}' as tuva_last_run
+    from medical_claim
+         left join calendar
+           on medical_claim.claim_start_date = calendar.full_date
+         left join test_catalog
+           on test_catalog.test_name = 'claim start date invalid'
+           and test_catalog.source_table = 'medical_claim'
+    where calendar.full_date is null
+    and medical_claim.claim_start_date is not null
+    group by
+          medical_claim.claim_id
+        , test_catalog.source_table
+        , test_catalog.test_category
+        , test_catalog.test_name
+        , test_catalog.pipeline_test
+
+)
+
+, valid_claim_end_date as (
+
+    select
+          test_catalog.test_name
+        , test_catalog.pipeline_test
+        , test_catalog.source_table
+        , 'all' as claim_type
+        , test_catalog.test_category
+        , 'claim_id' as grain
+        , medical_claim.claim_id
+        , count(*) as counts
+        , '{{ var('tuva_last_run')}}' as tuva_last_run
+    from medical_claim
+         left join calendar
+           on medical_claim.claim_end_date = calendar.full_date
+         left join test_catalog
+           on test_catalog.test_name = 'claim end date invalid'
+           and test_catalog.source_table = 'medical_claim'
+    where calendar.full_date is null
+    and medical_claim.claim_end_date is not null
+    group by
+          medical_claim.claim_id
+        , test_catalog.source_table
+        , test_catalog.test_category
+        , test_catalog.test_name
+        , test_catalog.pipeline_test
+
+)
+
+, valid_claim_line_start_date as (
+
+    select
+          test_catalog.test_name
+        , test_catalog.pipeline_test
+        , test_catalog.source_table
+        , 'all' as claim_type
+        , test_catalog.test_category
+        , 'claim_id' as grain
+        , medical_claim.claim_id
+        , count(*) as counts
+        , '{{ var('tuva_last_run')}}' as tuva_last_run
+    from medical_claim
+         left join calendar
+           on medical_claim.claim_line_start_date = calendar.full_date
+         left join test_catalog
+           on test_catalog.test_name = 'claim line start date invalid'
+           and test_catalog.source_table = 'medical_claim'
+    where calendar.full_date is null
+    and medical_claim.claim_line_start_date is not null
+    group by
+          medical_claim.claim_id
+        , test_catalog.source_table
+        , test_catalog.test_category
+        , test_catalog.test_name
+        , test_catalog.pipeline_test
+
+)
+
+, valid_claim_line_end_date as (
+
+    select
+          test_catalog.test_name
+        , test_catalog.pipeline_test
+        , test_catalog.source_table
+        , 'all' as claim_type
+        , test_catalog.test_category
+        , 'claim_id' as grain
+        , medical_claim.claim_id
+        , count(*) as counts
+        , '{{ var('tuva_last_run')}}' as tuva_last_run
+    from medical_claim
+         left join calendar
+           on medical_claim.claim_line_end_date = calendar.full_date
+         left join test_catalog
+           on test_catalog.test_name = 'claim line end date invalid'
+           and test_catalog.source_table = 'medical_claim'
+    where calendar.full_date is null
+    and medical_claim.claim_line_end_date is not null
+    group by
+          medical_claim.claim_id
+        , test_catalog.source_table
+        , test_catalog.test_category
+        , test_catalog.test_name
+        , test_catalog.pipeline_test
+
+)
+
+, valid_admission_date as (
+
+    select
+          test_catalog.test_name
+        , test_catalog.pipeline_test
+        , test_catalog.source_table
+        , 'institutional' as claim_type
+        , test_catalog.test_category
+        , 'claim_id' as grain
+        , medical_claim.claim_id
+        , count(*) as counts
+        , '{{ var('tuva_last_run')}}' as tuva_last_run
+    from medical_claim
+         left join calendar
+           on medical_claim.admission_date = calendar.full_date
+         left join test_catalog
+           on test_catalog.test_name = 'admission date invalid'
+           and test_catalog.source_table = 'medical_claim'
+    where medical_claim.claim_type = 'institutional'
+    and calendar.full_date is null
+    and medical_claim.admission_date is not null
+    group by
+          medical_claim.claim_id
+        , test_catalog.source_table
+        , test_catalog.test_category
+        , test_catalog.test_name
+        , test_catalog.pipeline_test
+
+)
+
+, valid_discharge_date as (
+
+    select
+          test_catalog.test_name
+        , test_catalog.pipeline_test
+        , test_catalog.source_table
+        , 'institutional' as claim_type
+        , test_catalog.test_category
+        , 'claim_id' as grain
+        , medical_claim.claim_id
+        , count(*) as counts
+        , '{{ var('tuva_last_run')}}' as tuva_last_run
+    from medical_claim
+         left join calendar
+           on medical_claim.discharge_date = calendar.full_date
+         left join test_catalog
+           on test_catalog.test_name = 'discharge date invalid'
+           and test_catalog.source_table = 'medical_claim'
+    where medical_claim.claim_type = 'institutional'
+    and calendar.full_date is null
+    and medical_claim.discharge_date is not null
+    group by
+          medical_claim.claim_id
+        , test_catalog.source_table
+        , test_catalog.test_category
+        , test_catalog.test_name
+        , test_catalog.pipeline_test
+
+)
+
 select * from claim_start_date_after_claim_end_date
 union all
 select * from admission_date_after_discharge_date
+union all
+select * from valid_claim_start_date
+union all
+select * from valid_claim_end_date
+union all
+select * from valid_claim_line_start_date
+union all
+select * from valid_claim_line_end_date
+union all
+select * from valid_admission_date
+union all
+select * from valid_discharge_date
