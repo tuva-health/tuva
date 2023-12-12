025f7755d58350a0ce2f99980321f71b7b278ad7
sarah-tuva <sarah@tuvahealth.com>
2023-12-12 16:35:34 -0700

    Add npi checks

--- models/data_quality/claims_preprocessing/intermediate/data_quality__claims_preprocessing_test_detail_stage_medical_claim.sql
+++ models/data_quality/claims_preprocessing/intermediate/data_quality__claims_preprocessing_test_detail_stage_medical_claim.sql
@@ -122,3 +122,13 @@ select distinct
     , test_name
     , pipeline_test
 from {{ ref('data_quality__claims_preprocessing_medical_claim_line_numbers') }}
+union all
+select distinct
+    source_table
+    , claim_type
+    , grain
+    , claim_id
+    , test_category
+    , test_name
+    , pipeline_test
+from {{ ref('data_quality__claims_preprocessing_medical_claim_invalid_npi') }}
