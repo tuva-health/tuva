152a1e3114034a756c5a6695e19721363b613246
sarah-tuva <sarah@tuvahealth.com>
2023-12-11 17:44:11 -0700

    Organize data quality macros

---
+++ macros/data_quality/pharmacy_claim_header_duplicate_check.sql
@@ -0,0 +1,20 @@
+{#
+    Required variable input: relation and column_list
+
+#}
+
+
+{% macro pharmacy_claim_header_duplicate_check(relation, column_list) %}
+    {%- for column_item in column_list %}
+        select
+            claim_id
+            , '{{ column_item }}' as column_checked
+            , count(distinct {{ column_item }}) as duplicate_count
+        from {{ relation }}
+        group by claim_id
+        having count(distinct {{ column_item }}) > 1
+        {% if not loop.last -%}
+            union all
+        {%- endif -%}
+        {%- endfor -%}
+{% endmacro %}
