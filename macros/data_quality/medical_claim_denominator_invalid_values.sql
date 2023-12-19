1c71b1dfdfb688f835d772491d41c22e7d0078d3
sarah-tuva <sarah@tuvahealth.com>
2023-12-19 16:50:52 -0700

    Clean up logic for denominator macros

--- macros/data_quality/medical_claim_denominator_invalid_values.sql
+++ macros/data_quality/medical_claim_denominator_invalid_values.sql
@@ -2,7 +2,7 @@
     Required variable input: relation
 
     The first step of this macro is to query the test catalog to retrieve
-    the test_field for all invalid_value tests for eligibility.
+    the test_field for all invalid_value tests for medical_claim.
 
     The second step sets the results of the query to a list.
 
@@ -10,11 +10,11 @@
     for the CTE that builds the denominators for invalid value tests.
 #}
 
-{% macro eligibility_denominator_invalid_values(relation) %}
+{% macro medical_claim_denominator_invalid_values(relation) %}
 {%- set sql_statement -%}
     select test_field
     from {{ ref('data_quality__test_catalog') }}
-    where source_table = 'normalized_input__eligibility'
+    where source_table = 'normalized_input__medical_claim'
     and test_category = 'invalid_values'
 {%- endset -%}
 
@@ -29,12 +29,12 @@
     {%- for test_field in results_list -%}
     select
           cat.test_name
-        , count(distinct rel.patient_id) as denominator
+        , count(distinct rel.claim_id,rel.data_source) as denominator
         , '{{ var('tuva_last_run')}}' as tuva_last_run
     from {{ relation }} as rel
          left join {{ ref('data_quality__test_catalog') }} as cat
            on cat.test_category = 'invalid_values'
-           and cat.source_table = 'normalized_input__eligibility'
+           and cat.source_table = 'normalized_input__medical_claim'
            and cat.test_field = '{{ test_field }}'
     where rel.{{ test_field }} is not null
     group by cat.test_name
