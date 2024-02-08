191023b0bcc5108423c9690b1976d432ee739cc1
renish-maitri <renish.rauniyar@maitriservices.com>
2024-02-08 09:09:14 +0545

    chagne in age

--- models/quality_measures/intermediate/nqf0059_diabetes_hemoglobin_a1c/quality_measures__int_nqf0059_denominator.sql
+++ models/quality_measures/intermediate/nqf0059_diabetes_hemoglobin_a1c/quality_measures__int_nqf0059_denominator.sql
@@ -172,7 +172,7 @@ with performance_period as (
         , performance_period.performance_period_begin
         , performance_period.performance_period_end
         , patients.birth_date
-        , floor({{ datediff('patients.birth_date', 'visit_encounters.encounter_start_date', 'hour') }} / 8760.0) as age
+        , floor({{ datediff('patients.birth_date', 'performance_period.performance_period_end', 'hour') }} / 8760.0) as age
     from visit_encounters
     left join patients
         on visit_encounters.patient_id = patients.patient_id
