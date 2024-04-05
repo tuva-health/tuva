4d9474b6218a6f8b2f194c6087eedd6f8dedbb7b
utsavpaudel <utsavpaudel111@gmail.com>
2024-04-05 13:27:48 +0545

    feat: numerator from procedure and pharmacy_claims

--- models/quality_measures/intermediate/nqf0053_osteoporosis_management_in_women_who_had_a_fracture/quality_measures__int_nqf0053_numerator.sql
+++ models/quality_measures/intermediate/nqf0053_osteoporosis_management_in_women_who_had_a_fracture/quality_measures__int_nqf0053_numerator.sql
@@ -80,8 +80,20 @@ value_sets as (
         , code_system
         , concept_name
     from value_sets
-    where lower(concept_name) in ('bone mineral density test')
+    where lower(concept_name) in (
+          'bone mineral density test'
+        , 'bone mineral density tests cpt'
+        , 'bone mineral density tests icd10pcs'
+        , 'bone mineral density tests hcpcs'
+        , 'dexa dual energy xray absorptiometry, bone density'
+        , 'central dual energy x-ray absorptiometry (dxa)'
+        , 'spinal densitometry x-ray' 
+        , 'ultrasonography for densitometry' 
+        , 'ct bone density axial'
+        , 'peripheral dual-energy x-ray absorptiometry (dxa)'
+    )
 )
+
 
 
 , proc_osteo as (
@@ -149,7 +161,7 @@ value_sets as (
           code
         , code_system
     from value_sets
-    where lower(concept_name) = 'fracture procedures'
+    where lower(concept_name) = 'fracture diagnoses'
 
 )
 
@@ -191,14 +203,13 @@ value_sets as (
           proc_osteo_within_range.*
         , bone_fracture_conditions.claim_id
         , bone_fracture_conditions.recorded_date
-        -- , bone_fracture_conditions.source_code
-        -- bone_fracture_conditions.source_code_type
+        
     from 
         proc_osteo_within_range
     inner join bone_fracture_conditions
         on proc_osteo_within_range.patient_id = bone_fracture_conditions.patient_id
-        and proc_osteo_within_range.encounter_id = bone_fracture_conditions.encounter_id
-
+        --  and coalesce(proc_osteo_within_range.encounter_id, 'NULL') = coalesce(bone_fracture_conditions.encounter_id, 'NULL')
+        
 )
 
 , osteo_proc_within_6_months as (
@@ -209,6 +220,8 @@ value_sets as (
     where datediff(month, recorded_date, procedure_date) <= 6
 )
 
+-- pharmacy_claim begin
+
 , osteo_pharmacy_codes as (
 
     select
@@ -217,7 +230,7 @@ value_sets as (
         , concept_name
     from value_sets
     where lower(concept_name) in ('osteoporosis medications for urology care')
-    /*No other method to figure out vitamins/minerals CAN BE CHANGED TO osteoporosis medication for urology care to match the drugs in CMS doc*/
+    /*osteoporosis medication for urology care to match the drugs in CMS doc*/
 )
 
 , pharm_claim_osteo as (
@@ -230,7 +243,7 @@ value_sets as (
     inner join osteo_pharmacy_codes
     on pharmacy_claims.ndc_code = osteo_pharmacy_codes.code
         -- and pharmacy_claims.code_system = osteo_pharmacy_codes.code_system
-        /* this is commented out as their is only NDC code system in pharmacy_claims */
+        /* this is commented out as there is only NDC code system in pharmacy_claims */
 )
 
 , pharmacy_claim_osteo_within_range as (
@@ -246,7 +259,7 @@ value_sets as (
         on pharm_claim_osteo.patient_id = patients_proc_osteo_not_taken.patient_id
         and pharm_claim_osteo.dispensing_date between 
             patients_proc_osteo_not_taken.performance_period_begin and patients_proc_osteo_not_taken.performance_period_end
-            
+
 )
 
 , bone_fracture_codes_pharmacy as (
@@ -307,8 +320,8 @@ value_sets as (
         , osteo_proc_within_6_months.measure_id
         , osteo_proc_within_6_months.measure_name
         , osteo_proc_within_6_months.measure_version
-        , osteo_proc_within_6_months.procedure_date as evidence_date
-        , 'DXA Yes' as evidence_value
+        -- , osteo_proc_within_6_months.procedure_date as evidence_date
+        -- , 'DXA Yes' as evidence_value
         , 1 as numerator_flag
     from osteo_proc_within_6_months
 
@@ -321,8 +334,8 @@ value_sets as (
         , osteo_pharmacy_within_6_months.measure_id
         , osteo_pharmacy_within_6_months.measure_name
         , osteo_pharmacy_within_6_months.measure_version
-        , osteo_pharmacy_within_6_months.dispensing_date as evidence_date
-        , 'Pharmacy_claims' as evidence_value
+        -- , osteo_pharmacy_within_6_months.dispensing_date as evidence_date
+        -- , 'Pharmacy_claims' as evidence_value
         , 1 as numerator_flag
     from osteo_pharmacy_within_6_months
 )
@@ -336,8 +349,8 @@ value_sets as (
         , cast(measure_id as {{ dbt.type_string() }}) as measure_id
         , cast(measure_name as {{ dbt.type_string() }}) as measure_name
         , cast(measure_version as {{ dbt.type_string() }}) as measure_version
-        , cast(evidence_date as date) as evidence_date
-        , cast(evidence_value as {{ dbt.type_string() }}) as evidence_value
+        -- , cast(evidence_date as date) as evidence_date
+        -- , cast(evidence_value as {{ dbt.type_string() }}) as evidence_value
         , cast(numerator_flag as integer) as numerator_flag
     from numerator
 )
@@ -349,47 +362,8 @@ select
     , measure_id
     , measure_name
     , measure_version
-    , evidence_date
-    , evidence_value
+    -- , evidence_date
+    -- , evidence_value
     , numerator_flag
 from add_data_types
-
-
-
-----------------------------------------------------------------------------------------------
--- select * from osteo_proc_within_6_months
-
-
--- proc_osteo_codes as (
---     select
---           code
---         , code_system
---     from {{ ref('quality_measures__value_sets') }}
---     where code IN ('3095F')
--- ),
-
--- procedure_taken2 as (
-
---     select 
---         proc_osteo_codes.*,
---         'Yes' as DXA_flag
---     from {{ref('quality_measures__stg_core__procedure')}} procs
---     inner join proc_osteo_codes
---     on procs.source_code = proc_osteo_codes.code
---         and procs.source_code_type = proc_osteo_codes.code_system
-
--- ),
-
--- proc_with_dxa_flag as (
-
---     select a.*, b.DXA_flag
---     from {{ref('quality_measures__stg_core__procedure')}} a
---     left join procedure_taken2 b
---     on  a.procedure_id = b.procedure_id
-
--- ),
-
-----------------------------------------------------------------------------------------------
-
-
 
