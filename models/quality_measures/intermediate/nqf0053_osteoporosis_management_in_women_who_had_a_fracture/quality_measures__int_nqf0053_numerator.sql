2c431edf714b7cdb70596aa73dc3f397158efbe3
utsavpaudel <utsavpaudel111@gmail.com>
2024-04-08 09:37:18 +0545

    minor: aesthetic changes

--- models/quality_measures/intermediate/nqf0053_osteoporosis_management_in_women_who_had_a_fracture/quality_measures__int_nqf0053_numerator.sql
+++ models/quality_measures/intermediate/nqf0053_osteoporosis_management_in_women_who_had_a_fracture/quality_measures__int_nqf0053_numerator.sql
@@ -6,66 +6,18 @@
 
 with denominator as (
 
-    select * from {{ref('quality_measures__int_nqf0053_denominator')}}
+    select 
+          patient_id
+        , performance_period_begin
+        , performance_period_end
+        , measure_id
+        , measure_name
+        , measure_version
+    from {{ref('quality_measures__int_nqf0053_denominator')}}
 
-),
+)
 
-/*
-medications_osteo as (
-
-    select * 
-    from {{ref('quality_measures__stg_core__medication')}}
-    where lower(source_description) in 
-        (
-        'bisphosphonates',
-        'alendronate', 
-        'alendronate-cholecalciferol', 
-        'ibandronate', 
-        'risedronate', 
-        'zoledronic acid', 
-        'teriparatide',
-        'denosumab', 
-        'abaloparatide', 
-        'romosozumab',
-        'raloxifine'
-        )
-),
-
-medications_within_range as(
-
-    select medications_osteo.*
-        , denominator.measure_id
-        , denominator.measure_name
-        , denominator.measure_version
-        , denominator.performance_period_begin
-        , denominator.performance_period_end
-    from medications_osteo
-    inner join denominator
-        on medications_osteo.patient_id = denominator.patient_id
-        and medications_osteo.prescribing_date between 
-            denominator.performance_period_begin and denominator.performance_period_end
-
-),
-
-
-pharma_osteo as (
-
-    select *
-    from {{ref('quality_measures__stg_pharmacy_claim')}}
-
-),
-
-
--- medications_osteo_codes as (
---     select
---           code
---         , code_system
---     from {{ ref('quality_measures__value_sets') }}
---     where code IN ('G8633')
--- ),
-*/
-
-value_sets as (
+, value_sets as (
     select
           code
         , code_system
@@ -109,16 +61,6 @@ value_sets as (
     inner join osteo_proccodes
         on procs.source_code = osteo_proccodes.code
         and procs.source_code_type = osteo_proccodes.code_system
-
-    -- where lower(source_description) in 
-    --     (
-    --     'central dual energy x-ray absorptiometry (dxa)',
-    --     'spinal densitometry x-ray', 
-    --     'ultrasonography for densitometry', 
-    --     'ct bone density axial', 
-    --     'peripheral dual-energy x-ray absorptiometry (dxa)' 
-    --     )
-    -- or lower(source_description) like '%bone mineral density test%'
 )
 
 
@@ -203,13 +145,13 @@ value_sets as (
           proc_osteo_within_range.*
         , bone_fracture_conditions.claim_id
         , bone_fracture_conditions.recorded_date
-        
+
     from 
         proc_osteo_within_range
     inner join bone_fracture_conditions
         on proc_osteo_within_range.patient_id = bone_fracture_conditions.patient_id
-        --  and coalesce(proc_osteo_within_range.encounter_id, 'NULL') = coalesce(bone_fracture_conditions.encounter_id, 'NULL')
-        
+        --  and proc_osteo_within_range.encounter_id = bone_fracture_conditions.encounter_id
+        /*The one person in procedure likely to be in numerator has encounter_id empty in procedure as well as condition*/
 )
 
 , osteo_proc_within_6_months as (
@@ -308,8 +250,6 @@ value_sets as (
     from osteo_pharmacy_for_bone_fracture
     where datediff(month, dispensing_date, recorded_date) <= 6
 )
-
---------------------------
 
 
 , numerator as (
