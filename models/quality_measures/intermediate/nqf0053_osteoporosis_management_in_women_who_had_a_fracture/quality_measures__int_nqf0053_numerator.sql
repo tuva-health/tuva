8c6624a2cdeb19eb4affc406042e2d39ec907501
utsavpaudel <utsavpaudel111@gmail.com>
2024-04-10 14:40:58 +0545

    feat: 0053 long model and docs

--- models/quality_measures/intermediate/nqf0053_osteoporosis_management_in_women_who_had_a_fracture/quality_measures__int_nqf0053_numerator.sql
+++ models/quality_measures/intermediate/nqf0053_osteoporosis_management_in_women_who_had_a_fracture/quality_measures__int_nqf0053_numerator.sql
@@ -243,7 +243,7 @@ with denominator as (
         , cast(measure_name as {{ dbt.type_string() }}) as measure_name
         , cast(measure_version as {{ dbt.type_string() }}) as measure_version
         , cast(evidence_date as date) as evidence_date
-        , cast(null as {{ dbt.type_string() }} ) as evidence_value
+        , null as evidence_value
         , cast(numerator_flag as integer) as numerator_flag
     from numerator
 )
