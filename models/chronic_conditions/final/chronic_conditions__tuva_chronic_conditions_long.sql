44c8d9d2a7caee8eaeff90e3b35d5ad5f8c9ff85
Coco Zuloaga <coco@tuvahealth.com>
2024-04-25 14:58:19 -0600

    Tuva CC new

--- models/chronic_conditions/final/chronic_conditions__tuva_chronic_conditions_long.sql
+++ models/chronic_conditions/final/chronic_conditions__tuva_chronic_conditions_long.sql
@@ -3,63 +3,34 @@
    )
 }}
 
-with condition_row_number as (
-
-    select 
-          patient_id
-        , normalized_code
-        , recorded_date
-        , row_number() over(
-            partition by
-                  patient_id
-                , normalized_code
-            order by recorded_date asc
-          ) as rn_asc
-        , row_number() over(
-            partition by
-                  patient_id
-                , normalized_code
-            order by recorded_date desc
-          ) as rn_desc
-    from {{ ref('tuva_chronic_conditions__stg_core__condition') }}
-
-)
-
-, patient_conditions as (
-
-    select 
-          patient_id
-        , normalized_code as icd_10_cm
-        , max(
-            case
-                when rn_asc = 1
-                then recorded_date
-            end
-          ) as first_diagnosis_date
-        , max(
-            case
-                when rn_desc = 1
-                then recorded_date
-            end
-          ) as last_diagnosis_date
-    from condition_row_number
-    group by 
-          patient_id
-        , normalized_code
-
-)
-
+with all_conditions as (
 select 
-      pc.patient_id
-    , h.condition_family
-    , h.condition
-    , min(first_diagnosis_date) as first_diagnosis_date
-    , max(last_diagnosis_date) as last_diagnosis_date
-    , '{{ var('tuva_last_run')}}' as tuva_last_run
-from {{ ref('chronic_conditions__tuva_chronic_conditions_hierarchy') }} h
-     inner join patient_conditions pc
-        on h.icd_10_cm_code = pc.icd_10_cm
-group by 
-      pc.patient_id
-    , h.condition_family
-    , h.condition
+  patient_id,
+  normalized_code,
+  recorded_date
+    from {{ ref('tuva_chronic_conditions__stg_core__condition') }}
+),
+
+
+conditions_with_first_and_last_diagnosis_date as (
+select 
+  patient_id,
+  normalized_code as icd_10_cm,
+  min(recorded_date) as first_diagnosis_date,
+  max(recorded_date) as last_diagnosis_date
+from all_conditions
+group by patient_id, normalized_code
+
+)
+
+
+select
+  aa.patient_id,
+  bb.concept_name as condition,
+  min(first_diagnosis_date) as first_diagnosis_date,
+  max(last_diagnosis_date) as last_diagnosis_date,
+  '{{ var('tuva_last_run')}}' as tuva_last_run
+from conditions_with_first_and_last_diagnosis_date aa
+inner join {{ ref('value_set_member_relevant_fields') }} bb
+on aa.icd_10_cm = bb.code
+group by aa.patient_id, bb.concept_name
