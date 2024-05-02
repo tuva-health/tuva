fcda162e39e70def77927c653068bed3b8c3bda5
thutuva <thuxuan@tuvahealth.com>
2024-05-02 16:23:23 +0000

    added enabled config

--- models/ahrq_measures/pqi/staging/ahrq_measures__stg_pqi_inpatient_encounter.sql
+++ models/ahrq_measures/pqi/staging/ahrq_measures__stg_pqi_inpatient_encounter.sql
@@ -1,3 +1,8 @@
+{{ config(
+     enabled = var('pqi_enabled',var('claims_enabled',var('tuva_marts_enabled',False))) | as_bool
+   )
+}}
+
 select 
     *
   , to_char(encounter_start_date, 'yyyy') as year_number
