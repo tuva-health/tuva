543d11efa00c2e1a294c349b90443ad014a0ead9
bradmontierth <72261515+bradmontierth@users.noreply.github.com>
2024-05-15 08:38:31 -0600

    AHRQ PQI measures (#451)
    * initial commit with pqis
    
    initial commit with pqis
    
    * moving to ahrq measures mart
    
    moving to ahrq measures mart
    
    * added enabled vars; added tuva_last_run; YAML edits; changed profile
    
    * added enabled config
    
    * select only columns for stg and adding column def to yml
    
    * revert back to default settings
    
    * revert to default
    
    * cross compat for bigquery
    
    * revert to default
    
    * fix bigquery syntax errors
    
    * update varchar length for redshift
    
    ---------
    
    Co-authored-by: thutuva <thuxuan@tuvahealth.com>

---
+++ models/ahrq_measures/pqi/intermediate/ahrq_measures__int_pqi_16_num.sql
@@ -0,0 +1,50 @@
+{{ config(
+     enabled = var('pqi_enabled', var('claims_enabled', var('tuva_marts_enabled', false))) | as_bool
+)}}
+
+with diagnosis as (
+    select distinct
+        c.encounter_id
+      , c.data_source
+    from {{ ref('ahrq_measures__stg_pqi_condition') }} as c
+    inner join {{ ref('pqi__value_sets') }} as pqi
+      on c.normalized_code = pqi.code
+      and c.normalized_code_type = 'icd-10-cm'
+      and pqi.value_set_name = 'diabetes_diagnosis_codes'
+      and pqi_number = '16'
+    where c.encounter_id is not null
+),
+
+procedure as (
+    select distinct
+        p.encounter_id
+      , p.data_source
+    from {{ ref('ahrq_measures__stg_pqi_procedure') }} as p
+    inner join diagnosis as d 
+      on p.encounter_id = d.encounter_id
+      and d.data_source = p.data_source
+    inner join {{ ref('pqi__value_sets') }} as pqi
+      on p.normalized_code = pqi.code
+      and p.normalized_code_type = 'icd-10-pcs'
+      and pqi.value_set_name = 'lower-extremity_amputation_procedure_codes'
+      and pqi_number = '16'
+)
+
+select
+    e.data_source
+  , e.patient_id
+  , e.year_number
+  , e.encounter_id
+  , '{{ var('tuva_last_run') }}' as tuva_last_run
+from {{ ref('ahrq_measures__stg_pqi_inpatient_encounter') }} as e
+inner join {{ ref('ahrq_measures__int_pqi_16_denom') }} as denom 
+  on e.patient_id = denom.patient_id
+  and e.data_source = denom.data_source
+  and e.year_number = denom.year_number
+inner join procedure as p 
+  on e.encounter_id = p.encounter_id
+  and e.data_source = p.data_source
+left join {{ ref('ahrq_measures__int_pqi_16_exclusions') }} as shared
+  on e.encounter_id = shared.encounter_id
+  and e.data_source = shared.data_source
+where shared.encounter_id is null
