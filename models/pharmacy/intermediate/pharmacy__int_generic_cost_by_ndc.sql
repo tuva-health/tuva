5ab3af2f2a507409b72e3ba82fd07fe98d82d950
bradmontierth <72261515+bradmontierth@users.noreply.github.com>
2024-05-30 09:36:16 -0600

    Brand generic (#485)
    * rxnorm files
    
    * pharmacy mart work
    
    * finish brand generic analysis
    
    * add in config parameter
    
    * revert to defaults
    
    * add tuva last run variable
    
    * fix terminology type
    
    * fix missing ref
    
    * update data type in value set
    
    * redshift terminology string length
    
    * another character length fix
    
    * Update dbt_project.yml
    
    reverted test_data_override to false
    
    ---------
    
    Co-authored-by: thutuva <101137626+thutuva@users.noreply.github.com>

---
+++ models/pharmacy/intermediate/pharmacy__int_generic_cost_by_ndc.sql
@@ -0,0 +1,20 @@
+{{ config(
+    enabled = var('brand_generic_enabled', var('claims_enabled', var('tuva_marts_enabled', False))) | as_bool
+) }}
+
+/* for each brand rxcui, what is the average cost/unit of all generics available*/
+
+select
+    g.product_rxcui as brand_rxcui
+  , cl.data_source
+  , cl.ndc_code as generic_ndc_code
+  , cl.paid_amount
+  , cl.claim_count
+  , cl.total_units
+  , cl.cost_per_unit
+  , case when claim_count > 0 then 1 else 0 end as prescribed_atleast_one_generic_history
+  , '{{ var('tuva_last_run')}}' as tuva_last_run
+from {{ ref('pharmacy__rxnorm_generic_available') }} as g
+left join {{ ref('pharmacy__int_claims_current_cost') }} as cl 
+  on cl.ndc_code = g.ndc
+where cl.brand_vs_generic = 'generic'
---
+++ models/pharmacy/staging/pharmacy__stg_pharmacy_claim.sql
@@ -0,0 +1,25 @@
+{{ config(
+    enabled = var('pqi_enabled', var('claims_enabled', var('tuva_marts_enabled', False))) | as_bool
+) }}
+
+select 
+    p.ndc_code
+  , n.fda_description as ndc_description
+  , p.data_source
+  , n.rxcui
+  , p.paid_amount
+  , p.allowed_amount
+  , p.claim_id
+  , p.claim_line_number
+  , p.patient_id
+  , p.member_id
+  , p.prescribing_provider_npi
+  , p.dispensing_provider_npi
+  , p.dispensing_date
+  , p.quantity
+  , p.days_supply
+  , p.refills
+  , p.paid_date
+from {{ ref('core__pharmacy_claim') }} as p
+left join {{ ref('terminology__ndc') }} as n 
+  on p.ndc_code = n.ndc
