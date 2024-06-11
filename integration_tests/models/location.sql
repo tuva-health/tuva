364509030b72a02cd3bae807b4ed8265bfaa8540
Forrest Beckwith <100245109+tuvaforrest@users.noreply.github.com>
2024-06-11 10:32:27 -0400

    updated test data vars (#496)
    * changed "test data override" var to "use_synthetic_data"
    updated models to only build input layer models for enabled configurations
    changed source config when use_synthetic_data = false to work for clinical sources
    removed duplicate "docs generate" sub sub project
    
    * removed package lock and ignored
    
    * removed package lock and ignored

--- integration_tests/models/location.sql
+++ integration_tests/models/location.sql
@@ -1,3 +1,11 @@
+{{ config(
+     enabled = var('clinical_enabled',var('tuva_marts_enabled',False))
+ | as_bool
+   )
+}}
+
+{% if var('use_synthetic_data') == true -%}
+
 select
 cast(null as {{ dbt.type_string() }} ) as location_id
 , cast(null as {{ dbt.type_string() }} ) as npi
@@ -13,3 +21,9 @@ cast(null as {{ dbt.type_string() }} ) as location_id
 , cast(null as {{ dbt.type_string() }} ) as data_source
 , cast(null as {{ dbt.type_timestamp() }} ) as tuva_last_run
 limit 0
+
+{%- else -%}
+
+select * from {{ source('source_input', 'location') }}
+
+{%- endif %}
