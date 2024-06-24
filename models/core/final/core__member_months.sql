bc50016e0ae6537c284c1d1b3ee607546439b769
thutuva <101137626+thutuva@users.noreply.github.com>
2024-06-24 16:50:12 -0400

    Flagging claims with enrollment (#509)
    * added logic to flag claims with enrollment
    
    * added enabled and tuva_last_run
    
    * fixed alias
    
    * fixed casting
    
    * fixed casting
    
    * updated logic that populates inferred claim start date or claim end date
    
    * updated YAML to describe new logic

--- models/core/final/core__member_months.sql
+++ models/core/final/core__member_months.sql
@@ -4,21 +4,21 @@
 }}
 
 with month_start_and_end_dates as (
-select 
-  concat(cast(year as {{ dbt.type_string() }} ),lpad(cast(month as {{ dbt.type_string() }}),2,'0')) as year_month
-, min(full_date) as month_start_date
-, max(full_date) as month_end_date
-from {{ ref('reference_data__calendar')}}
-group by 1
+  select
+      concat(cast(year as {{ dbt.type_string() }} ),lpad(cast(month as {{ dbt.type_string() }}),2,'0')) as year_month
+    , min(full_date) as month_start_date
+    , max(full_date) as month_end_date
+  from {{ ref('reference_data__calendar')}}
+  group by 1
 )
 
 select distinct
-  a.patient_id
-, year_month
-, a.payer
-, a.plan
-, data_source
-, '{{ var('tuva_last_run')}}' as tuva_last_run
+    a.patient_id
+  , year_month
+  , a.payer
+  , a.plan
+  , data_source
+  , '{{ var('tuva_last_run')}}' as tuva_last_run
 from {{ ref('core__eligibility') }} a
 inner join month_start_and_end_dates b
   on a.enrollment_start_date <= b.month_end_date
