0b11b0f5c4af62f8a2502d92f1eea3d3c7931e89
thutuva <101137626+thutuva@users.noreply.github.com>
2024-06-24 13:04:26 -0400

    updated logic that populates inferred claim start date or claim end date

--- models/claims_preprocessing/claims_enrollment/claims_enrollment__flag_claims_with_enrollment.sql
+++ models/claims_preprocessing/claims_enrollment/claims_enrollment__flag_claims_with_enrollment.sql
@@ -11,19 +11,17 @@ with claim_dates as(
         , patient_id
         , payer
         , plan
-        , coalesce(claim_start_date, admission_date, claim_end_date, discharge_date) as inferred_claim_start_date
-        , coalesce(claim_end_date, discharge_date, claim_start_date, admission_date) as inferred_claim_end_date
+        , coalesce(claim_line_start_date, claim_start_date, admission_date) as inferred_claim_start_date
+        , coalesce(claim_line_end_date, claim_end_date, discharge_date) as inferred_claim_end_date
         , case
-            when claim_start_date is not null then 'claim_start_date'
-            when claim_start_date is null and admission_date is not null then 'admission_date'
-            when claim_start_date is null and admission_date is null and claim_end_date is not null then 'claim_end_date'
-            when claim_start_date is null and admission_date is null and claim_end_date is null and discharge_date is not null then 'discharge_date'
+            when claim_line_start_date is not null then 'claim_line_start_date'
+            when claim_line_start_date is null and claim_start_date is not null then 'claim_start_date'
+            when claim_line_start_date is null and claim_start_date is null and admission_date is not null then 'admission_date'
         end as inferred_claim_start_column_used
         , case
-            when claim_end_date is not null then 'claim_end_date'
-            when claim_end_date is null and discharge_date is not null then 'discharge_date'
-            when claim_end_date is null and discharge_date is null and claim_start_date is not null then 'claim_start_date'
-            when claim_end_date is null and discharge_date is null and claim_start_date is null and admission_date is not null then 'admission_date'
+            when claim_line_end_date is not null then 'claim_line_end_date'
+            when claim_line_end_date is null and claim_end_date is not null then 'claim_end_date'
+            when claim_line_end_date is null and claim_end_date is null and discharge_date is not null then 'discharge_date'
         end as inferred_claim_end_column_used
     from {{ ref('normalized_input__medical_claim')}}
 )
