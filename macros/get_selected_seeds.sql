485eb1b0cb7bc8d7ebacc5f371d7ff04018b3ece
tuvaforrest <forrest@tuvahealth.com>
2024-07-02 11:09:22 -0400

    added config specific quoting

--- macros/get_selected_seeds.sql
+++ macros/get_selected_seeds.sql
@@ -6,8 +6,24 @@
     {% set ns = namespace(selected_seeds=[]) %}
 
     {% for node in nodes %}
-        {% if node.resource_type == 'seed' and node.unique_id in selected_nodes and node.package_name in['the_tuva_project','tuva'] %}
-            {% set fully_qualified_name = adapter.quote(node.database) ~ '.' ~ adapter.quote(node.schema) ~ '.' ~ adapter.quote(node.alias) %}
+        {% if node.resource_type == 'seed' and node.unique_id in selected_nodes and node.package_name in['the_tuva_project','tuva','integration_tests'] %}
+            {% if adapter.config.quoting.database %}
+                {% set db =adapter.quote(node.database) %}
+            {% else %}
+                {% set db = node.database %}
+            {% endif %}
+            {% if adapter.config.quoting.schema %}
+                {% set sch =adapter.quote(node.schema) %}
+            {% else %}
+                {% set sch = node.schema %}
+            {% endif %}
+            {% if adapter.config.quoting.identifier %}
+                {% set idf =adapter.quote(node.alias) %}
+            {% else %}
+                {% set idf = node.alias %}
+            {% endif %}
+            {% set fully_qualified_name = db ~ '.' ~ sch ~ '.' ~ idf %}
+
             {% do ns.selected_seeds.append(fully_qualified_name) %}
         {% endif %}
     {% endfor %}
