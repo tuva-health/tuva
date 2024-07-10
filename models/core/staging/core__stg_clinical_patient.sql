5dcc43dbd2f340bbdda4f1e71468b6e6e633f985
Tom Sherwood <tom@tuvahealth.com>
2024-07-10 16:26:30 -0400

    Take 4

--- models/core/staging/core__stg_clinical_patient.sql
+++ models/core/staging/core__stg_clinical_patient.sql
@@ -3,31 +3,24 @@
    )
 }}
 
-WITH base_patient_data AS (
-    SELECT
-        cast(patient_id as {{ dbt.type_string() }} ) as patient_id
-        , cast(first_name as {{ dbt.type_string() }} ) as first_name
-        , cast(last_name as {{ dbt.type_string() }} ) as last_name
-        , cast(sex as {{ dbt.type_string() }} ) as sex
-        , cast(race as {{ dbt.type_string() }} ) as race
-        , {{ try_to_cast_date('birth_date', 'YYYY-MM-DD') }} as birth_date
-        , {{ try_to_cast_date('death_date', 'YYYY-MM-DD') }} as death_date
-        , cast(death_flag as {{ dbt.type_int() }} ) as death_flag
-        , cast(social_security_number as {{ dbt.type_string() }} ) as social_security_number
-        , cast(address as {{ dbt.type_string() }} ) as address
-        , cast(city as {{ dbt.type_string() }} ) as city
-        , cast(state as {{ dbt.type_string() }} ) as state
-        , cast(zip_code as {{ dbt.type_string() }} ) as zip_code
-        , cast(county as {{ dbt.type_string() }} ) as county
-        , cast(latitude as {{ dbt.type_float() }} ) as latitude
-        , cast(longitude as {{ dbt.type_float() }} ) as longitude
-        , cast(data_source as {{ dbt.type_string() }} ) as data_source
-        , cast('{{ var('tuva_last_run')}}' as {{ dbt.type_timestamp() }} ) as tuva_last_run
-    FROM {{ ref('patient') }}
-)
-
-SELECT 
-    *
+SELECT
+    cast(patient_id as {{ dbt.type_string() }} ) as patient_id
+    , cast(first_name as {{ dbt.type_string() }} ) as first_name
+    , cast(last_name as {{ dbt.type_string() }} ) as last_name
+    , cast(sex as {{ dbt.type_string() }} ) as sex
+    , cast(race as {{ dbt.type_string() }} ) as race
+    , {{ try_to_cast_date('birth_date', 'YYYY-MM-DD') }} as birth_date
+    , {{ try_to_cast_date('death_date', 'YYYY-MM-DD') }} as death_date
+    , cast(death_flag as {{ dbt.type_int() }} ) as death_flag
+    , cast(social_security_number as {{ dbt.type_string() }} ) as social_security_number
+    , cast(address as {{ dbt.type_string() }} ) as address
+    , cast(city as {{ dbt.type_string() }} ) as city
+    , cast(state as {{ dbt.type_string() }} ) as state
+    , cast(zip_code as {{ dbt.type_string() }} ) as zip_code
+    , cast(county as {{ dbt.type_string() }} ) as county
+    , cast(latitude as {{ dbt.type_float() }} ) as latitude
+    , cast(longitude as {{ dbt.type_float() }} ) as longitude
+    , cast(data_source as {{ dbt.type_string() }} ) as data_source
     , cast(FLOOR({{ datediff("cast(birth_date as date)", "cast('" ~ var('tuva_last_run') ~ "' as date)", 'day') }} / 365) as {{ dbt.type_int() }}) AS age
     , cast(
         CASE
@@ -43,4 +36,5 @@ SELECT
             ELSE '90+'
         END as {{ dbt.type_string() }}
     ) AS age_group
-FROM base_patient_data
+    , cast('{{ var('tuva_last_run')}}' as {{ dbt.type_timestamp() }} ) as tuva_last_run
+FROM {{ ref('patient') }}
