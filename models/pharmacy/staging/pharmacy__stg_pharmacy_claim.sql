ed6ae64ad5d4bcb5fd9ef722028146b5bbb24d78
bradmontierth <72261515+bradmontierth@users.noreply.github.com>
2024-07-11 09:21:13 -0600

    fixing bug with pqi data type

--- models/pharmacy/staging/pharmacy__stg_pharmacy_claim.sql
+++ models/pharmacy/staging/pharmacy__stg_pharmacy_claim.sql
@@ -1,5 +1,5 @@
 {{ config(
-    enabled = var('pqi_enabled', var('claims_enabled', var('tuva_marts_enabled', False))) | as_bool
+    enabled = var('brand_generic_enabled', var('claims_enabled', var('tuva_marts_enabled', False))) | as_bool
 ) }}
 
 select 
