a63587888dca7aa83b9b848cf576ab6c3b99712d
bradmontierth <72261515+bradmontierth@users.noreply.github.com>
2024-07-15 16:45:59 -0600

    adding try to cast date for join to calendar table

--- models/data_quality/final/data_quality__quality_trend.sql
+++ models/data_quality/final/data_quality__quality_trend.sql
@@ -3,15 +3,16 @@
    )
 }}
 
+
 with cte as (
-select d.source_date as source_date_type
+select {{ try_to_cast_date('substring(d.source_date,1,10)' ) }} as source_date_type
     ,summary_sk
     ,SUM(CASE WHEN BUCKET_NAME = 'valid' THEN 1 ELSE 0 END) as VALID_NUM
     ,SUM(CASE WHEN BUCKET_NAME <> 'null' THEN 1 ELSE 0 END) as FILL_NUM
     ,COUNT(DRILL_DOWN_VALUE) as DENOM
 from {{ ref('data_quality__data_quality_detail') }} d
 group by
-    d.source_date
+    {{ try_to_cast_date('substring(d.source_date,1,10)' ) }}
     ,summary_sk
 
 )
@@ -22,9 +23,9 @@ select
     , sum(VALID_NUM) as VALID_NUM
     , sum(FILL_NUM) as FILL_NUM
     , sum(DENOM)  as DENOM
-    , '{{ var('tuva_last_run')}}' as tuva_last_run
 from cte
-left join {{ ref('reference_data__calendar') }} c on cte.source_date_type = c.full_date
+left join {{ ref('terminology__dqi_calendar') }} c on cte.source_date_type = c.full_date
 group by
       c.first_day_of_month
     , summary_sk
+
