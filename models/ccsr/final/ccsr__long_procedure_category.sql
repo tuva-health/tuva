1fab32127d266b1963e3347ea1b653e4575faa46
Sarah Morgan <sarah@tuvahealth.com>
2024-07-19 04:59:43 -0600

    Bug Fixes: Data Quality and CCSR (#548)
    * replace invalid function for bigquery
    
    * add claim_id to ccsr procedure models
    
    * remove unnecessary tests
    
    * reformat to match style guide and add claim_id
    
    * update dbt docs

--- models/ccsr/final/ccsr__long_procedure_category.sql
+++ models/ccsr/final/ccsr__long_procedure_category.sql
@@ -7,23 +7,26 @@ with procedure as (
     
     select * from {{ ref('ccsr__stg_core__procedure') }}
 
-), ccsr__procedure_category_map as (
+)
+
+, ccsr__procedure_category_map as (
 
     select * from {{ ref ('ccsr__procedure_category_map') }}
 
 )
 
 select distinct
-    procedure.encounter_id,
-    procedure.patient_id,
-    procedure.normalized_code,
-    ccsr__procedure_category_map.code_description,
-    ccsr__procedure_category_map.ccsr_parent_category,
-    ccsr__procedure_category_map.ccsr_category,
-    ccsr__procedure_category_map.ccsr_category_description,
-    ccsr__procedure_category_map.clinical_domain,
-    {{ var('prccsr_version') }} as prccsr_version,
-    '{{ var('tuva_last_run')}}' as tuva_last_run
+      procedure.encounter_id
+    , procedure.claim_id
+    , procedure.patient_id
+    , procedure.normalized_code
+    , ccsr__procedure_category_map.code_description
+    , ccsr__procedure_category_map.ccsr_parent_category
+    , ccsr__procedure_category_map.ccsr_category
+    , ccsr__procedure_category_map.ccsr_category_description
+    , ccsr__procedure_category_map.clinical_domain
+    , {{ var('prccsr_version') }} as prccsr_version
+    , '{{ var('tuva_last_run')}}' as tuva_last_run
 from procedure
 left join ccsr__procedure_category_map
     on procedure.normalized_code = ccsr__procedure_category_map.code
