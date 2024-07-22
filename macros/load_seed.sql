022032830c9c4113e61a5017cf377c535481a782
Justin Mills <justin.mills@waymarkcare.com>
2024-07-22 10:07:26 -0400

    Postgres implementation of the load_seed macro
    This relies on two optional variables set in your dbt project to
    override the S3 bucket name and optionally provide a prefix.
    
    This assumes you've created a pg-friendly format of the seed data ([this
    repo](https://github.com/waymark-care/tuva-postgres-seed-converter) can
    be used to generate one). This macro implementation also requires that
    you have setup your RDS cluster/instance with a IAM Role that has the
    proper privileges to access the S3 bucket where the seed data is stored.

--- macros/load_seed.sql
+++ macros/load_seed.sql
@@ -229,6 +229,56 @@ COPY_OPTIONS (
 
 
 
+-- postgres - note this requires some pre-work on your part you need to clone
+-- the data from the tuva public resource bucket to re-assemble it into a single
+-- file per seed - also ensure you've set the Content-Type system header on each
+-- to be gzip (https://stackoverflow.com/a/74439053) otherwise the extension
+-- won't know to decompress it.
+--
+-- TODO: revisit removing column list, I think it'll work fine with '' for cols
+{% macro postgres__load_seed(uri,pattern,compression,headers,null_marker) %}
+{%- set columns = adapter.get_columns_in_relation(this) -%}
+{%- set collist = [] -%}
+{%- for col in columns -%}
+  {%- do collist.append(col.name) -%}
+{%- endfor -%}
+{%- set cols = collist|join(",") -%}
+
+{%- set s3_bucket = var("tuva_seeds_s3_bucket", uri.split("/")[0]) -%}
+{%- set s3_key = uri.split("/")[1:]|join("/") + "/" + pattern + "_0.csv.gz" -%}
+{%- if var("tuva_seeds_s3_key_prefix", "") != "" -%}
+{%- set s3_key = var("tuva_seeds_s3_key_prefix") + "/" + s3_key -%}
+{%- endif -%}
+{%- set s3_region = "us-east-1" -%}
+{%- set options = ["(", "format csv", ", encoding ''utf8''"] -%}
+{%- do options.append(", null ''\\N''") if null_marker == true -%}
+{%- do options.append(")") -%}
+{%- set options_s = options | join("") -%}
+
+{% set sql %}
+SELECT aws_s3.table_import_from_s3(
+   '{{ this }}',
+   '{{ cols }}',
+   '{{ options_s }}',
+   aws_commons.create_s3_uri('{{s3_bucket}}', '{{s3_key}}', '{{s3_region}}')
+)
+{% endset %}
+
+{% call statement('postgressql',fetch_result=true) %}
+{{ sql }}
+{% endcall %}
+
+{% if execute %}
+{# debugging { log(sql, True)} #}
+{% set results = load_result('postgressql') %}
+{{ log("Loaded data from external s3 resource\n  loaded to: " ~ this ~ "\n  from: s3://" ~ s3_bucket ~ "/" ~ s3_key ,True) }}
+{# debugging { log(results, True) } #}
+{% endif %}
+
+{% endmacro %}
+
+
+
 {% macro default__load_seed(uri,pattern,compression,headers,null_marker) %}
 {% if execute %}
 {% do log('No adapter found, seed not loaded',info = True) %}
