b191cd81a91d54f18f91b22f2fcab75e6c8cbbf4
Tom Sherwood <tom@tuvahealth.com>
2024-07-31 12:10:43 -0400

    Switching to limit rows based on claim_id rather than claim_line_number

--- models/claims_preprocessing/encounters/staging/encounters__stg_medical_claim.sql
+++ models/claims_preprocessing/encounters/staging/encounters__stg_medical_claim.sql
@@ -4,6 +4,7 @@
 
 select
     m.apr_drg_code
+  , m.patient_id 
   , m.claim_id
   , m.claim_line_number
   , m.claim_id || cast(m.claim_line_number as {{dbt.type_string() }} ) as claim_line_id
@@ -39,8 +40,6 @@ from {{ ref('normalized_input__medical_claim') }} m
 inner join {{ ref('service_category__service_category_grouper') }} g on m.claim_id = g.claim_id
 and
 m.claim_line_number = g.claim_line_number
-and
-g.duplicate_row_number = 1
 left join {{ ref('ccsr__dxccsr_v2023_1_cleaned_map') }} dx on m.diagnosis_code_1 = dx.icd_10_cm_code
 left join {{ ref('terminology__provider') }} p on m.facility_id = p.npi
 left join {{ ref('terminology__ccs_services_procedures') }} c on m.hcpcs_code = c.hcpcs_code
@@ -49,4 +48,3 @@ left join {{ ref('terminology__ms_drg') }} drg on m.ms_drg_code = drg.ms_drg_cod
 left join {{ ref('terminology__revenue_center') }} r on m.revenue_center_code = r.revenue_center_code
 left join {{ ref('terminology__place_of_service') }} pos on m.place_of_service_code = pos.place_of_service_code
 left join {{ ref('terminology__bill_type') }} bt on m.bill_type_code = bt.bill_type_code
-
