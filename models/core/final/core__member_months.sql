a0af85cd574fa517bc98c066fd59614d0792e978
Eldon Jenkins <eldon@tuvahealth.com>
2024-08-02 14:18:40 -0600

    Swap out lpad for Fabric

--- models/core/final/core__member_months.sql
+++ models/core/final/core__member_months.sql
@@ -5,7 +5,11 @@
 
 with month_start_and_end_dates as (
   select
-      concat(cast(year as {{ dbt.type_string() }} ),lpad(cast(month as {{ dbt.type_string() }}),2,'0')) as year_month
+      {% if target.type == 'fabric' %}
+        concat(cast(year as {{ dbt.type_string() }} ),RIGHT(REPLICATE('0', 2) + cast(month as {{ dbt.type_string() }}), 2)) as year_month
+      {% else %}
+        concat(cast(year as {{ dbt.type_string() }} ),lpad(cast(month as {{ dbt.type_string() }}),2,'0')) as year_month
+      {% endif %}
     , min(full_date) as month_start_date
     , max(full_date) as month_end_date
   from {{ ref('reference_data__calendar')}}
