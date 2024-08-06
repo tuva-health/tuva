8295cd8a9b380ff948f2654078090c5e10ecf677
Tom Sherwood <tom@tuvahealth.com>
2024-08-06 16:28:07 -0400

    updating ref for rbcs crosswalk

--- models/claims_preprocessing/encounters/intermediate/office_visits/office_visits__int_office_visits_imaging.sql
+++ models/claims_preprocessing/encounters/intermediate/office_visits/office_visits__int_office_visits_imaging.sql
@@ -9,6 +9,6 @@ select
 from {{ ref('office_visits__int_office_visits') }} ov
 inner join {{ ref('encounters__stg_medical_claim') }} mc on mc.claim_id = ov.claim_id
     and mc.claim_line_number = ov.claim_line_number
-inner join rbetos__hcpcs_to_rbcs rbcs on mc.hcpcs_code = rbcs.hcpcs_cd
+inner join {{ ref('rbcs__hcpcs_to_rbcs') }} rbcs on mc.hcpcs_code = rbcs.hcpcs_cd
 where 1 = 1
     and rbcs.rbcs_cat_desc = 'Imaging'
