d890734d3eb6eb270a00977122cb7be9fdfc7d1f
bradmontierth <72261515+bradmontierth@users.noreply.github.com>
2024-08-15 16:55:02 -0600

    adding urgent care to encounter table

--- models/claims_preprocessing/encounters/final/urgent_care__encounter_grain.sql
+++ models/claims_preprocessing/encounters/final/urgent_care__encounter_grain.sql
@@ -3,13 +3,19 @@
    )
 }}
 
-with detail_values as (
+with encounter_date as (
+  select distinct old_encounter_id
+  ,start_date as encounter_start_date
+  from {{ ref('urgent_care__generate_encounter_id') }}
+)
+
+,detail_values as (
     select stg.*
     ,cli.encounter_id
     ,cli.old_encounter_id
-    ,ed.encounter_start_date
-    ,ed.encounter_end_date
     ,cli.encounter_type
+    ,d.encounter_start_date 
+    , row_number() over (partition by cli.encounter_id order by stg.claim_type, stg.start_date) as encounter_row_number --institutional then professional
     from  {{ ref('encounters__stg_medical_claim') }} stg
     inner join {{ ref('encounters__combined_claim_line_crosswalk') }} cli on stg.claim_id = cli.claim_id  
     and
@@ -18,44 +24,7 @@ with detail_values as (
     cli.encounter_type = 'urgent care'
     and
     cli.claim_line_attribution_number = 1
-    inner join {{ ref('urgent_care__start_end_dates') }} ed on cli.old_encounter_id = ed.encounter_id
-)
-
-,encounter_cross_walk as (
-  select distinct encounter_id
-  ,old_encounter_id
-  from detail_values
-)
-
-,first_last_inst_inst_values as (
-select *
-,row_number() over (partition by encounter_id order by start_date, claim_id) as first_num
-,row_number() over (partition by encounter_id order by end_date desc, claim_id) as last_num
-from detail_values
-where claim_type = 'institutional'
-)
-
-, institutional_claim_details as (
-    select distinct
-        d.encounter_id
-        , f.diagnosis_code_1
-        , f.diagnosis_code_type
-        , f.facility_id as facility_id
-        , f.ms_drg_code as ms_drg_code
-        , f.apr_drg_code as apr_drg_code
-        , f.admit_source_code as admit_source_code
-        , f.admit_type_code as admit_type_code
-        , l.discharge_disposition_code as discharge_disposition_code
-        , d.patient_id
-        , d.data_source
-    from detail_values d
-    inner join first_last_inst_inst_values f on d.encounter_id = f.encounter_id
-    and
-    f.first_num =1 
-    inner join first_last_inst_inst_values l on d.encounter_id = l.encounter_id
-    and
-    l.last_num = 1 
-
+    inner join encounter_date d on cli.old_encounter_id = d.old_encounter_id
 )
 
 , patient as (
@@ -83,66 +52,71 @@ group by encounter_id
 ,encounter_type -- not changing grain, but bringing into final
 )
 
-select
-  x.encounter_id
-, a.encounter_start_date
-, a.encounter_end_date
-, c.patient_id
+
+,highest_paid_diagnosis as 
+(
+  select encounter_id
+  , diagnosis_code_1
+  , diagnosis_code_type
+  , row_number() over (partition by encounter_id order by sum(paid_amount) desc ) as paid_order
+  , sum(paid_amount) as paid_amount
+  from detail_values
+  where diagnosis_code_1 is not null
+  group by diagnosis_code_1
+  , encounter_id
+  , diagnosis_code_type
+)
+
+,highest_paid_facility as 
+(
+  select encounter_id
+  , facility_id
+  , row_number() over (partition by encounter_id order by sum(paid_amount) desc ) as paid_order
+  , sum(paid_amount) as paid_amount
+  from detail_values
+  where facility_id is not null
+  group by 
+   encounter_id
+  , facility_id
+)
+
+
+select   d.encounter_id
+, d.encounter_start_date
+, d.patient_id
 , tot.encounter_type
-, {{ dbt.datediff("birth_date","encounter_end_date","day")}}/365 as admit_age
+, {{ dbt.datediff("birth_date","d.encounter_start_date","day")}}/365 as admit_age
 , e.gender
 , e.race
-, c.diagnosis_code_type as primary_diagnosis_code_type
-, c.diagnosis_code_1 as primary_diagnosis_code
+, hp.diagnosis_code_type as primary_diagnosis_code_type
+, hp.diagnosis_code_1 as primary_diagnosis_code
 , coalesce(icd10cm.long_description, icd9cm.long_description) as primary_diagnosis_description
-, c.facility_id as facility_id
+, hf.facility_id as facility_id
 , b.provider_organization_name as facility_name
-, c.ms_drg_code
-, j.ms_drg_description
-, j.medical_surgical
-, c.apr_drg_code
-, k.apr_drg_description
-, c.admit_source_code
-, h.admit_source_description
-, c.admit_type_code
-, i.admit_type_description
-, c.discharge_disposition_code
-, g.discharge_disposition_description
 , tot.total_paid_amount
 , tot.total_allowed_amount
 , tot.total_charge_amount
 , tot.claim_count
 , tot.inst_claim_count
 , tot.prof_claim_count
-, {{ dbt.datediff("a.encounter_start_date","a.encounter_end_date","day") }} as length_of_stay
-, case
-    when c.discharge_disposition_code = '20' then 1
-    else 0
-  end mortality_flag
-, c.data_source
+, d.data_source
 , '{{ var('tuva_last_run')}}' as tuva_last_run
-from {{ ref('urgent_care__start_end_dates') }} a
-inner join encounter_cross_walk x on a.encounter_id = x.old_encounter_id
-inner join total_amounts tot on x.encounter_id = tot.encounter_id
-left join institutional_claim_details c
-  on x.encounter_id = c.encounter_id
+from detail_values d
+inner join total_amounts tot on d.encounter_id = tot.encounter_id
+left join highest_paid_diagnosis hp on d.encounter_id = hp.encounter_id
+and
+hp.paid_order = 1
+left join highest_paid_facility hf on d.encounter_id = hf.encounter_id
+and
+hf.paid_order = 1
 left join patient e
-  on c.patient_id = e.patient_id
-left join {{ ref('terminology__provider') }} b
-  on c.facility_id = b.npi
-left join {{ ref('terminology__discharge_disposition') }} g
-  on c.discharge_disposition_code = g.discharge_disposition_code
-left join {{ ref('terminology__admit_source') }} h
-  on c.admit_source_code = h.admit_source_code
-left join {{ ref('terminology__admit_type') }} i
-  on c.admit_type_code = i.admit_type_code
-left join {{ ref('terminology__ms_drg') }} j
-  on c.ms_drg_code = j.ms_drg_code
-left join {{ ref('terminology__apr_drg') }} k
-  on c.apr_drg_code = k.apr_drg_code
-left join {{ ref('terminology__icd_10_cm')}} icd10cm
-  on c.diagnosis_code_1 = icd10cm.icd_10_cm
-  and c.diagnosis_code_type = 'icd-10-cm'
-left join {{ ref('terminology__icd_9_cm')}} icd9cm
-  on c.diagnosis_code_1 = icd9cm.icd_9_cm
-  and c.diagnosis_code_type = 'icd-9-cm'
+  on d.patient_id = e.patient_id
+left join dev_brad.terminology.provider b
+  on hf.facility_id = b.npi
+left join dev_brad.terminology.icd_10_cm icd10cm
+  on hp.diagnosis_code_1 = icd10cm.icd_10_cm
+  and hp.diagnosis_code_type = 'icd-10-cm'
+left join dev_brad.terminology.icd_9_cm icd9cm
+  on hp.diagnosis_code_1 = icd9cm.icd_9_cm
+  and hp.diagnosis_code_type = 'icd-9-cm'
+where d.encounter_row_number = 1
--- models/core/staging/core__stg_claims_encounter.sql
+++ models/core/staging/core__stg_claims_encounter.sql
@@ -36,6 +36,9 @@ select
     , cast(total_allowed_amount as {{ dbt.type_numeric() }} ) as allowed_amount
     , cast(total_charge_amount as {{ dbt.type_numeric() }} ) as charge_amount
     , cast(data_source as {{ dbt.type_string() }} ) as data_source
+    , cast(claim_count as  {{ dbt.type_int() }}) as claim_count
+    , cast(inst_claim_count as  {{ dbt.type_int() }}) as inst_claim_count
+    , cast(prof_claim_count as  {{ dbt.type_int() }}) as prof_claim_count
     , cast('{{ var('tuva_last_run')}}' as {{ dbt.type_timestamp() }} ) as  tuva_last_run
 from {{ ref('acute_inpatient__encounter_grain') }} 
 
@@ -69,6 +72,9 @@ select
     , cast(total_allowed_amount as {{ dbt.type_numeric() }} ) as allowed_amount
     , cast(total_charge_amount as {{ dbt.type_numeric() }} ) as charge_amount
     , cast(data_source as {{ dbt.type_string() }} ) as data_source
+        , cast(claim_count as  {{ dbt.type_int() }}) as claim_count
+    , cast(inst_claim_count as  {{ dbt.type_int() }}) as inst_claim_count
+    , cast(prof_claim_count as  {{ dbt.type_int() }}) as prof_claim_count
     , cast('{{ var('tuva_last_run')}}' as {{ dbt.type_timestamp() }} ) as  tuva_last_run
 from {{ ref('emergency_department__encounter_grain') }} 
 
@@ -102,6 +108,9 @@ select
     , cast(total_allowed_amount as {{ dbt.type_numeric() }} ) as allowed_amount
     , cast(total_charge_amount as {{ dbt.type_numeric() }} ) as charge_amount
     , cast(data_source as {{ dbt.type_string() }} ) as data_source
+        , cast(claim_count as  {{ dbt.type_int() }}) as claim_count
+    , cast(inst_claim_count as  {{ dbt.type_int() }}) as inst_claim_count
+    , cast(prof_claim_count as  {{ dbt.type_int() }}) as prof_claim_count
     , cast('{{ var('tuva_last_run')}}' as {{ dbt.type_timestamp() }} ) as  tuva_last_run
 from {{ ref('inpatient_hospice__encounter_grain') }} 
 
@@ -135,6 +144,9 @@ select
     , cast(total_allowed_amount as {{ dbt.type_numeric() }} ) as allowed_amount
     , cast(total_charge_amount as {{ dbt.type_numeric() }} ) as charge_amount
     , cast(data_source as {{ dbt.type_string() }} ) as data_source
+        , cast(claim_count as  {{ dbt.type_int() }}) as claim_count
+    , cast(inst_claim_count as  {{ dbt.type_int() }}) as inst_claim_count
+    , cast(prof_claim_count as  {{ dbt.type_int() }}) as prof_claim_count
     , cast('{{ var('tuva_last_run')}}' as {{ dbt.type_timestamp() }} ) as  tuva_last_run
 from {{ ref('inpatient_psych__encounter_grain') }} 
 
@@ -168,6 +180,9 @@ select
     , cast(total_allowed_amount as {{ dbt.type_numeric() }} ) as allowed_amount
     , cast(total_charge_amount as {{ dbt.type_numeric() }} ) as charge_amount
     , cast(data_source as {{ dbt.type_string() }} ) as data_source
+        , cast(claim_count as  {{ dbt.type_int() }}) as claim_count
+    , cast(inst_claim_count as  {{ dbt.type_int() }}) as inst_claim_count
+    , cast(prof_claim_count as  {{ dbt.type_int() }}) as prof_claim_count
     , cast('{{ var('tuva_last_run')}}' as {{ dbt.type_timestamp() }} ) as  tuva_last_run
 from {{ ref('inpatient_rehab__encounter_grain') }} 
 
@@ -201,6 +216,9 @@ select
     , cast(total_allowed_amount as {{ dbt.type_numeric() }} ) as allowed_amount
     , cast(total_charge_amount as {{ dbt.type_numeric() }} ) as charge_amount
     , cast(data_source as {{ dbt.type_string() }} ) as data_source
+        , cast(claim_count as  {{ dbt.type_int() }}) as claim_count
+    , cast(inst_claim_count as  {{ dbt.type_int() }}) as inst_claim_count
+    , cast(prof_claim_count as  {{ dbt.type_int() }}) as prof_claim_count
     , cast('{{ var('tuva_last_run')}}' as {{ dbt.type_timestamp() }} ) as  tuva_last_run
 from {{ ref('inpatient_snf__encounter_grain') }} 
 
@@ -234,5 +252,45 @@ select
     , cast(total_allowed_amount as {{ dbt.type_numeric() }} ) as allowed_amount
     , cast(total_charge_amount as {{ dbt.type_numeric() }} ) as charge_amount
     , cast(data_source as {{ dbt.type_string() }} ) as data_source
+        , cast(claim_count as  {{ dbt.type_int() }}) as claim_count
+    , cast(inst_claim_count as  {{ dbt.type_int() }}) as inst_claim_count
+    , cast(prof_claim_count as  {{ dbt.type_int() }}) as prof_claim_count
     , cast('{{ var('tuva_last_run')}}' as {{ dbt.type_timestamp() }} ) as  tuva_last_run
 from {{ ref('inpatient_substance_use__encounter_grain') }} 
+
+
+union all 
+
+select
+    cast(encounter_id as {{ dbt.type_string() }} ) as encounter_id
+    , cast(patient_id as {{ dbt.type_string() }} ) as patient_id
+    , cast(encounter_type as {{ dbt.type_string() }} ) as encounter_type
+    , {{ try_to_cast_date('encounter_start_date', 'YYYY-MM-DD') }} as encounter_start_date
+    , {{ try_to_cast_date('encounter_start_date', 'YYYY-MM-DD') }} as encounter_end_date
+    , cast(null as {{ dbt.type_int() }} ) as length_of_stay
+    , cast(null as {{ dbt.type_string() }} ) as admit_source_code
+    , cast(null as {{ dbt.type_string() }} ) as admit_source_description
+    , cast(null as {{ dbt.type_string() }} ) as admit_type_code
+    , cast(null as {{ dbt.type_string() }} ) as admit_type_description
+    , cast(null as {{ dbt.type_string() }} ) as discharge_disposition_code
+    , cast(null as {{ dbt.type_string() }} ) as discharge_disposition_description
+    , cast(null as {{ dbt.type_string() }} ) as attending_provider_id
+    , cast(null as {{ dbt.type_string() }} ) as attending_provider_name
+    , cast(facility_id as {{ dbt.type_string() }} ) as facility_id
+    , cast(facility_name as {{ dbt.type_string() }} ) as facility_name
+    , cast(primary_diagnosis_code_type as {{ dbt.type_string() }} ) as primary_diagnosis_code_type
+    , cast(primary_diagnosis_code as {{ dbt.type_string() }} ) as primary_diagnosis_code
+    , cast(primary_diagnosis_description as {{ dbt.type_string() }} ) as primary_diagnosis_description
+    , cast(null as {{ dbt.type_string() }} ) as ms_drg_code
+    , cast(null as {{ dbt.type_string() }} ) as ms_drg_description
+    , cast(null as {{ dbt.type_string() }} ) as apr_drg_code
+    , cast(null as {{ dbt.type_string() }} ) as apr_drg_description
+    , cast(total_paid_amount as {{ dbt.type_numeric() }} ) as  paid_amount
+    , cast(total_allowed_amount as {{ dbt.type_numeric() }} ) as allowed_amount
+    , cast(total_charge_amount as {{ dbt.type_numeric() }} ) as charge_amount
+    , cast(data_source as {{ dbt.type_string() }} ) as data_source
+    , cast(claim_count as  {{ dbt.type_int() }}) as claim_count
+    , cast(inst_claim_count as  {{ dbt.type_int() }}) as inst_claim_count
+    , cast(prof_claim_count as  {{ dbt.type_int() }}) as prof_claim_count
+    , cast('{{ var('tuva_last_run')}}' as {{ dbt.type_timestamp() }} ) as  tuva_last_run
+from {{ ref('urgent_care__encounter_grain') }} 
