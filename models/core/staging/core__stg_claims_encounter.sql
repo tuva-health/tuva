59639f830b35f4ed3dbd043f3a27b67368f4bd42
bradmontierth <72261515+bradmontierth@users.noreply.github.com>
2024-09-11 11:41:38 -0600

    fixing missing columns from clinical encounter

--- models/core/staging/core__stg_claims_encounter.sql
+++ models/core/staging/core__stg_claims_encounter.sql
@@ -13,82 +13,81 @@
 WITH base AS (
   {{ dbt_utils.union_relations(
     relations=[
-      ref('acute_inpatient__encounter_grain'),
-      ref('emergency_department__encounter_grain'),
-      ref('inpatient_hospice__encounter_grain'),
-      ref('inpatient_psych__encounter_grain'),
-      ref('inpatient_rehab__encounter_grain'),
-      ref('inpatient_snf__encounter_grain'),
-      ref('inpatient_substance_use__encounter_grain'),
-      ref('urgent_care__encounter_grain'),
-      ref('office_visit__encounter_grain'),
-      ref('outpatient_hospice__encounter_grain'),
-      ref('outpatient_hospital_or_clinic__encounter_grain'),
-      ref('outpatient_injections__encounter_grain'),
-      ref('outpatient_psych__encounter_grain'),
-      ref('outpatient_ptotst__encounter_grain'),
-      ref('outpatient_surgery__encounter_grain'),
-      ref('outpatient_radiology__encounter_grain'),
-      ref('outpatient_rehab__encounter_grain'),
-      ref('outpatient_substance_use__encounter_grain'),
-      ref('home_health__encounter_grain'),
-      ref('dialysis__encounter_grain'),
-      ref('asc__encounter_grain'),
-      ref('ambulance__encounter_grain'),
-      ref('dme__encounter_grain'),
-      ref('lab__encounter_grain'),
-      ref('orphaned_claim__encounter_grain'),
+      , ref('acute_inpatient__encounter_grain')
+      , ref('emergency_department__encounter_grain')
+      , ref('inpatient_hospice__encounter_grain')
+      , ref('inpatient_psych__encounter_grain')
+      , ref('inpatient_rehab__encounter_grain')
+      , ref('inpatient_snf__encounter_grain')
+      , ref('inpatient_substance_use__encounter_grain')
+      , ref('urgent_care__encounter_grain')
+      , ref('office_visit__encounter_grain')
+      , ref('outpatient_hospice__encounter_grain')
+      , ref('outpatient_hospital_or_clinic__encounter_grain')
+      , ref('outpatient_injections__encounter_grain')
+      , ref('outpatient_psych__encounter_grain')
+      , ref('outpatient_ptotst__encounter_grain')
+      , ref('outpatient_surgery__encounter_grain')
+      , ref('outpatient_radiology__encounter_grain')
+      , ref('outpatient_rehab__encounter_grain')
+      , ref('outpatient_substance_use__encounter_grain')
+      , ref('home_health__encounter_grain')
+      , ref('dialysis__encounter_grain')
+      , ref('asc__encounter_grain')
+      , ref('ambulance__encounter_grain')
+      , ref('dme__encounter_grain')
+      , ref('lab__encounter_grain')
+      , ref('orphaned_claim__encounter_grain')
       ],
     exclude=["_loaded_at"]
   ) }}
 )
 
 SELECT
-  cast(encounter_id as {{ dbt.type_string() }} ) as encounter_id,
-  cast(p.patient_id as {{ dbt.type_string() }} ) as patient_id,
-  cast(base.patient_data_source_id as {{ dbt.type_string() }} ) as patient_data_source_id,
-  cast(encounter_type as {{ dbt.type_string() }} ) as encounter_type,
-  cast(encounter_group as {{ dbt.type_string() }} ) as encounter_group,
-  {{ try_to_cast_date('encounter_start_date', 'YYYY-MM-DD') }} as encounter_start_date,
-  {{ try_to_cast_date('encounter_end_date', 'YYYY-MM-DD') }} as encounter_end_date,
-  cast(length_of_stay as {{ dbt.type_int() }} ) as length_of_stay,
-  cast(admit_source_code as {{ dbt.type_string() }} ) as admit_source_code,
-  cast(admit_source_description as {{ dbt.type_string() }} ) as admit_source_description,
-  cast(admit_type_code as {{ dbt.type_string() }} ) as admit_type_code,
-  cast(admit_type_description as {{ dbt.type_string() }} ) as admit_type_description,
-  cast(discharge_disposition_code as {{ dbt.type_string() }} ) as discharge_disposition_code,
-  cast(discharge_disposition_description as {{ dbt.type_string() }} ) as discharge_disposition_description,
-  cast(null as {{ dbt.type_string() }} ) as attending_provider_id,
-  cast(null as {{ dbt.type_string() }} ) as attending_provider_name,
-  cast(observation_flag as {{ dbt.type_int() }} ) as observation_flag,
-  cast(facility_id as {{ dbt.type_string() }} ) as facility_id,
-  cast(facility_name as {{ dbt.type_string() }} ) as facility_name,
-  cast(facility_type as {{ dbt.type_string() }} ) as facility_type,
-  cast(lab_flag as {{ dbt.type_int() }} ) as lab_flag,
-  cast(dme_flag as {{ dbt.type_int() }} ) as dme_flag,
-  cast(ambulance_flag as {{ dbt.type_int() }} ) as ambulance_flag,
-  cast(pharmacy_flag as {{ dbt.type_int() }} ) as pharmacy_flag,
-  cast(ed_flag as {{ dbt.type_int() }} ) as ed_flag,
-  cast(delivery_flag as {{ dbt.type_int() }} ) as delivery_flag,
-  cast(delivery_type as {{ dbt.type_string() }} ) as delivery_type,
-  cast(newborn_flag as {{ dbt.type_int() }} ) as newborn_flag,
-  cast(nicu_flag as {{ dbt.type_int() }} ) as nicu_flag,
-  cast(primary_diagnosis_code_type as {{ dbt.type_string() }} ) as primary_diagnosis_code_type,
-  cast(primary_diagnosis_code as {{ dbt.type_string() }} ) as primary_diagnosis_code,
-  cast(primary_diagnosis_description as {{ dbt.type_string() }} ) as primary_diagnosis_description,
-  cast(ms_drg_code as {{ dbt.type_string() }} ) as ms_drg_code,
-  cast(ms_drg_description as {{ dbt.type_string() }} ) as ms_drg_description,
-  cast(apr_drg_code as {{ dbt.type_string() }} ) as apr_drg_code,
-  cast(apr_drg_description as {{ dbt.type_string() }} ) as apr_drg_description,
-  cast(total_paid_amount as {{ dbt.type_numeric() }} ) as paid_amount,
-  cast(total_allowed_amount as {{ dbt.type_numeric() }} ) as allowed_amount,
-  cast(total_charge_amount as {{ dbt.type_numeric() }} ) as charge_amount,
-  cast(base.data_source as {{ dbt.type_string() }} ) as data_source,
-  cast(claim_count as  {{ dbt.type_int() }}) as claim_count,
-  cast(inst_claim_count as  {{ dbt.type_int() }}) as inst_claim_count,
-  cast(prof_claim_count as  {{ dbt.type_int() }}) as prof_claim_count,
-  cast(_dbt_source_relation  as  {{ dbt.type_string() }}) as source_model,
-  cast('{{ var('tuva_last_run')}}' as {{ dbt.type_timestamp() }} ) as  tuva_last_run
+    cast(encounter_id as {{ dbt.type_string() }} ) as encounter_id
+  , cast(p.patient_id as {{ dbt.type_string() }} ) as patient_id
+  , cast(encounter_type as {{ dbt.type_string() }} ) as encounter_type
+  , cast(encounter_group as {{ dbt.type_string() }} ) as encounter_group
+  , {{ try_to_cast_date('encounter_start_date' 'YYYY-MM-DD') }} as encounter_start_date
+  , {{ try_to_cast_date('encounter_end_date' 'YYYY-MM-DD') }} as encounter_end_date
+  , cast(length_of_stay as {{ dbt.type_int() }} ) as length_of_stay
+  , cast(admit_source_code as {{ dbt.type_string() }} ) as admit_source_code
+  , cast(admit_source_description as {{ dbt.type_string() }} ) as admit_source_description
+  , cast(admit_type_code as {{ dbt.type_string() }} ) as admit_type_code
+  , cast(admit_type_description as {{ dbt.type_string() }} ) as admit_type_description
+  , cast(discharge_disposition_code as {{ dbt.type_string() }} ) as discharge_disposition_code
+  , cast(discharge_disposition_description as {{ dbt.type_string() }} ) as discharge_disposition_description
+  , cast(null as {{ dbt.type_string() }} ) as attending_provider_id
+  , cast(null as {{ dbt.type_string() }} ) as attending_provider_name
+  , cast(observation_flag as {{ dbt.type_int() }} ) as observation_flag
+  , cast(facility_id as {{ dbt.type_string() }} ) as facility_id
+  , cast(facility_name as {{ dbt.type_string() }} ) as facility_name
+  , cast(facility_type as {{ dbt.type_string() }} ) as facility_type
+  , cast(lab_flag as {{ dbt.type_int() }} ) as lab_flag
+  , cast(dme_flag as {{ dbt.type_int() }} ) as dme_flag
+  , cast(ambulance_flag as {{ dbt.type_int() }} ) as ambulance_flag
+  , cast(pharmacy_flag as {{ dbt.type_int() }} ) as pharmacy_flag
+  , cast(ed_flag as {{ dbt.type_int() }} ) as ed_flag
+  , cast(delivery_flag as {{ dbt.type_int() }} ) as delivery_flag
+  , cast(delivery_type as {{ dbt.type_string() }} ) as delivery_type
+  , cast(newborn_flag as {{ dbt.type_int() }} ) as newborn_flag
+  , cast(nicu_flag as {{ dbt.type_int() }} ) as nicu_flag
+  , cast(primary_diagnosis_code_type as {{ dbt.type_string() }} ) as primary_diagnosis_code_type
+  , cast(primary_diagnosis_code as {{ dbt.type_string() }} ) as primary_diagnosis_code
+  , cast(primary_diagnosis_description as {{ dbt.type_string() }} ) as primary_diagnosis_description
+  , cast(ms_drg_code as {{ dbt.type_string() }} ) as ms_drg_code
+  , cast(ms_drg_description as {{ dbt.type_string() }} ) as ms_drg_description
+  , cast(apr_drg_code as {{ dbt.type_string() }} ) as apr_drg_code
+  , cast(apr_drg_description as {{ dbt.type_string() }} ) as apr_drg_description
+  , cast(total_paid_amount as {{ dbt.type_numeric() }} ) as paid_amount
+  , cast(total_allowed_amount as {{ dbt.type_numeric() }} ) as allowed_amount
+  , cast(total_charge_amount as {{ dbt.type_numeric() }} ) as charge_amount
+  , cast(base.data_source as {{ dbt.type_string() }} ) as data_source
+  , cast(claim_count as  {{ dbt.type_int() }}) as claim_count
+  , cast(inst_claim_count as  {{ dbt.type_int() }}) as inst_claim_count
+  , cast(prof_claim_count as  {{ dbt.type_int() }}) as prof_claim_count
+  , cast(_dbt_source_relation  as  {{ dbt.type_string() }}) as source_model
+  , cast('{{ var('tuva_last_run')}}' as {{ dbt.type_timestamp() }} ) as  tuva_last_run
 FROM base
 inner join {{ ref('encounters__patient_data_source_id') }} p on base.patient_data_source_id = p.patient_data_source_id
 
