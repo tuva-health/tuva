23dff7e47791973c9afa50b584102301458ed237
BijayaShres <bijaya.shrestha@maitriservices.com>
2024-09-12 09:51:19 +0545

    feat: denominator implemented

--- models/quality_measures/intermediate/adh-ras_medication_adherence_for_hypertension/quality_measures__int_adhras_denominator.sql
+++ models/quality_measures/intermediate/adh-ras_medication_adherence_for_hypertension/quality_measures__int_adhras_denominator.sql
@@ -52,6 +52,7 @@ with performance_period as (
           pc.patient_id
         , dispensing_date
         , days_supply
+        , ndc_code
     from pharmacy_claim as pc
     inner join visit_codes as vc
     on pc.ndc_code = vc.code
@@ -106,3 +107,88 @@ with performance_period as (
     where {{ datediff('first_dispensing_date', 'performance_period_end', 'day') }} > 89
 
 )
+
+
+, patient_with_more_than_1_prescriptions as (
+
+    select distinct
+      patient_id
+      from patient_with_row_number
+      where row_number = 2
+
+)
+
+, patient_with_mt_1_prescriptions_and_first_fill_gt_90 as (
+
+    select
+          ff.patient_id
+        , ff.dispensing_date
+        , ff.first_dispensing_date
+        , ff.days_supply
+    from patient_with_first_fill_greater_than_90 as ff
+    inner join patient_with_more_than_1_prescriptions as mt1
+    on ff.patient_id = mt1.patient_id
+     
+)
+
+, patient_with_age as (
+
+    select
+          pp.patient_id
+    from {{ref('quality_measures__stg_core__patient')}} as p
+    inner join patient_within_performance_period as pp
+    on p.patient_id = pp.patient_id
+    where (floor({{ datediff('birth_date', 'pp.performance_period_begin', 'hour') }} / 8760.0)) > 17
+
+)
+
+, qualifying_patients as (
+
+    select
+          pp90.patient_id
+        , pp90.dispensing_date
+        , pp90.first_dispensing_date
+        , pp90.days_supply
+        , pp.performance_period_begin
+        , pp.performance_period_end
+        , pp.measure_id
+        , pp.measure_name
+        , pp.measure_version
+        , 1 as denominator_flag
+    from patient_with_mt_1_prescriptions_and_first_fill_gt_90 as pp90
+    inner join patient_with_age as pa
+    on pp90.patient_id = pa.patient_id
+    cross join performance_period as pp
+
+)
+
+, add_data_types as (
+
+    select
+          cast(patient_id as {{ dbt.type_string() }}) as patient_id
+        , cast(dispensing_date as date) as dispensing_date
+        , cast(first_dispensing_date as date) as first_dispensing_date
+        , cast(days_supply as integer) as days_supply
+        , cast(performance_period_begin as date) as performance_period_begin
+        , cast(performance_period_end as date) as performance_period_end
+        , cast(measure_id as {{ dbt.type_string() }}) as measure_id
+        , cast(measure_name as {{ dbt.type_string() }}) as measure_name
+        , cast(measure_version as {{ dbt.type_string() }}) as measure_version
+        , cast(denominator_flag as integer) as denominator_flag
+    from qualifying_patients
+
+)
+
+select 
+      patient_id
+    , dispensing_date
+    , first_dispensing_date
+    , days_supply
+    , performance_period_begin
+    , performance_period_end
+    , measure_id
+    , measure_name
+    , measure_version
+    , denominator_flag
+    , '{{ var('tuva_last_run')}}' as tuva_last_run
+from add_data_types
