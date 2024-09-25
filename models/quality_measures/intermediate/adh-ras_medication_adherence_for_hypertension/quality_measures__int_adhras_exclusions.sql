21614b3344b0d888a1236021e33f6863319eee7b
BijayaShres <bijaya.shrestha@maitriservices.com>
2024-09-25 16:01:43 +0545

    fix: minor issues fixed

--- models/quality_measures/intermediate/adh-ras_medication_adherence_for_hypertension/quality_measures__int_adhras_exclusions.sql
+++ models/quality_measures/intermediate/adh-ras_medication_adherence_for_hypertension/quality_measures__int_adhras_exclusions.sql
@@ -31,23 +31,26 @@ with denominator as (
 
 , valid_hospice_palliative as (
 
-  select
-      patient_id
-    , exclusion_date
-    , exclusion_reason
-  from {{ref('quality_measures__int_shared_exclusions_hospice_palliative')}}
-  where exclusion_date between {{ performance_period_begin }} and {{ performance_period_end }}
+    select
+        patient_id
+      , exclusion_date
+      , exclusion_reason
+    from {{ ref('quality_measures__int_shared_exclusions_hospice_palliative') }}
+    where exclusion_date between {{ performance_period_begin }} and {{ performance_period_end }}
 
 )
 
-, esrd_codes as (
+, codes as (
 
     select
             code
           , code_system
           , concept_name
     from {{ ref('quality_measures__value_sets') }}
-    where concept_name = 'PQA ESRD'
+    where concept_name in (
+            'PQA ESRD'
+          , 'PQA Sacubitril Valsartan Medications'
+        )
 
 )
 
@@ -56,24 +59,13 @@ with denominator as (
     select
           condition.patient_id
         , condition.recorded_date as exclusion_date
-        , esrd_codes.concept_name as exclusion_reason
+        , codes.concept_name as exclusion_reason
     from {{ ref('quality_measures__stg_core__condition') }} as condition
-    inner join esrd_codes 
-      on coalesce(condition.normalized_code, condition.source_code) = esrd_codes.code 
-        and coalesce(condition.normalized_code_type, condition.source_code_type) = esrd_codes.code_system
+    inner join codes 
+      on coalesce(condition.normalized_code, condition.source_code) = codes.code 
+        and coalesce(condition.normalized_code_type, condition.source_code_type) = codes.code_system
     where condition.recorded_date between {{ performance_period_begin }} and {{ performance_period_end }}
 
-)
-
-, sacubitril_codes as (
-
-    select
-          code
-        , code_system
-        , concept_name
-    from {{ ref('quality_measures__value_sets') }}
-    where concept_name = 'PQA Sacubitril Valsartan Medications'
-    
 )
 
 , sacubitril_pharmacy_claim as (
@@ -81,43 +73,11 @@ with denominator as (
     select
           pharmacy_claim.patient_id
         , pharmacy_claim.dispensing_date as exclusion_date
-        , sacubitril_codes.concept_name as exclusion_reason
+        , codes.concept_name as exclusion_reason
     from {{ ref('quality_measures__stg_pharmacy_claim') }} as pharmacy_claim
-    inner join sacubitril_codes
-      on pharmacy_claim.ndc_code = sacubitril_codes.code 
+    inner join codes
+      on pharmacy_claim.ndc_code = codes.code 
     where pharmacy_claim.dispensing_date between {{ performance_period_begin }} and {{ performance_period_end }}
-
-)
-
-, sacubitril_medication as (
-
-    select
-          medication.patient_id
-        , medication.dispensing_date as exclusion_date
-        , sacubitril_codes.concept_name as exclusion_reason
-    from {{ ref('quality_measures__stg_core__medication') }} as medication
-    inner join sacubitril_codes
-      on medication.source_code = sacubitril_codes.code 
-        and medication.source_code_type = sacubitril_codes.code_system
-    where medication.dispensing_date between {{ performance_period_begin }} and {{ performance_period_end }}
-
-)
-
-, valid_sacubitril as (
-
-    select 
-          patient_id
-        , exclusion_date
-        , exclusion_reason
-    from sacubitril_pharmacy_claim
-
-    union all
-
-    select 
-          patient_id
-        , exclusion_date
-        , exclusion_reason
-    from sacubitril_medication
 
 )
 
@@ -143,7 +103,7 @@ with denominator as (
           patient_id
         , exclusion_date
         , exclusion_reason 
-    from valid_sacubitril
+    from sacubitril_pharmacy_claim
 
 )
 
@@ -156,17 +116,18 @@ with denominator as (
     from exclusions
     inner join denominator
       on exclusions.patient_id = denominator.patient_id
+
 )
 
 , add_data_types as (
 
-  select
-      distinct
-          cast(patient_id as {{ dbt.type_string() }}) as patient_id
-        , cast(exclusion_date as date) as exclusion_date
-        , cast(exclusion_reason as {{ dbt.type_string() }}) as exclusion_reason
-        , 1 as exclusion_flag
-  from measure_exclusions
+    select
+        distinct
+            cast(patient_id as {{ dbt.type_string() }}) as patient_id
+          , cast(exclusion_date as date) as exclusion_date
+          , cast(exclusion_reason as {{ dbt.type_string() }}) as exclusion_reason
+          , 1 as exclusion_flag
+    from measure_exclusions
 
 )
 
@@ -174,5 +135,6 @@ select
       patient_id
     , exclusion_date
     , exclusion_reason
-    , exclusion_flag   
+    , exclusion_flag
+    , '{{ var('tuva_last_run')}}' as tuva_last_run 
 from add_data_types
