8d9e63b2b24c993904b6ec7f1c109c0825b742e5
thutuva <101137626+thutuva@users.noreply.github.com>
2024-10-04 10:34:42 -0400

    edited query for BQ

--- models/data_quality/intermediate/data_quality__dx_and_px.sql
+++ models/data_quality/intermediate/data_quality__dx_and_px.sql
@@ -1,17 +1,17 @@
 {{ config(
-     enabled = var('insights_enabled',var('claims_enabled',var('tuva_marts_enabled',False))) | as_bool
+     enabled = var('data_quality_enabled',var('claims_enabled',var('tuva_marts_enabled',False))) | as_bool
    )
 }}
 
 with total_claims as(
     select
-        count(claim_id) as total_claims
-    from core.medical_claim
+        cast(count(claim_id) as {{ dbt.type_int() }} ) as total_claims
+    from {{ ref('core__medical_claim') }}
 )
 , claims_with_primary_dx as(
     select distinct
         claim_id
-    from core.condition
+    from {{ ref('core__condition') }}
     where condition_rank = 1
 
 )
@@ -19,23 +19,25 @@ with total_claims as(
 , claims_with_secondary_dx as(
     select
     total_claims
-    , secondary_dx_claim_count
-    , secondary_dx_claim_count/total_claims * 100 as percent_claim_with_secondar_dx
+    , cast(secondary_dx_claim_count as {{ dbt.type_int() }} ) as secondary_dx_claim_count
+    , (cast(secondary_dx_claim_count as {{ dbt.type_int() }} ) / cast(total_claims as {{ dbt.type_int() }} )) * 100 as percent_claim_with_secondar_dx
     , 'Percent of claims with secondary diagnosis' as data_quality_check
     from
     (
     select
-        count(distinct claim_id) as secondary_dx_claim_count
-    from core.condition
+        tc.total_claims
+        , cast(count(distinct claim_id) as {{ dbt.type_int() }} ) as secondary_dx_claim_count
+    from {{ ref('core__condition') }}
+    cross join total_claims tc
     where condition_rank >= 2
-    )
-    cross join total_claims
+    group by total_claims
+    ) x
 )
 , missing_primary_dx as (
     select
         'missing primary diagnosis' as data_quality_check
-        , count(*)
-    from core.medical_claim m
+        , cast(count(*) as {{ dbt.type_int() }} ) as missing_primary_dx_count
+    from {{ ref('core__medical_claim') }} m
     right join claims_with_primary_dx pdx
         on m.claim_id = pdx.claim_id
     where m.claim_id is null
@@ -43,8 +45,8 @@ with total_claims as(
 , invalid_primary_dx as(
     select
         'invalid primary diagnosis' as data_quality_check
-        , count(*)
-    from core.condition
+        , cast(count(*) as {{ dbt.type_int() }} ) as invalid_primary_dx_count
+    from {{ ref('core__condition') }}
     where condition_rank = 1
     and normalized_code is null
 )
@@ -52,13 +54,13 @@ with total_claims as(
 
     select
         'multiple primary diagnosis' as data_quality_check
-        , count(*)
+        , cast(count(*) as {{ dbt.type_int() }} ) as multiple_primary_dx_count
     from
         (
             select
                 claim_id
                 , row_number() over (partition by claim_id order by claim_id) as row_nbr
-            from core.condition
+            from {{ ref('core__condition') }}
             where condition_rank = 1
             group by
                 claim_id
@@ -69,16 +71,16 @@ with total_claims as(
 , invalid_secondary_dx as(
     select
         'multiple primary diagnosis' as data_quality_check
-        , count(*)
-    from core.condition
+        , cast(count(*) as {{ dbt.type_int() }} ) as invalid_secondary_dx_count
+    from {{ ref('core__condition') }}
     where condition_rank = 2
     and normalized_code is null
 )
 , invalid_procedure as(
     select
         'invalid procedure' as data_quality_check
-        , count(*)
-    from core.procedure
+        , cast(count(*) as {{ dbt.type_int() }} ) as invalid_px_count
+    from {{ ref('core__procedure') }}
     where normalized_code is null
 )
 
