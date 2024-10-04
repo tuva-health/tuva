f01a97bcdbfb61a8bc35529c5cf1c46220222140
Coco Zuloaga <coco@tuvahealth.com>
2024-10-04 13:39:30 -0600

    changes to pmpm prep

--- models/core/final/core__member_months.sql
+++ models/core/final/core__member_months.sql
@@ -3,6 +3,8 @@
    )
 }}
 
+
+
 with month_start_and_end_dates as (
   select
     {{ dbt.concat(["year",
@@ -11,8 +13,10 @@ with month_start_and_end_dates as (
     , max(full_date) as month_end_date
   from {{ ref('reference_data__calendar')}}
   group by year, month, year_month
-)
+),
 
+
+final_before_attribution_fields as (
 select distinct
     a.patient_id
   , year_month
@@ -24,3 +28,36 @@ from {{ ref('core__eligibility') }} a
 inner join month_start_and_end_dates b
   on a.enrollment_start_date <= b.month_end_date
   and a.enrollment_end_date >= b.month_start_date
+),
+
+
+add_attribution_fields as (
+select
+    a.patient_id
+  , a.year_month
+  , a.payer
+  , a.{{ quote_column('plan') }}
+  , a.data_source
+  , a.tuva_last_run
+  
+  , b.payer_attributed_provider
+  , b.payer_attributed_provider_practice
+  , b.payer_attributed_provider_organization
+  , b.payer_attributed_provider_lob
+  , b.custom_attributed_provider
+  , b.custom_attributed_provider_practice
+  , b.custom_attributed_provider_organization
+  , b.custom_attributed_provider_lob
+
+from final_before_attribution_fields a
+left join {{ ref('financial_pmpm__stg_provider_attribution') }} b
+on a.patient_id = b.patient_id
+and a.year_month = b.year_month
+and a.payer = b.payer
+and a.{{ quote_column('plan') }} = b.{{ quote_column('plan') }}
+and a.data_source = b.data_source
+)
+
+
+select *
+from add_attribution_fields
