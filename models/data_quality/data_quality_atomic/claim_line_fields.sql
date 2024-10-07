21e059f37c7b9b3f812bf1614f5e8c2c29c6cea7
Hunter <hunter@gooddata.studio>
2024-10-07 07:08:32 -0700

    fixes to claim_line check

--- models/data_quality/data_quality_atomic/claim_line_fields.sql
+++ models/data_quality/data_quality_atomic/claim_line_fields.sql
@@ -10,11 +10,11 @@ select
     {% if target.type == 'fabric' %}
     , case when revenue_center_code is null then 0 when strlen(revenue_center_code) != 2 then 1 else 0 end as revenue_center_code_invalid 
     , case when place_of_service_code is null then 0 when strlen(place_of_service_code) != 2 then 1 else 0 end as place_of_service_code_invalid
-    , case when is null then 0 when strlen(hcpcs_code) != 5 then 1 else 0 end as hcpcs_code_invalid 
+    , case when hcpcs_code is null then 0 when strlen(hcpcs_code) != 5 then 1 else 0 end as hcpcs_code_invalid 
     {% else %}
     , case when revenue_center_code is null then 0 when length(revenue_center_code) != 2 then 1 else 0 end as revenue_center_code_invalid 
     , case when place_of_service_code is null then 0 when length(place_of_service_code) != 2 then 1 else 0 end as place_of_service_code_invalid
-    , case when is null then 0 when length(hcpcs_code) != 5 then 1 else 0 end as hcpcs_code_invalid 
+    , case when hcpcs_code is null then 0 when length(hcpcs_code) != 5 then 1 else 0 end as hcpcs_code_invalid 
     , case when revenue_center_code is null then 1 else 0 end as revenue_center_code_missing
     , case when place_of_service_code is null then 1 else 0 end as place_of_service_code_missing 
     , case when hcpcs_code is null then 1 else 0 end as hcpcs_code_missing
@@ -22,7 +22,7 @@ from
     {{ref('core__medical_claim')}}
 ) 
 
-, medical_claim_missing AS (
+, medical_claim_missing_or_invald_prep AS (
 select 
     claim_id
     , sum(revenue_center_code_missing) AS revenue_center_code_missing 
@@ -37,53 +37,65 @@ group by
     claim_id 
 )
 
+, medical_claim_missing_or_invalid as (
+select 
+claim_id
+, case when revenue_center_code_missing > 0 then 1 else 0 end as revenue_center_code_missing
+, case when place_of_service_code_missing > 0 then 1 else 0 end as place_of_service_code_missing
+, case when hcpcs_code_missing > 0 then 1 else 0 end as hcpcs_code_missing
+, case when revenue_center_code_invalid > 0 then 1 else 0 end as revenue_center_code_invalid
+, case when place_of_service_code_invalid > 0 then 1 else 0 end as place_of_service_code_invalid
+, case when hcpcs_code_invalid > 0 then 1 else 0 end as hcpcs_code_invalid
+from 
+medical_claim_missing_or_invald_prep
+)
+
 , final as (
 select 
     coalesce(sum(revenue_center_code_missing), 0) AS result
-    , 'revenue center code missing' AS data_quality_check
+    , 'medical claim revenue_center_code missing' AS data_quality_check
 from 
-    medical_claim_missing
+    medical_claim_missing_or_invalid
 
 union all  
 
 select 
     coalesce(sum(place_of_service_code_missing), 0) AS result
-    , 'place of service code missing' AS data_quality_check
+    , 'medical claim place_of_service code missing' AS data_quality_check
 from 
-    medical_claim_missing
+    medical_claim_missing_or_invalid
 
 union all 
 
 select 
     coalesce(sum(hcpcs_code_missing), 0) AS result
-    , 'medical claim hcpcs code missing' AS data_quality_check
+    , 'medical claim hcpcs_code missing' AS data_quality_check
 from 
-    medical_claim_missing
+    medical_claim_missing_or_invalid
 
 union all 
 
 select 
     coalesce(sum(revenue_center_code_invalid), 0) AS result
-    , 'medical claim revenue center code invalid' AS data_quality_check
+    , 'medical claim revenue_center_code invalid' AS data_quality_check
 from 
-    medical_claim_missing
+    medical_claim_missing_or_invalid
 
 union all 
 
 select 
     coalesce(sum(place_of_service_code_invalid), 0) AS result
-    , 'medical claim place of service code invalid' AS data_quality_check
+    , 'medical claim place_of_service code invalid' AS data_quality_check
 from 
-    medical_claim_missing
+    medical_claim_missing_or_invalid
 
 union all 
 
 select 
     coalesce(sum(hcpcs_code_invalid), 0) AS result
-    , 'medical claim hcpcs code invalid' AS data_quality_check
+    , 'medical claim hcpcs_code invalid' AS data_quality_check
 from 
-    medical_claim_missing
-
+    medical_claim_missing_or_invalid
 ) 
 
 select 
