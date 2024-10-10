f0fb7e6725466c8090784e1dcbb250b02e7449a6
thutuva <101137626+thutuva@users.noreply.github.com>
2024-10-10 16:09:46 -0400

    edited data_loss to add counts for eligibility spans and member months

--- models/data_quality/data_quality__data_loss.sql
+++ models/data_quality/data_quality__data_loss.sql
@@ -26,10 +26,40 @@ with input_medical as (
 ,input_eligibility as (
   select
     'eligibility' as table_name,
-    count(distinct patient_id) as patient_count
+    count(distinct patient_id) as patient_count,
+    count(distinct {{ dbt.concat([
+                      "member_id",
+                      "'-'",
+                      "enrollment_start_date",
+                      "'-'",
+                      "enrollment_end_date",
+                      "'-'",
+                      "payer",
+                      "'-'",
+                      "plan",
+        ]) }}) as span_count
   from {{ ref('eligibility') }}
 )
 
+, month_start_and_end_dates as (
+  select
+    {{ dbt.concat(["year",
+                  dbt.right(dbt.concat(["'0'", "month"]), 2)]) }} as year_month
+    , min(full_date) as month_start_date
+    , max(full_date) as month_end_date
+  from {{ ref('reference_data__calendar')}}
+  group by year, month, year_month
+)
+, input_member_months as (
+  select
+    'eligibility' as table_name
+    , count(*) as member_month_count
+  from {{ ref('eligibility') }} e
+  inner join month_start_and_end_dates d
+    on e.enrollment_start_date <= d.month_end_date
+    and e.enrollment_end_date >= d.month_start_date
+
+)
 -- Core layer CTEs
 ,core_medical as (
   select
@@ -54,7 +84,14 @@ with input_medical as (
 ,core_eligibility as (
   select
     'eligibility' as table_name,
-    count(distinct patient_id) as patient_count
+    count(distinct patient_id) as patient_count,
+    count(*) as span_count
+  from {{ ref('core__eligibility') }}
+)
+,core_member_months as (
+  select
+    'eligibility' as table_name,
+    count(*) as member_month_count
   from {{ ref('core__member_months') }}
 )
 
@@ -177,5 +214,27 @@ select
   input.patient_count as input_layer_value,
   core.patient_count as core_value
 from input_eligibility input
+join input_eligibility core
+  on input.table_name = core.table_name
+
+union all
+
+select
+  input.table_name,
+  'Total Unique Eligibility Spans' as metric,
+  input.span_count as input_layer_value,
+  core.span_count as core_value
+from input_eligibility input
 join core_eligibility core
+  on input.table_name = core.table_name
+
+union all
+
+select
+  input.table_name,
+  'Total Member Months' as metric,
+  input.member_month_count as input_layer_value,
+  core.member_month_count as core_value
+from input_member_months input
+join core_member_months core
   on input.table_name = core.table_name
