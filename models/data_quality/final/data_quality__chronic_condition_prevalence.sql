8d5ff972aa6965ffb048a246a7e8224c9d432c6f
Tom Sherwood <tom@tuvahealth.com>
2024-10-15 15:45:59 -0400

    Updating final sql to get reference values

--- models/data_quality/final/data_quality__chronic_condition_prevalence.sql
+++ models/data_quality/final/data_quality__chronic_condition_prevalence.sql
@@ -10,13 +10,26 @@ with condition_counts as (
         count(distinct patient_id) as total_distinct_patients
     from {{ ref('core__member_months') }} -- Using member months as the denominator as there could be members without conditions
 )
-
+, results as (
 select
     conditions.condition
     , conditions.patients
     , (conditions.patients / total_patients.total_distinct_patients) * 100 as percent_of_total
-    , null as medicare_benchmark
-    , '{{ var('tuva_last_run') }}' as tuva_last_run
+    , ROW_NUMBER() OVER (ORDER BY (conditions.patients / total_distinct_patients) * 100 DESC) AS condition_rank
 from condition_counts as conditions
 cross join total_patients
-order by conditions.condition
+)
+
+select
+    results.condition
+    , results.patients
+    , results.condition_rank
+    , results.percent_of_total
+    , ref_data.analytics_value as percent_of_lds_total
+    , ref_data.rank as lds_condition_rank
+    , '{{ var('tuva_last_run') }}' as tuva_last_run
+from results
+full outer join {{ ref('data_quality__medicare_reference_data') }} as ref_data on results.condition = ref_data.analytics_measure
+where ref_data.analytics_concept = 'Chronic Condition'
+order by
+    results.condition_rank
