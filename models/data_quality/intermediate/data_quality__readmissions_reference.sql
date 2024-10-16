7c3c397bf85946ad530a0c542980b1f12519e4a3
bradmontierth <72261515+bradmontierth@users.noreply.github.com>
2024-10-16 09:38:07 -0600

    creating 2 union output tables for analytics checks

--- models/data_quality/intermediate/data_quality__readmissions_reference.sql
+++ models/data_quality/intermediate/data_quality__readmissions_reference.sql
@@ -14,7 +14,7 @@ select cte.analytics_measure
 ,cte.data_source_value
 ,m.analytics_value
 from cte
-left join {{ ref('data_quality__reference_mart_analytics') }} m on r.analytics_measure = m.analytics_measure
+left join {{ ref('data_quality__reference_mart_analytics') }} m on cte.analytics_measure = m.analytics_measure
 
 
 
