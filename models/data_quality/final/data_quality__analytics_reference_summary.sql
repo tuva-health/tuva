610d29d36514d8c1dff5f6908d3fd97188bfc959
Tom Sherwood <tom@tuvahealth.com>
2024-10-17 14:14:01 -0400

    service categories pmpm and trend

--- models/data_quality/final/data_quality__analytics_reference_summary.sql
+++ models/data_quality/final/data_quality__analytics_reference_summary.sql
@@ -14,6 +14,7 @@ with unioned_data as (
         , ref('data_quality__acute_inpatient_reference')
         , ref('data_quality__acute_inpatient_prevelence')
         , ref('data_quality__encounters_cost_and_utilization')
+        , ref('data_quality__service_categories_pmpm')
       ],
       exclude=["_loaded_at"]
   ) }}
