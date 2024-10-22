0395c522649fc9b16f9a5eee9f0b4bf4c090eea3
bradmontierth <72261515+bradmontierth@users.noreply.github.com>
2024-10-22 14:53:21 -0600

    adding generics

--- models/data_quality/dqi/mart_review/final/mart_review__generics.sql
+++ models/data_quality/dqi/mart_review/final/mart_review__generics.sql
@@ -1,8 +1,6 @@
 {{ config(
-     enabled = var('claims_enabled',var('tuva_marts_enabled',False))
- | as_bool
-   )
-}}
+    enabled = var('claims_enabled', var('tuva_marts_enabled', False)) | as_bool
+) }}
 
 select *
 from {{ ref('pharmacy__generic_available_list') }}
