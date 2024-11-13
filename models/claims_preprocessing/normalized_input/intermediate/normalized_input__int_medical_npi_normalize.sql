a50814ceff4071634b54424087f740861110f97b
bradmontierth <72261515+bradmontierth@users.noreply.github.com>
2024-11-13 15:26:13 -0700

    fix concat with null behavior on fabric

--- models/claims_preprocessing/normalized_input/intermediate/normalized_input__int_medical_npi_normalize.sql
+++ models/claims_preprocessing/normalized_input/intermediate/normalized_input__int_medical_npi_normalize.sql
@@ -4,26 +4,40 @@
    )
 }}
 
-
 select distinct
-  med.claim_id
+    med.claim_id
   , med.claim_line_number
   , med.claim_type
   , med.data_source
   , rend_prov.npi as normalized_rendering_npi
-  , cast(coalesce({{ dbt.concat(["rend_prov.provider_last_name", "', '",  "rend_prov.provider_first_name"]) }}, rend_prov.provider_organization_name) as {{ dbt.type_string() }} ) as normalized_rendering_name
+  , case 
+      when rend_prov.entity_type_code = 1 then 
+        cast({{ dbt.concat(["rend_prov.provider_last_name", "', '", "rend_prov.provider_first_name"]) }} as {{ dbt.type_string() }})
+      else 
+        cast(rend_prov.provider_organization_name as {{ dbt.type_string() }})
+    end as normalized_rendering_name
   , bill_prov.npi as normalized_billing_npi
-  , cast(coalesce({{ dbt.concat(["bill_prov.provider_last_name", "', '", "bill_prov.provider_first_name"]) }}, bill_prov.provider_organization_name) as {{ dbt.type_string() }} ) as normalized_billing_name
+  , case 
+      when bill_prov.entity_type_code = 1 then
+        cast({{ dbt.concat(["bill_prov.provider_last_name", "', '", "bill_prov.provider_first_name"]) }} as {{ dbt.type_string() }})
+      else 
+        cast(bill_prov.provider_organization_name as {{ dbt.type_string() }})
+    end as normalized_billing_name
   , fac_prov.npi as normalized_facility_npi
-  , cast(coalesce({{ dbt.concat(["fac_prov.provider_last_name", "', '", "fac_prov.provider_first_name"]) }}, fac_prov.provider_organization_name) as {{ dbt.type_string() }} ) as normalized_facility_name
-  , '{{ var('tuva_last_run')}}' as tuva_last_run
-from {{ ref('normalized_input__stg_medical_claim') }} med
-left join {{ ref('terminology__provider') }} rend_prov
-    on med.rendering_npi = rend_prov.npi
-    and rend_prov.entity_type_description = 'Individual'
-left join {{ ref('terminology__provider') }} bill_prov
-    on med.billing_npi = bill_prov.npi
-left join {{ ref('terminology__provider') }} fac_prov
-    on med.facility_npi = fac_prov.npi
-    and fac_prov.entity_type_description = 'Organization'
-    and med.claim_type = 'institutional'
+  , case 
+      when fac_prov.entity_type_code = 1 then
+        cast({{ dbt.concat(["fac_prov.provider_last_name", "', '", "fac_prov.provider_first_name"]) }} as {{ dbt.type_string() }})
+      else 
+        cast(fac_prov.provider_organization_name as {{ dbt.type_string() }})
+    end as normalized_facility_name
+  , '{{ var('tuva_last_run') }}' as tuva_last_run
+from {{ ref('normalized_input__stg_medical_claim') }} as med
+left join {{ ref('terminology__provider') }} as rend_prov
+  on med.rendering_npi = rend_prov.npi
+  and rend_prov.entity_type_description = 'Individual'
+left join {{ ref('terminology__provider') }} as bill_prov
+  on med.billing_npi = bill_prov.npi
+left join {{ ref('terminology__provider') }} as fac_prov
+  on med.facility_npi = fac_prov.npi
+  and fac_prov.entity_type_description = 'Organization'
+  and med.claim_type = 'institutional'
