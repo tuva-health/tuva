64a1aa4db4a0d53d5198753155eebb29fd5915bb
thutva <thuxuan@tuvahealth.com>
2024-11-25 11:45:44 -0500

    add summary counts

---
+++ models/data_quality/audit_dashboard/data_quality__claim_summary_counts.sql
@@ -0,0 +1,35 @@
+select
+    'medical claim total rows' as data_quality_check
+    , count(*) as result_count
+from {{ ref('medical_claim') }}
+
+union all
+
+select
+    'medical claim institutional total rows' as data_quality_check
+    , count(*) as result_count
+from {{ ref('medical_claim') }}
+where claim_type = 'institutional'
+
+union all
+
+select
+    'medical claim professional total rows' as data_quality_check
+    , count(*) as result_count
+from {{ ref('medical_claim') }}
+where claim_type = 'professional'
+
+union all
+
+select
+    'medical claim distinct claim count' as data_quality_check
+    , count(distinct claim_id) as result_count
+from {{ ref('medical_claim') }}
+
+union all
+
+select
+    'medical claim distinct patient count' as data_quality_check
+    , count(distinct patient_id) as result_count
+from {{ ref('medical_claim') }}
+
