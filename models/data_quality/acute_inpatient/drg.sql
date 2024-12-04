284bc381e958fa3060bfa4867f4d5772bee8ad58
BijayaShres <bijaya.shrestha@maitriservices.com>
2024-12-04 17:44:43 +0545

    feat: new features added

---
+++ models/data_quality/acute_inpatient/drg.sql
@@ -0,0 +1,10 @@
+{{ config(
+    enabled = var('claims_enabled', False)
+) }}
+
+select distinct 
+    claim_id 
+from {{ ref('header_values') }}
+where 
+    usable_ms_drg_code = 1 
+    or usable_apr_drg_code = 1
