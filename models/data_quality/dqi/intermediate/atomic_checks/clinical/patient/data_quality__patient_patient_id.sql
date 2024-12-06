20799d0fe171f6fbe09a98ce058ad10c111f25c1
sarah-tuva <sarah@tuvahealth.com>
2024-12-05 17:14:16 -0700

    Revert back to patient_id for clinical data quality tests

--- models/data_quality/dqi/intermediate/atomic_checks/clinical/patient/data_quality__patient_patient_id.sql
+++ models/data_quality/dqi/intermediate/atomic_checks/clinical/patient/data_quality__patient_patient_id.sql
@@ -12,10 +12,10 @@ SELECT
     {% endif %}
     , 'PATIENT' AS table_name
     , 'Person ID' as drill_down_key
-    , coalesce(person_id, 'NULL') AS drill_down_value
-    , 'PERSON_ID' as field_name
-    , case when m.person_id is not null then 'valid' else 'null' end as bucket_name
+    , coalesce(patient_id, 'NULL') AS drill_down_value
+    , 'PATIENT_ID' as field_name
+    , case when m.patient_id is not null then 'valid' else 'null' end as bucket_name
     , cast(null as {{ dbt.type_string() }}) as invalid_reason
-    , cast(person_id as {{ dbt.type_string() }}) as field_value
+    , cast(patient_id as {{ dbt.type_string() }}) as field_value
     , '{{ var('tuva_last_run')}}' as tuva_last_run
 from {{ ref('patient')}} m
