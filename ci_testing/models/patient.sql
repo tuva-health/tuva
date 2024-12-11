24039c19f7ff7f07d10643086f1247b9502afd22
sarah-tuva <sarah@tuvahealth.com>
2024-12-10 17:25:58 -0700

    Add phone to ci_testing/patient

--- ci_testing/models/patient.sql
+++ ci_testing/models/patient.sql
@@ -16,6 +16,7 @@ cast(null as {{ dbt.type_string() }} ) as person_id
 , cast(null as {{ dbt.type_string() }} ) as county
 , cast(null as {{ dbt.type_float() }} ) as latitude
 , cast(null as {{ dbt.type_float() }} ) as longitude
+, cast(null as {{ dbt.type_string() }}) as phone
 , cast(null as {{ dbt.type_string() }} ) as data_source
 , cast(null as {{ dbt.type_timestamp() }} ) as tuva_last_run
 {{ limit_zero() }}
