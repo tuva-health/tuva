c3cb456a49a664c3c0c6dbf1c6f6dbf722c578af
sarah-tuva <sarah@tuvahealth.com>
2024-12-11 15:55:49 -0700

    Fix Fabric issues

--- ci_testing/models/provider_attribution.sql
+++ ci_testing/models/provider_attribution.sql
@@ -17,7 +17,7 @@ select {% if target.type == 'fabric' %} top 0 {% else %}{% endif %}
     , cast(null as {{ dbt.type_string() }} ) as patient_id
     , cast(null as {{ dbt.type_string() }} ) as year_month
     , cast(null as {{ dbt.type_string() }} ) as payer
-    , cast(null as {{ dbt.type_string() }} ) as plan
+    , cast(null as {{ dbt.type_string() }} ) as {{ quote_column('plan') }}
     , cast(null as {{ dbt.type_string() }} ) as data_source
     , cast(null as {{ dbt.type_string() }} ) as payer_attributed_provider
     , cast(null as {{ dbt.type_string() }} ) as payer_attributed_provider_practice
--- models/core/final/core__condition.sql
+++ models/core/final/core__condition.sql
@@ -9,14 +9,11 @@ with all_conditions as (
 {% if var('clinical_enabled', var('tuva_marts_enabled', False)) == true
     and var('claims_enabled', var('tuva_marts_enabled', False)) == true -%}
 
-    {{ dbt_utils.union_relations(
-
-            relations=[
-                ref('core__stg_claims_condition'),
-                ref('core__stg_clinical_condition')
-            ]
-
-        ) }}
+    select *
+    from {{ ref('core__stg_claims_condition') }}
+    union all
+    select *
+    from {{ ref('core__stg_clinical_condition') }}
 
 {% elif var('clinical_enabled', var('tuva_marts_enabled',False)) == true -%}
 
