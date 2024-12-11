b2ecc96ed34aa9518536df8be3cea57c1569994f
sarah-tuva <sarah@tuvahealth.com>
2024-12-11 14:00:15 -0700

    Fix provider attribution in ci_testing

--- ci_testing/models/provider_attribution.sql
+++ ci_testing/models/provider_attribution.sql
@@ -4,4 +4,29 @@
    )
 }}
 
+{# logic to use the seed data or an empty table -#}
+
+{% if var('test_data_override') == true -%}
+
 select * from {{ ref('provider_attribution_seed') }}
+
+{%- else -%}
+
+select {% if target.type == 'fabric' %} top 0 {% else %}{% endif %}
+      cast(null as {{ dbt.type_string() }} ) as person_id
+    , cast(null as {{ dbt.type_string() }} ) as patient_id
+    , cast(null as {{ dbt.type_string() }} ) as year_month
+    , cast(null as {{ dbt.type_string() }} ) as payer
+    , cast(null as {{ dbt.type_string() }} ) as plan
+    , cast(null as {{ dbt.type_string() }} ) as data_source
+    , cast(null as {{ dbt.type_string() }} ) as payer_attributed_provider
+    , cast(null as {{ dbt.type_string() }} ) as payer_attributed_provider_practice
+    , cast(null as {{ dbt.type_string() }} ) as payer_attributed_provider_organization
+    , cast(null as {{ dbt.type_string() }} ) as payer_attributed_provider_lob
+    , cast(null as {{ dbt.type_string() }} ) as custom_attributed_provider
+    , cast(null as {{ dbt.type_string() }} ) as custom_attributed_provider_practice
+    , cast(null as {{ dbt.type_string() }} ) as custom_attributed_provider_organization
+    , cast(null as {{ dbt.type_string() }} ) as custom_attributed_provider_lob
+    {% if target.type == 'fabric' %} {% else %} limit 0 {% endif %}
+
+{%- endif %}
