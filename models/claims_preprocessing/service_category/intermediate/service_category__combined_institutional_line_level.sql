c0aac909d971ee62c5d8414c55b34a69785768a0
bradmontierth <72261515+bradmontierth@users.noreply.github.com>
2025-01-09 15:48:18 -0700

    adding dme institutional to union

--- models/claims_preprocessing/service_category/intermediate/service_category__combined_institutional_line_level.sql
+++ models/claims_preprocessing/service_category/intermediate/service_category__combined_institutional_line_level.sql
@@ -10,6 +10,7 @@ WITH combine_line_models AS (
       ref('service_category__outpatient_radiology_institutional'),
       ref('service_category__observation_institutional'),
       ref('service_category__ambulance_institutional'),
+      ref('service_category__dme_institutional'),
       ref('service_category__lab_institutional')
     ],
     exclude=["_loaded_at"]
