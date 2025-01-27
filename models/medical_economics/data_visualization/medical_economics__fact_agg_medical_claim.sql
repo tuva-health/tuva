59a0b6771cba8d1dad25402991cee90dde721d0e
J'aime Tue <jaimetue@Jaimes-MacBook-Air.local>
2025-01-27 09:37:11 -0700

    edit of agg tables for power bi data viz

--- models/medical_economics/data_visualization/medical_economics__fact_agg_medical_claim.sql
+++ models/medical_economics/data_visualization/medical_economics__fact_agg_medical_claim.sql
@@ -1,6 +1,28 @@
+
+with fact_medical_claim as (
+
+  select 
+      aa.claim_id  
+    , aa.encounter_id
+    , aa.payer
+    , aa.claim_start_date
+    , replace(left(aa.claim_start_date,7),'-','') as year_month
+    , aa.service_category_id
+    , aa.condition_grouper_id
+    , aa.specialty_provider_id
+    , bb.comparative_population_id
+    , aa.paid_amount
+    , aa.allowed_amount
+  from {{ ref('medical_economics__fact_medical_claim') }} aa
+  left join {{ ref('medical_economics__dim_comparative_population') }} bb
+      on aa.person_id = bb.person_id  
+
+)
+
 select 
       payer 
-    , replace(left(claim_start_date,7),'-','') as year_month
+    , year_month
+    , comparative_population_id
     , service_category_id
     , condition_grouper_id
     , specialty_provider_id
@@ -8,5 +30,5 @@ select
     , sum(allowed_amount) as allowed_amount
     , count(distinct claim_id) as distinct_claim_id
     , count(distinct encounter_id) as distinct_encounter_id
-from {{ ref('medical_economics__fact_medical_claim') }} 
-group by 1,2,3,4,5
+from fact_medical_claim
+group by 1,2,3,4,5,6
--- models/medical_economics/data_visualization/medical_economics__fact_agg_member_months.sql
+++ models/medical_economics/data_visualization/medical_economics__fact_agg_member_months.sql
@@ -1,7 +1,23 @@
+with member_months as (
+
+  select 
+      aa.person_id
+    , aa.payer
+    , aa.year_month
+    , bb.comparative_population_id
+    , aa.member_month
+    , aa.risk_adjusted_member_months
+  from {{ ref('medical_economics__risk_adjusted_member_months') }} aa 
+  left join {{ ref('medical_economics__dim_comparative_population') }} bb
+    on aa.person_id = bb.person_id 
+
+)
+
 select 
       payer 
     , year_month 
+    , comparative_population_id
     , sum(member_month) as member_months
     , sum(risk_adjusted_member_months) as risk_adjusted_member_months
-from {{ ref('medical_economics__risk_adjusted_member_months') }} 
-group by 1,2
+from member_months
+group by 1,2,3
