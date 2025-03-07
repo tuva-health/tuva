7ba05daaaf37c6f128d25bbf2287e3c757154c30
Chase Jones <60072797+chase-jones@users.noreply.github.com>
2025-03-07 12:05:23 -0500

    Flagging for input layer on patient incorrect

--- models/input_layer/input_layer__patient.sql
+++ models/input_layer/input_layer__patient.sql
@@ -1,6 +1,5 @@
 {{ config(
-     enabled = var('claims_enabled',var('clinical_enabled',var('tuva_marts_enabled',False)))
- | as_bool
+     enabled = var('clinical_enabled',var('tuva_marts_enabled',False)) | as_bool
    )
 }}
 SELECT *
