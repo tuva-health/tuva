56b5dac451b022219029357cd1788cbd07949d23
David Shimamoto <david.shimamoto@gmail.com>
2025-05-06 17:18:05 -0700

    Add data_source to table joins and unique checks in service category models

--- models/claims_preprocessing/service_category/intermediate/service_category__observation_institutional.sql
+++ models/claims_preprocessing/service_category/intermediate/service_category__observation_institutional.sql
@@ -5,14 +5,15 @@
 
 select distinct
    med.claim_id
-, med.claim_line_number
-, 'outpatient' as service_category_1
-, 'observation' as service_category_2
-, 'observation' as service_category_3
-, '{{ this.name }}' as source_model_name
-, '{{ var('tuva_last_run') }}' as tuva_last_run
+   , med.claim_line_number
+   , med.data_source
+   , 'outpatient' as service_category_1
+   , 'observation' as service_category_2
+   , 'observation' as service_category_3
+   , '{{ this.name }}' as source_model_name
+   , '{{ var('tuva_last_run') }}' as tuva_last_run
 from {{ ref('service_category__stg_medical_claim') }} as med
 where claim_type = 'institutional'
-and (revenue_center_code in ('0762')
-or hcpcs_code in ('G0378', 'G0379')
-)
+   and (revenue_center_code in ('0762')
+   or hcpcs_code in ('G0378', 'G0379')
+   )
