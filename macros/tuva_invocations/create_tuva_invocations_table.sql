e5f04100843de3b4401f4e84435a89864abbc5d4
David Shimamoto <david.shimamoto@gmail.com>
2025-05-16 17:16:10 -0700

    Fixed for multi-platform support and custom_tuva_prefix.

--- macros/tuva_invocations/create_tuva_invocations_table.sql
+++ macros/tuva_invocations/create_tuva_invocations_table.sql
@@ -1,13 +1,45 @@
 {% macro create_tuva_invocations_table() %}
-    {% do adapter.create_schema(api.Relation.create(database=target.database, schema="metadata")) %}
-    {% set create_table_query %}
-        create table if not exists metadata.tuva_invocations
+  
+  {%- set schema_name = var('tuva_schema_prefix') ~ '_metadata' if var('tuva_schema_prefix',None) != None else 'metadata' -%}
+  
+  {% do adapter.create_schema(api.Relation.create(database=target.database, schema=schema_name)) %}
+    
+  {%- set table_name = 'tuva_invocations' -%}
+  {%- set target_type = target.type -%}
+  
+  {%- if target_type == 'bigquery' or target_type == 'fabric' -%}
+    {%- set sql -%}
+        CREATE OR REPLACE TABLE {{ schema_name }}.{{ table_name }}
         (
             invocation_id {{ dbt.type_string() }},
             project_name {{ dbt.type_string() }},
             tuva_package_version {{ dbt.type_string() }},
             run_started_at {{ dbt.type_timestamp() }}
         )
-    {% endset %}
-    {% do run_query(create_table_query) %}
+    {%- endset -%}
+  {%- elif target_type == 'snowflake' -%}
+    {%- set sql -%}
+        CREATE OR REPLACE TABLE {{ schema_name }}.{{ table_name }} 
+        (
+            invocation_id {{ dbt.type_string() }},
+            project_name {{ dbt.type_string() }},
+            tuva_package_version {{ dbt.type_string() }},
+            run_started_at {{ dbt.type_timestamp() }}
+        )
+    {%- endset -%}
+  {%- elif target_type == 'redshift' or target_type == 'postgres' or target_type == 'duckdb' -%}
+    {%- set sql -%}
+        CREATE TABLE IF NOT EXISTS {{ schema_name }}.{{ table_name }} 
+        (
+            invocation_id {{ dbt.type_string() }},
+            project_name {{ dbt.type_string() }},
+            tuva_package_version {{ dbt.type_string() }},
+            run_started_at {{ dbt.type_timestamp() }}
+        )
+    {%- endset -%}
+  {%- else -%}
+    {{ exceptions.raise_compiler_error("Unsupported target platform: " ~ target.type) }}
+  {%- endif -%}
+
+  {% do run_query(sql) %}
 {% endmacro %}
--- macros/tuva_invocations/insert_invocation_row.sql
+++ macros/tuva_invocations/insert_invocation_row.sql
@@ -1,11 +1,13 @@
 {% macro log_invocation_start() %}
+    {# Get the schema name using the same logic as in create_tuva_invocations_table #}
+    {%- set schema_name = var('tuva_schema_prefix') ~ '_metadata' if var('tuva_schema_prefix',None) != None else 'metadata' -%}
+    
     {# Ensure that the schema and table exist first #}
     {% do the_tuva_project.create_tuva_invocations_table() %}
-    {% do the_tuva_project.trim_tuva_invocations() %}
-
+    
     {# Insert the record #}
     {% set query %}
-        insert into metadata.tuva_invocations
+        insert into {{ schema_name }}.tuva_invocations
         (
             invocation_id,
             project_name,
@@ -20,5 +22,9 @@
             '{{ run_started_at }}'
         )
     {% endset %}
+    
     {% do run_query(query) %}
+
+    {# do the_tuva_project.trim_tuva_invocations() #}
+
 {% endmacro %}
