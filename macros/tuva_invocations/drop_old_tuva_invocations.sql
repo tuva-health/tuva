01c82ce633c6613cb51fe67f3f6900e6f7440967
David Shimamoto <david.shimamoto@gmail.com>
2025-05-20 10:53:05 -0700

    Fixed dropping old tuva invocations for bigquery

--- macros/tuva_invocations/drop_old_tuva_invocations.sql
+++ macros/tuva_invocations/drop_old_tuva_invocations.sql
@@ -1,8 +1,8 @@
-{% macro trim_tuva_invocations() %}
+{% macro drop_old_tuva_invocations() %}
     {%- set schema_name = var('tuva_schema_prefix') ~ '_metadata' if var('tuva_schema_prefix',None) != None else 'metadata' -%}
     {% set query %}
         delete from {{ schema_name }}.tuva_invocations
-        where run_started_at < ({{ current_timestamp() }} - interval '7 days')
+        where run_started_at < cast({{ dateadd('month', -1, current_timestamp()) }} as {{ dbt.type_timestamp() }})
     {% endset %}
     {% do run_query(query) %}
 {% endmacro %}
