7e3735817d1991e015e54369ba1919b9b8ced202
Chase Jones <60072797+chase-jones@users.noreply.github.com>
2025-05-24 12:23:42 -0400

    Bug 872, backfilling payer and plan with empty string

--- models/input_layer/input_layer__eligibility.sql
+++ models/input_layer/input_layer__eligibility.sql
@@ -3,5 +3,35 @@
  | as_bool
    )
 }}
-select *
-from {{ ref('eligibility') }}
+SELECT e.person_id
+     , e.member_id
+     , e.subscriber_id
+     , e.gender
+     , e.race
+     , e.birth_date
+     , e.death_date
+     , e.death_flag
+     , e.enrollment_start_date
+     , e.enrollment_end_date
+     , COALESCE(e.payer, '') as payer
+     , e.payer_type
+     , COALESCE({{ quote_column('plan') }}, '') as {{ quote_column('plan') }}
+     , e.original_reason_entitlement_code
+     , e.dual_status_code
+     , e.medicare_status_code
+     , e.group_id
+     , e.group_name
+     , e.first_name
+     , e.last_name
+     , e.social_security_number
+     , e.subscriber_relation
+     , e.address
+     , e.city
+     , e.state
+     , e.zip_code
+     , e.phone
+     , e.data_source
+     , e.file_name
+     , e.file_date
+     , e.ingest_datetime
+from {{ ref('eligibility') }} as e
