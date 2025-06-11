9a6787c5e439039550f9f8507c0c27f4e4adae69
bradmontierth <72261515+bradmontierth@users.noreply.github.com>
2025-06-11 14:11:47 -0600

    listing out columns from cte and joining to correct model

--- models/claims_preprocessing/encounters/intermediate/inpatient_long_term/inpatient_long_term__prof_claims.sql
+++ models/claims_preprocessing/encounters/intermediate/inpatient_long_term/inpatient_long_term__prof_claims.sql
@@ -4,28 +4,44 @@
 }}
 
 with first_claim as (
-    select *
+
+    select
+          encounter_id
+        , patient_data_source_id
     from {{ ref('inpatient_long_term__generate_encounter_id') }}
     where encounter_claim_number = 1
+
 )
 
 , join_first_claim_dates as (
-    select f.*
-    , dat.encounter_end_date
-    , dat.encounter_start_date
+
+    select
+          f.encounter_id
+        , f.patient_data_source_id
+        , dat.encounter_end_date
+        , dat.encounter_start_date
     from first_claim as f
-    inner join {{ ref('inpatient_long_term__start_end_dates') }} as dat on f.encounter_id = dat.encounter_id
+    inner join {{ ref('inpatient_long_term__start_end_dates') }} as dat
+         on f.encounter_id = dat.encounter_id
+
 )
 
 -- ensuring each prof claim is only attributed to one institutional claim with claim_attribution_number
-select dat.encounter_id
-, dat.encounter_start_date
-, dat.encounter_end_date
-, prof.claim_id
-, prof.claim_line_number
-, row_number() over (partition by prof.claim_line_id
-order by dat.encounter_id) as claim_attribution_number
+select
+      dat.encounter_id
+    , dat.encounter_start_date
+    , dat.encounter_end_date
+    , med.claim_id
+    , med.claim_line_number
+    , row_number() over (
+        partition by med.claim_id, med.claim_line_number, med.data_source
+        order by dat.encounter_id
+      ) as claim_attribution_number
 from {{ ref('encounters__stg_medical_claim') }} as med
-inner join {{ ref('encounters__stg_professional') }} as prof on med.claim_line_id = prof.claim_line_id
-inner join join_first_claim_dates as dat on med.patient_data_source_id = dat.patient_data_source_id
-and med.start_date between dat.encounter_start_date and dat.encounter_end_date
+inner join {{ ref('encounters__prof_and_lower_priority') }} as plp
+    on med.claim_id = plp.claim_id
+    and med.claim_line_number = plp.claim_line_number
+    and med.data_source = plp.data_source
+inner join join_first_claim_dates as dat
+    on med.patient_data_source_id = dat.patient_data_source_id
+    and med.start_date between dat.encounter_start_date and dat.encounter_end_date
