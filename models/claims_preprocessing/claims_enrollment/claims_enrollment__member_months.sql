f431fb64bcf6535671f1b970e673a1e1e7637897
David Shimamoto <david.shimamoto@gmail.com>
2025-06-13 09:17:04 -0700

    Null out member_id from member_months. Adjust aggregate so it's at the correct grain.

--- models/claims_preprocessing/claims_enrollment/claims_enrollment__member_months.sql
+++ models/claims_preprocessing/claims_enrollment/claims_enrollment__member_months.sql
@@ -8,14 +8,17 @@
 with stg_eligibility as (
   select
     person_id
-    , member_id
     , payer
     , {{ quote_column('plan') }}
     , data_source
-    , enrollment_start_date
-    , enrollment_end_date
-    , tuva_last_run
+    , min(enrollment_start_date) as min_enrollment_start_date
+    , max(enrollment_end_date) as max_enrollment_end_date
   from {{ ref('normalized_input__eligibility') }} as elig
+  group by 
+    person_id
+    , payer
+    , {{ quote_column('plan') }}
+    , data_source
 )
 
 , month_start_and_end_dates as (
@@ -27,24 +30,25 @@ with stg_eligibility as (
   from {{ ref('reference_data__calendar') }}
   group by year, month, year_month
 )
-select distinct
+select 
+  -- Generate a unique key for each member month
   dense_rank() over (
     order by
       a.person_id
-    , a.member_id
     , b.year_month
     , a.payer
     , a.{{ quote_column('plan') }}
     , a.data_source
     ) as member_month_key
   , a.person_id
-  , a.member_id
+  -- As a temporary fix, we are nulling out member_id to get to the grain we want. 
+  -- In a future release we will remove this field.
+  , cast(null as {{ dbt.type_string() }}) as member_id 
   , b.year_month
   , a.payer
   , a.{{ quote_column('plan') }}
   , a.data_source
-  , a.tuva_last_run
 from stg_eligibility as a
 inner join month_start_and_end_dates as b
-  on a.enrollment_start_date <= b.month_end_date
-  and a.enrollment_end_date >= b.month_start_date
+  on a.min_enrollment_start_date <= b.month_end_date
+  and a.max_enrollment_end_date >= b.month_start_date
