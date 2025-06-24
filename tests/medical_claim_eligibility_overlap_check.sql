cafddfaf127765bd295e3621c95dfe077f661865
Krish Maypole <krish@tuvahealth.com>
2025-06-24 15:56:20 -0700

    update checks

--- tests/medical_claim_eligibility_overlap_check.sql
+++ tests/medical_claim_eligibility_overlap_check.sql
@@ -32,7 +32,8 @@ medical_claims as (
 final as (
     select  
         m.data_source
-        , count(*) as n_overlapping_rows
+        , 'overlap' as test
+        , count(*) as n_rows
     from medical_claims as m
     inner join eligibility as e
     on m.person_id = e.person_id
@@ -41,10 +42,25 @@ final as (
     and m.{{ quote_column('plan') }} = e.{{ quote_column('plan') }}
     and m.inferred_claim_start_date between e.enrollment_start_date and e.enrollment_end_date
     group by m.data_source
+    union all
+    select
+        mc.data_source
+        , 'medical claim' as test
+        , count(*) as n_rows
+    from medical_claims as mc
+    group by mc.data_source
+    union all
+    select
+        el.data_source
+        , 'eligibility' as test
+        , count(*) as n_rows
+    from eligibility as el
+    group by el.data_source
 )
 
 select 
     data_source
-    , n_overlapping_rows
+    , test
+    , n_rows
 from final
-where n_overlapping_rows < 1
+where n_rows < 1
