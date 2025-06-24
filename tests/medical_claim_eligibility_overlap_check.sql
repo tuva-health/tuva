17d57d789fbf40efa14fed824c42bb87ef378609
Krish Maypole <krish@tuvahealth.com>
2025-06-24 15:42:37 -0700

    first pass at overlap checks

---
+++ tests/medical_claim_eligibility_overlap_check.sql
@@ -0,0 +1,50 @@
+{{ config(
+     enabled = var('claims_preprocessing_enabled',var('claims_enabled',var('tuva_marts_enabled',False)))
+ | as_bool,
+     tags = ['dqi', 'tuva_dqi_sev_2']
+   )
+}}
+
+with eligibility as (
+    select
+        person_id
+        , member_id
+        , enrollment_start_date
+        , enrollment_end_date
+        , payer
+        , {{ quote_column('plan') }}
+        , data_source
+    from {{ ref('input_layer__eligibility') }}
+),
+
+medical_claims as (
+    select
+        person_id
+        , member_id
+        , payer
+        , {{ quote_column('plan') }}
+        , data_source
+        , coalesce(claim_start_date, admission_date, claim_line_start_date) 
+            as inferred_claim_start_date
+    from {{ ref('input_layer__medical_claim') }}
+),
+
+final as (
+    select  
+        m.data_source
+        , count(*) as n_overlapping_rows
+    from medical_claims as m
+    inner join eligibility as e
+    on m.person_id = e.person_id
+    and m.member_id = e.member_id
+    and m.payer = e.payer
+    and m.{{ quote_column('plan') }} = e.{{ quote_column('plan') }}
+    and m.inferred_claim_start_date between e.enrollment_start_date and e.enrollment_end_date
+    group by m.data_source
+)
+
+select 
+    data_source
+    , n_overlapping_rows
+from final
+where n_overlapping_rows < 1
---
+++ tests/pharmacy_claim_eligibility_overlap_check.sql
@@ -0,0 +1,49 @@
+{{ config(
+     enabled = var('claims_preprocessing_enabled',var('claims_enabled',var('tuva_marts_enabled',False)))
+ | as_bool,
+     tags = ['dqi', 'tuva_dqi_sev_2']
+   )
+}}
+
+with eligibility as (
+    select
+        person_id
+        , member_id
+        , enrollment_start_date
+        , enrollment_end_date
+        , payer
+        , {{ quote_column('plan') }}
+        , data_source
+    from {{ ref('input_layer__eligibility') }}
+),
+
+pharmacy_claims as (
+    select
+        person_id
+        , member_id
+        , payer
+        , {{ quote_column('plan') }}
+        , data_source
+        , dispensing_date
+    from {{ ref('input_layer__pharmacy_claim') }}
+),
+
+final as (
+    select  
+        p.data_source
+        , count(*) as n_overlapping_rows
+    from pharmacy_claims as p
+    inner join eligibility as e
+    on p.person_id = e.person_id
+    and p.member_id = e.member_id
+    and p.payer = e.payer
+    and p.{{ quote_column('plan') }} = e.{{ quote_column('plan') }}
+    and p.dispensing_date between e.enrollment_start_date and e.enrollment_end_date
+    group by p.data_source
+)
+
+select 
+    data_source
+    , n_overlapping_rows
+from final
+where n_overlapping_rows < 1
