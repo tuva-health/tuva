815fb89abd0000cf566e7cfd0b935712556053ff
Krish Maypole <krish@tuvahealth.com>
2025-06-25 13:37:31 -0700

    database compatibility

--- tests/medical_claim_eligibility_overlap_check.sql
+++ tests/medical_claim_eligibility_overlap_check.sql
@@ -53,8 +53,8 @@ with eligibility as (
     select
         oc.data_source
         , oc.n_rows as n_overlapping_records
-        , coalesce(mc.n_rows, 0) < 1 as is_mc_empty
-        , coalesce(ec.n_rows, 0) < 1 as is_elig_empty
+        , coalesce(mc.n_rows, 0) = 0 as is_mc_empty
+        , coalesce(ec.n_rows, 0) = 0 as is_elig_empty
     from overlap_check as oc
     left outer join mc_records_check as mc
     on oc.data_source = mc.data_source
@@ -67,4 +67,4 @@ select
     , n_overlapping_records
 from final
 where not (is_mc_empty or is_elig_empty)
-and n_overlapping_records < 1
+and n_overlapping_records = 0
