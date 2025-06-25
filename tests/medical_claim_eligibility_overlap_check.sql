339ce3ec772628953646f2d46b5a220064417bc7
Krish Maypole <krish@tuvahealth.com>
2025-06-25 10:54:17 -0700

    refactor testing, update severity level

--- tests/medical_claim_eligibility_overlap_check.sql
+++ tests/medical_claim_eligibility_overlap_check.sql
@@ -1,66 +1,70 @@
 {{ config(
      enabled = var('claims_preprocessing_enabled',var('claims_enabled',var('tuva_marts_enabled',False)))
  | as_bool,
-     tags = ['dqi', 'tuva_dqi_sev_2']
+     tags = ['dqi', 'tuva_dqi_sev_2', 'dqi_service_categories', 'dqi_ccsr', 'dqi_cms_chronic_conditions',
+            'dqi_tuva_chronic_conditions', 'dqi_cms_hccs', 'dqi_ed_classification',
+            'dqi_financial_pmpm', 'dqi_quality_measures', 'dqi_readmission'],
+     severity = 'warn'
    )
 }}
 
 with eligibility as (
-    select
+    select distinct
         person_id
-        , member_id
-        , enrollment_start_date
-        , enrollment_end_date
-        , payer
-        , {{ quote_column('plan') }}
         , data_source
     from {{ ref('input_layer__eligibility') }}
 )
 
 , medical_claims as (
-    select
+    select distinct
         person_id
-        , member_id
-        , payer
-        , {{ quote_column('plan') }}
         , data_source
-        , coalesce(claim_start_date, admission_date, claim_line_start_date)
-            as inferred_claim_start_date
     from {{ ref('input_layer__medical_claim') }}
 )
 
-, final as (
+, mc_records_check as (
+    select
+        data_source
+        , count(*) as n_rows
+    from medical_claims
+    group by data_source
+)
+
+, elig_records_check as (
+    select
+        data_source
+        , count(*) as n_rows
+    from eligibility
+    group by data_source
+)
+
+, overlap_check as (
     select
         m.data_source
-        , 'overlap' as test
         , count(*) as n_rows
     from medical_claims as m
     inner join eligibility as e
     on m.person_id = e.person_id
-    and m.member_id = e.member_id
-    and m.payer = e.payer
-    and m.{{ quote_column('plan') }} = e.{{ quote_column('plan') }}
-    and m.inferred_claim_start_date between e.enrollment_start_date and e.enrollment_end_date
+    and m.data_source = e.data_source
     group by m.data_source
-    union all
+),
+
+final as (
     select
-        mc.data_source
-        , 'medical claim' as test
-        , count(*) as n_rows
-    from medical_claims as mc
-    group by mc.data_source
-    union all
-    select
-        el.data_source
-        , 'eligibility' as test
-        , count(*) as n_rows
-    from eligibility as el
-    group by el.data_source
+        oc.data_source
+        , oc.n_rows as n_overlapping_records
+        , coalesce(mc.n_rows, 0) < 1 as is_mc_empty
+        , coalesce(ec.n_rows, 0) < 1 as is_elig_empty
+    from overlap_check as oc
+    left join mc_records_check as mc
+    on oc.data_source = mc.data_source
+    left join elig_records_check as ec
+    on oc.data_source = ec.data_source
 )
 
 select
     data_source
-    , test
-    , n_rows
+    , n_overlapping_records
 from final
-where n_rows < 1
+where not (is_mc_empty or is_elig_empty)
+and n_overlapping_records < 1
