26fad7552ad95fe24ad91c9fba8b3eb20778557f
Chase Jones <60072797+chase-jones@users.noreply.github.com>
2025-07-16 09:07:29 -0400

    970 | Creating de-duplication logic for patients in both claims and clinical
    Made decision to choose claims over clinical for datasets. Patient table has an expectation of PK on the person_id column.

--- models/core/final/core__patient.sql
+++ models/core/final/core__patient.sql
@@ -6,9 +6,18 @@
 
 {% if var('clinical_enabled', var('tuva_marts_enabled',False)) == true and var('claims_enabled', var('tuva_marts_enabled',False)) == true -%}
 
-select * from {{ ref('core__stg_claims_patient') }}
+with person_list_to_exclude_because_in_claims as (
+    select distinct person_id
+    from {{ ref('core__stg_claims_patient') }}
+)
+select *
+from {{ ref('core__stg_claims_patient') }}
 union all
-select * from {{ ref('core__stg_clinical_patient') }}
+select *
+from {{ ref('core__stg_clinical_patient') }} as cscp
+left outer join person_list_to_exclude_because_in_claims as pltebic on cscp.person_id = pltebic.person_id
+/* IF EXISTS IN CLAIMS, CHOOSE CLAIMS RECORD OVER CLINICAL RECORD */
+where pltebic.person_id is null
 
 {% elif var('clinical_enabled', var('tuva_marts_enabled',False)) == true -%}
 
