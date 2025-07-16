4383041868db42b9ac59388fd34327639e1ae6d5
Chase Jones <60072797+chase-jones@users.noreply.github.com>
2025-07-16 10:09:12 -0400

    Specify only wanting columns from clinical patient

--- models/core/final/core__patient.sql
+++ models/core/final/core__patient.sql
@@ -13,7 +13,7 @@ with person_list_to_exclude_because_in_claims as (
 select *
 from {{ ref('core__stg_claims_patient') }}
 union all
-select *
+select cscp.*
 from {{ ref('core__stg_clinical_patient') }} as cscp
 left outer join person_list_to_exclude_because_in_claims as pltebic on cscp.person_id = pltebic.person_id
 /* IF EXISTS IN CLAIMS, CHOOSE CLAIMS RECORD OVER CLINICAL RECORD */
