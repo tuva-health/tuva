da68d4e0239f2db96a816a091934cecb6d73a4f6
Andreas Martinson <andreas@tuvahealth.com>
2025-08-04 09:22:01 -0700

    Added config headers per feedback

--- tests/check_pharmacy_allowed_amount.sql
+++ tests/check_pharmacy_allowed_amount.sql
@@ -1,7 +1,15 @@
 /* This is taken from the claims mapping guide, which says, 
 "The expectation is that the sum of paid_amount, coinsurance_amount, copayment_amount, and deductible_amount will be equivalent to allowed_amount."
 */
-{{ config(severity = 'warn') }}
+{{ config(
+     enabled = var('claims_preprocessing_enabled',var('claims_enabled',var('tuva_marts_enabled',False)))
+ | as_bool,
+     tags = ['dqi', 'tuva_dqi_sev_2', 'dqi_service_categories', 'dqi_ccsr', 'dqi_cms_chronic_conditions',
+            'dqi_tuva_chronic_conditions', 'dqi_cms_hccs', 'dqi_ed_classification',
+            'dqi_financial_pmpm', 'dqi_quality_measures', 'dqi_readmission'],
+     severity = 'warn'
+   )
+}}
 
 
 select 
--- tests/unique_claim_id_claim_type.sql
+++ tests/unique_claim_id_claim_type.sql
@@ -1,3 +1,12 @@
+{{ config(
+     enabled = var('claims_preprocessing_enabled',var('claims_enabled',var('tuva_marts_enabled',False)))
+ | as_bool,
+     tags = ['dqi', 'tuva_dqi_sev_2', 'dqi_service_categories', 'dqi_ccsr', 'dqi_cms_chronic_conditions',
+            'dqi_tuva_chronic_conditions', 'dqi_cms_hccs', 'dqi_ed_classification',
+            'dqi_financial_pmpm', 'dqi_quality_measures', 'dqi_readmission'],
+   )
+}}
+
 -- This test ensures only 1 claim type per claim
 with distinct_combinations as (
   select distinct
