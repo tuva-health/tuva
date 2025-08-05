a8ba29f5f7569097981a5f547e0e557a342c8217
Krish Maypole <krish@tuvahealth.com>
2025-08-05 16:11:37 -0700

    update test and severity level

--- tests/pmpm_paid_amount_threshold_check.sql
+++ tests/pmpm_paid_amount_threshold_check.sql
@@ -1,7 +1,7 @@
 {{ config(
      enabled = var('claims_preprocessing_enabled',var('claims_enabled',var('tuva_marts_enabled',False)))
  | as_bool,
-     tags = ['dqi', 'tuva_dqi_sev_2', 'dqi_service_categories', 'dqi_ccsr', 'dqi_cms_chronic_conditions',
+     tags = ['dqi', 'tuva_dqi_sev_5', 'dqi_service_categories', 'dqi_ccsr', 'dqi_cms_chronic_conditions',
             'dqi_tuva_chronic_conditions', 'dqi_cms_hccs', 'dqi_ed_classification',
             'dqi_financial_pmpm', 'dqi_quality_measures', 'dqi_readmission'],
      severity = 'warn'
@@ -9,17 +9,15 @@
 }}
 
 select
-    person_id
-    , payer
+    payer
     , {{ quote_column('plan') }}
     , data_source
     , count(year_month) as member_months
     , avg(total_paid) as avg_paid_pmpm
 from {{ ref('financial_pmpm__pmpm_prep') }}
 group by
-    person_id
-    , payer
+    payer
     , data_source
     , {{ quote_column('plan') }}
 having avg_paid_pmpm > 10000
-and member_months >= 10
+and member_months >= 100
