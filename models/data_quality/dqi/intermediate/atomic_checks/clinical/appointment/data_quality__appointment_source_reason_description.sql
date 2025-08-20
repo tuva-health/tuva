be36b47c27ce107af09a44752bb1668f7596a1e6
sarah-tuva <sarah@tuvahealth.com>
2025-08-20 12:35:13 -0600

    add appointment to data_quality

---
+++ models/data_quality/dqi/intermediate/atomic_checks/clinical/appointment/data_quality__appointment_source_reason_description.sql
@@ -0,0 +1,16 @@
+{{ config(
+    enabled = var('clinical_enabled', False)
+) }}
+
+select
+      data_source
+    , coalesce(cast(start_datetime as date),cast('1900-01-01' as date)) as source_date
+    , 'APPOINTMENT' as table_name
+    , 'Appointment ID' as drill_down_key
+    , coalesce(appointment_id, 'NULL') as drill_down_value
+    , 'SOURCE_REASON_DESCRIPTION' as field_name
+    , case when source_reason_description is not null then 'valid' else 'null' end as bucket_name
+    , cast(null as {{ dbt.type_string() }}) as invalid_reason
+    , cast(source_reason_description as {{ dbt.type_string() }}) as field_value
+    , '{{ var('tuva_last_run') }}' as tuva_last_run
+from {{ ref('appointment') }}
