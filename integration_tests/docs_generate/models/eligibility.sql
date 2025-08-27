f8f80252b6282b32aedb443447b5a88edff23464
sarah-tuva <sarah@tuvahealth.com>
2025-08-26 20:35:19 -0600

    add missing eligibility fields

--- integration_tests/docs_generate/models/eligibility.sql
+++ integration_tests/docs_generate/models/eligibility.sql
@@ -17,7 +17,9 @@ select
     , medicare_status_code
     , group_id
     , group_name
+    , name_suffix
     , first_name
+    , middle_name
     , last_name
     , social_security_number
     , subscriber_relation
@@ -26,6 +28,8 @@ select
     , state
     , zip_code
     , phone
+    , email
+    , ethnicity
     , data_source
     , file_name
     , file_date
