ce73ba52979bed2ab8edc10b333bc983d164c827
sarah-tuva <sarah@tuvahealth.com>
2025-09-15 16:42:03 -0600

    add optional hedis input to ci_testing

---
+++ ci_testing/models/hedis_measure_report.sql
@@ -0,0 +1,14 @@
+{{ config(
+     enabled = var('hedis_enabled', False) == True | as_bool
+   )
+}}
+
+{% if var('use_synthetic_data') == true -%}
+
+select * from {{ ref('hedis_measure_report_seed') }}
+
+{%- else -%}
+
+select * from {{ source('source_input', 'hedis_measure_report_seed') }}
+
+{%- endif %}
