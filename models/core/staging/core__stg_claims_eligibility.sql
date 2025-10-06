96ca4c2c36be329fa671c6211cfef35d5dd24710
Nabin-Maitri <nabin.hyanmikha@maitriservices.com>
2025-10-06 07:53:15 +0000

    feat: tuva_last_run datatype has been updated to timestamp

--- models/core/staging/core__stg_claims_eligibility.sql
+++ models/core/staging/core__stg_claims_eligibility.sql
@@ -48,5 +48,5 @@ select
        , cast(fips_state_abbreviation as {{ dbt.type_string() }}) as fips_state_abbreviation
        , cast(data_source as {{ dbt.type_string() }}) as data_source
        , {{ try_to_cast_date('file_date', 'YYYY-MM-DD') }} as file_date
-       , '{{ var('tuva_last_run') }}' as tuva_last_run
+       , cast('{{ var('tuva_last_run') }}' as {{ dbt.type_timestamp() }}) as tuva_last_run
 from {{ ref('normalized_input__eligibility') }}
