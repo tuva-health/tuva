0df49a8e27223e41772d93f40f4c1e7b80bc5e08
bradmontierth <bradmontierth@gmail.com>
2025-10-13 09:59:26 -0600

    sql fix

--- models/data_quality/dqi/mart_review/final/mart_review__dashboard_export.sql
+++ models/data_quality/dqi/mart_review/final/mart_review__dashboard_export.sql
@@ -70,9 +70,10 @@ from {{ ref('mart_review__pmpm') }}
 
 union all
 
--- Members with claims % by month
-select data_source, cast(year_month as {{ dbt.type_string() }}) as year_month,
-  cast(null as {{ dbt.type_string() }}) as payer, cast(null as {{ dbt.type_string() }}) as {{ quote_column('plan') }},
+select data_source,
+  cast(null as {{ dbt.type_string() }}) as payer,
+  cast(null as {{ dbt.type_string() }}) as {{ quote_column('plan') }},
+  cast(year_month as {{ dbt.type_string() }}) as year_month,
   'members_with_claims_pct' as metric,
   cast(null as {{ dbt.type_string() }}) as dim1_name, cast(null as {{ dbt.type_string() }}) as dim1_value, cast(null as {{ dbt.type_string() }}) as dim1_label,
   cast(null as {{ dbt.type_string() }}) as dim2_name, cast(null as {{ dbt.type_string() }}) as dim2_value, cast(null as {{ dbt.type_string() }}) as dim2_label,
@@ -82,9 +83,9 @@ from {{ ref('mart_review__members_with_claims') }}
 
 union all
 
--- Claims with enrollment % by month
-select data_source, cast(year_month as {{ dbt.type_string() }}) as year_month,
+select data_source,
   payer, {{ quote_column('plan') }},
+  cast(year_month as {{ dbt.type_string() }}) as year_month,
   'claims_with_enrollment_pct' as metric,
   cast(null as {{ dbt.type_string() }}) as dim1_name, cast(null as {{ dbt.type_string() }}) as dim1_value, cast(null as {{ dbt.type_string() }}) as dim1_label,
   cast(null as {{ dbt.type_string() }}) as dim2_name, cast(null as {{ dbt.type_string() }}) as dim2_value, cast(null as {{ dbt.type_string() }}) as dim2_label,
@@ -94,9 +95,9 @@ from {{ ref('mart_review__claims_with_enrollment') }}
 
 union all
 
--- Chronic condition frequency
-select data_source, cast(null as {{ dbt.type_string() }}) as year_month,
+select data_source,
   payer, {{ quote_column('plan') }},
+  cast(null as {{ dbt.type_string() }}) as year_month,
   'condition_count' as metric,
   'condition' as dim1_name, condition as dim1_value, condition as dim1_label,
   cast(null as {{ dbt.type_string() }}) as dim2_name, cast(null as {{ dbt.type_string() }}) as dim2_value, cast(null as {{ dbt.type_string() }}) as dim2_label,
@@ -106,9 +107,9 @@ from {{ ref('mart_review__condition_counts') }}
 
 union all
 
--- Inpatient LOS
-select data_source, cast(null as {{ dbt.type_string() }}) as year_month,
+select data_source,
   payer, {{ quote_column('plan') }},
+  cast(null as {{ dbt.type_string() }}) as year_month,
   'inpatient_los' as metric,
   'los_groups' as dim1_name, los_groups as dim1_value, los_groups as dim1_label,
   cast(null as {{ dbt.type_string() }}) as dim2_name, cast(null as {{ dbt.type_string() }}) as dim2_value, cast(null as {{ dbt.type_string() }}) as dim2_label,
@@ -118,9 +119,9 @@ from {{ ref('mart_review__inpatient_los_distribution') }}
 
 union all
 
--- DRG counts
-select data_source, cast(null as {{ dbt.type_string() }}) as year_month,
+select data_source,
   payer, {{ quote_column('plan') }},
+  cast(null as {{ dbt.type_string() }}) as year_month,
   'drg_count' as metric,
   'drg_code' as dim1_name, cast(drg_code as {{ dbt.type_string() }}) as dim1_value, drgwithdescription as dim1_label,
   cast(null as {{ dbt.type_string() }}) as dim2_name, cast(null as {{ dbt.type_string() }}) as dim2_value, cast(null as {{ dbt.type_string() }}) as dim2_label,
@@ -130,9 +131,9 @@ from {{ ref('mart_review__drg_counts') }}
 
 union all
 
--- ED avoidable
-select data_source, cast(null as {{ dbt.type_string() }}) as year_month,
+select data_source,
   payer, {{ quote_column('plan') }},
+  cast(null as {{ dbt.type_string() }}) as year_month,
   'ed_avoidable' as metric,
   'avoidable_category' as dim1_name, avoidable_category as dim1_value, avoidable_category as dim1_label,
   cast(null as {{ dbt.type_string() }}) as dim2_name, cast(null as {{ dbt.type_string() }}) as dim2_value, cast(null as {{ dbt.type_string() }}) as dim2_label,
@@ -162,9 +163,9 @@ from {{ ref('mart_review__service_categories_long') }}
 
 union all
 
--- Encounters by group: count
-select data_source, cast(null as {{ dbt.type_string() }}) as year_month,
+select data_source,
   payer, {{ quote_column('plan') }},
+  cast(null as {{ dbt.type_string() }}) as year_month,
   'encounter_count_by_group' as metric,
   'encounter_group' as dim1_name, encounter_group as dim1_value, encounter_group as dim1_label,
   cast(null as {{ dbt.type_string() }}) as dim2_name, cast(null as {{ dbt.type_string() }}) as dim2_value, cast(null as {{ dbt.type_string() }}) as dim2_label,
@@ -174,9 +175,9 @@ from {{ ref('mart_review__encounters_by_group') }}
 
 union all
 
--- Encounters by group: paid
-select data_source, cast(null as {{ dbt.type_string() }}) as year_month,
+select data_source,
   payer, {{ quote_column('plan') }},
+  cast(null as {{ dbt.type_string() }}) as year_month,
   'encounter_paid_by_group' as metric,
   'encounter_group' as dim1_name, encounter_group as dim1_value, encounter_group as dim1_label,
   cast(null as {{ dbt.type_string() }}) as dim2_name, cast(null as {{ dbt.type_string() }}) as dim2_value, cast(null as {{ dbt.type_string() }}) as dim2_label,
@@ -186,9 +187,9 @@ from {{ ref('mart_review__encounters_by_group') }}
 
 union all
 
--- Encounters by type: count
-select data_source, cast(null as {{ dbt.type_string() }}) as year_month,
+select data_source,
   payer, {{ quote_column('plan') }},
+  cast(null as {{ dbt.type_string() }}) as year_month,
   'encounter_count_by_type' as metric,
   'encounter_type' as dim1_name, encounter_type as dim1_value, encounter_type as dim1_label,
   cast(null as {{ dbt.type_string() }}) as dim2_name, cast(null as {{ dbt.type_string() }}) as dim2_value, cast(null as {{ dbt.type_string() }}) as dim2_label,
@@ -198,9 +199,9 @@ from {{ ref('mart_review__encounters_by_type') }}
 
 union all
 
--- Encounters by type: paid
-select data_source, cast(null as {{ dbt.type_string() }}) as year_month,
+select data_source,
   payer, {{ quote_column('plan') }},
+  cast(null as {{ dbt.type_string() }}) as year_month,
   'encounter_paid_by_type' as metric,
   'encounter_type' as dim1_name, encounter_type as dim1_value, encounter_type as dim1_label,
   cast(null as {{ dbt.type_string() }}) as dim2_name, cast(null as {{ dbt.type_string() }}) as dim2_value, cast(null as {{ dbt.type_string() }}) as dim2_label,
@@ -210,21 +211,30 @@ from {{ ref('mart_review__encounters_by_type') }}
 
 union all
 
--- HCC counts
-select data_source, cast(null as {{ dbt.type_string() }}) as year_month,
-  payer, {{ quote_column('plan') }},
+select h.data_source,
+  h.payer, h.{{ quote_column('plan') }},
+  cast(null as {{ dbt.type_string() }}) as year_month,
   'hcc_count' as metric,
-  'hcc_code' as dim1_name, hcc_code as dim1_value, hcc_code as dim1_label,
-  cast(null as {{ dbt.type_string() }}) as dim2_name, cast(null as {{ dbt.type_string() }}) as dim2_value, cast(null as {{ dbt.type_string() }}) as dim2_label,
-  cast(count_members as {{ dbt.type_numeric() }}) as value,
-  tuva_last_run
-from {{ ref('mart_review__hcc_counts') }}
+  'hcc_code' as dim1_name,
+  h.hcc_code as dim1_value,
+  coalesce(d.hcc_description, h.hcc_code) as dim1_label,
+  cast(null as {{ dbt.type_string() }}) as dim2_name,
+  cast(null as {{ dbt.type_string() }}) as dim2_value,
+  cast(null as {{ dbt.type_string() }}) as dim2_label,
+  cast(h.count_members as {{ dbt.type_numeric() }}) as value,
+  h.tuva_last_run as tuva_last_run
+from {{ ref('mart_review__hcc_counts') }} h
+left join (
+  select hcc_code, max(hcc_description) as hcc_description
+  from {{ ref('cms_hcc__int_disease_factors') }}
+  group by hcc_code
+) d on h.hcc_code = d.hcc_code
 
 union all
 
--- Pharmacy summary: spend
-select data_source, cast(null as {{ dbt.type_string() }}) as year_month,
+select data_source,
   payer, {{ quote_column('plan') }},
+  cast(null as {{ dbt.type_string() }}) as year_month,
   'pharm_spend' as metric,
   'theraclass' as dim1_name, theraclass as dim1_value, theraclass as dim1_label,
   'ndc_code' as dim2_name, ndc_code as dim2_value, ndc_description as dim2_label,
@@ -234,9 +244,9 @@ from {{ ref('mart_review__pharmacy_summary') }}
 
 union all
 
--- Pharmacy summary: averages
-select data_source, cast(null as {{ dbt.type_string() }}) as year_month,
+select data_source,
   payer, {{ quote_column('plan') }},
+  cast(null as {{ dbt.type_string() }}) as year_month,
   metric,
   'theraclass' as dim1_name, theraclass as dim1_value, theraclass as dim1_label,
   'ndc_code' as dim2_name, ndc_code as dim2_value, ndc_description as dim2_label,
