dfdba68a6e8189ebfeb06af9c77ee5abb70a61ec
Seamus McKinsey <seamus.mckinsey@color.com>
2025-10-16 13:41:03 -0500

    I'm going to break this out into a separate branch to change the approach
    Revert "Add granular control over on-run-start hook via dbt variables"
    
    This reverts commit 5d639fefcff95bc8272f1f051e1393b5076f5d2b.

--- macros/config/tuva_config.sql
+++ macros/config/tuva_config.sql
@@ -22,21 +22,3 @@
 
   {% do return(var_value) %}
 {% endmacro %}
-
-{% macro is_tuva_selected() %}
-  {# Check if any resources from the_tuva_project or tuva packages are selected #}
-  {% if execute %}
-    {% set nodes = graph.nodes.values() %}
-    {% set selected_nodes = selected_resources %}
-
-    {% for node in nodes %}
-      {% if node.package_name in ['the_tuva_project', 'tuva'] and node.unique_id in selected_nodes %}
-        {% do return(true) %}
-      {% endif %}
-    {% endfor %}
-
-    {% do return(false) %}
-  {% else %}
-    {% do return(true) %}
-  {% endif %}
-{% endmacro %}
