23aea6f5ab6c90bbb16c04fe689440710050d401
Andreas Martinson <andreas@tuvahealth.com>
2025-10-16 11:03:14 -0700

    Adding var for cms_provider_attribution

--- models/input_layer/input_layer__practitioner.sql
+++ models/input_layer/input_layer__practitioner.sql
@@ -1,5 +1,5 @@
 {{ config(
-     enabled = var('clinical_enabled',var('tuva_marts_enabled',False)) | as_bool
+     enabled = var('cms_provider_attribution_enabled',var('clinical_enabled',var('tuva_marts_enabled',False))) | as_bool
    )
 }}
 select *
