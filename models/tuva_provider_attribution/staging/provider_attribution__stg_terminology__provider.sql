260bb2ae74a7f3b3c3e955c4c94fc89bf244da65
brad <bradmontierth@gmail.com>
2025-10-21 08:57:34 -0600

    fixing clinical gating

--- models/tuva_provider_attribution/staging/provider_attribution__stg_terminology__provider.sql
+++ models/tuva_provider_attribution/staging/provider_attribution__stg_terminology__provider.sql
@@ -1,5 +1,5 @@
 {{ config(
-     enabled = var('provider_attribution_enabled', var('claims_enabled', var('tuva_marts_enabled', True))) | as_bool
+     enabled = var('provider_attribution_enabled', var('claims_enabled', var('tuva_marts_enabled', False))) | as_bool
    )
 }}
 
