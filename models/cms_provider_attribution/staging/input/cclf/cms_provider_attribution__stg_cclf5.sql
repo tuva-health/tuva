14e1e4a458eb09dd303ce283e3098e09f6a1edd3
Andreas Martinson <andreas@tuvahealth.com>
2025-10-22 11:11:38 -0700

    Removing CCLF5

--- models/cms_provider_attribution/staging/input/cclf/cms_provider_attribution__stg_cclf5.sql
+++
@@ -1,8 +0,0 @@
-select distinct
-      cur_clm_uniq_id as claim_id
-    , clm_line_num as claim_line_number
-    , bene_mbi_id as person_id
-    , clm_prvdr_spclty_cd 
-    , clm_prcsg_ind_cd
-    , clm_carr_pmt_dnl_cd
-from {{ source('phds_lakehouse_test', 'yak_cclf_5')}}
