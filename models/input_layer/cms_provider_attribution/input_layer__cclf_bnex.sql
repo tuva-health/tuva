5600c068cad50899f61ead08b69c94781cb313b0
Andreas Martinson <andreas@tuvahealth.com>
2025-10-28 15:09:34 -0700

    Removing input layer

--- models/input_layer/cms_provider_attribution/input_layer__cclf_bnex.sql
+++
@@ -1,7 +0,0 @@
-{{ config(
-     enabled = var('cms_provider_attribution_validation_enabled',False)
- | as_bool
-   )
-}}
-
-select mbi from {{ref('cclf_bnex')}}
--- models/input_layer/cms_provider_attribution/input_layer__network_providers.sql
+++
@@ -1,7 +0,0 @@
--- This file includes all providers in the network which cannot be assigned to a different ACO
--- This is an optional file, if not wanted, pass in nulls for all values
-select 
-      tin
-    , ccn
-    , npi
-from {{ref('network_providers')}}
