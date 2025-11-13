486ba7293d218c5415cf6f04a632f62042414ca6
andreas-homeward <andreas.martinson@homewardhealth.com>
2025-11-13 09:18:37 -0800

    Adding model version to lab suspects

--- models/hcc_suspecting/intermediate/hcc_suspecting__int_lab_suspects.sql
+++ models/hcc_suspecting/intermediate/hcc_suspecting__int_lab_suspects.sql
@@ -22,6 +22,7 @@ with egfr_labs as (
     select distinct
           hcc_code
         , hcc_description
+        , 'CMS-HCC-V28' as model_version
     from {{ ref('hcc_suspecting__hcc_descriptions') }}
 
 )
@@ -32,6 +33,7 @@ with egfr_labs as (
           person_id
         , payer
         , data_source
+        , model_version
         , hcc_code
         , current_year_billed
     from {{ ref('hcc_suspecting__int_patient_hcc_history') }}
@@ -128,6 +130,7 @@ with egfr_labs as (
         , code as lab_code
         , result_date
         , result
+        , 'CMS-HCC-V28' as model_version
         , case
             when result between 0 and 14 then '326'
             when result between 15 and 29 then '327'
@@ -175,6 +178,7 @@ with egfr_labs as (
             and unioned.payer = billed_hccs.payer
             and unioned.data_source = billed_hccs.data_source
             and unioned.hcc_code = billed_hccs.hcc_code
+            and unioned.model_version = billed_hccs.model_version
 
 )
 
@@ -187,6 +191,7 @@ with egfr_labs as (
         , result_date
         , result
         , lab_code
+        , model_version
         , hcc_code
         , hcc_description
         , contributing_factor
@@ -206,6 +211,7 @@ with egfr_labs as (
         , cast(result_date as date) as result_date
         , cast(result as {{ dbt.type_numeric() }}) as result
         , cast(lab_code as {{ dbt.type_string() }}) as lab_code
+        , cast(model_version as {{ dbt.type_string() }}) as model_version
         , cast(hcc_code as {{ dbt.type_string() }}) as hcc_code
         , cast(hcc_description as {{ dbt.type_string() }}) as hcc_description
         {% if target.type == 'fabric' %}
@@ -227,6 +233,7 @@ select
     , result_date
     , result
     , lab_code
+    , model_version
     , hcc_code
     , hcc_description
     , current_year_billed
