346bc2e4dde8f8caddbe80bdffca07e49fba3a6a
Chase Jones <60072797+chase-jones@users.noreply.github.com>
2025-12-05 11:17:16 -0500

    Testing

--- models/ccsr/staging/ccsr__stg_core__condition.sql
+++ models/ccsr/staging/ccsr__stg_core__condition.sql
@@ -2,10 +2,9 @@
      enabled = var('ccsr_enabled',var('claims_enabled',var('clinical_enabled',var('tuva_marts_enabled',False)))) | as_bool
    )
 }}
-
 select
-    encounter_id
-    , claim_id
+    encounter_id, 
+    claim_id
     , person_id
     , normalized_code
     , condition_rank
