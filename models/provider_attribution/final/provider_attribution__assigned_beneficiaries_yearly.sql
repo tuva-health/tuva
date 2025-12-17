fae9271d362ab1738c72f66d2b036f19be6e287d
Aaron Neiderhiser <aaronneiderhiser@Aarons-MacBook-Air.local>
2025-12-17 09:38:40 -0700

    removed claims expanded and updated tuva_provider_attribution to provider_attribution

--- models/provider_attribution/final/provider_attribution__assigned_beneficiaries_yearly.sql
+++ models/provider_attribution/final/provider_attribution__assigned_beneficiaries_yearly.sql
@@ -1,5 +1,5 @@
 {{ config(
-     enabled = var('tuva_provider_attribution', var('claims_enabled', var('tuva_marts_enabled', False))) | as_bool
+     enabled = var('provider_attribution', var('claims_enabled', var('tuva_marts_enabled', False))) | as_bool
    )
 }}
 
--- models/tuva_provider_attribution/staging/provider_attribution__stg_core__member_months.sql
+++
@@ -1,9 +0,0 @@
-{{ config(
-     enabled = var('tuva_provider_attribution', var('claims_enabled', var('tuva_marts_enabled', False))) | as_bool
-   )
-}}
-
-select
-    person_id
-  , year_month
-from {{ ref('core__member_months') }}
