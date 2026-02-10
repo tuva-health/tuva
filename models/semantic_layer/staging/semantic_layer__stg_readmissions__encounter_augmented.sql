2a7aa41a23b7e71c7c468a91cc9734885d9ae2dd
Rabee Zyoud <rabee05@hotmail.com>
2026-02-10 20:02:42 +0000

    fix: remove unused cast for tuva_last_run in readmissions staging query

--- models/semantic_layer/staging/semantic_layer__stg_readmissions__encounter_augmented.sql
+++ models/semantic_layer/staging/semantic_layer__stg_readmissions__encounter_augmented.sql
@@ -5,5 +5,4 @@
 
 SELECT
   ea.*
-  , cast('{{ var('tuva_last_run') }}' as {{ dbt.type_timestamp() }}) as tuva_last_run
-FROM {{ ref('readmissions__encounter_augmented') }} as ea 
+FROM {{ ref('readmissions__encounter_augmented') }} as ea
