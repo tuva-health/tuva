00aeb0d9f1b7ae74afcbdc5776ba056a3dbee3d2
Aaron Neiderhiser <aaronneiderhiser@Aarons-MacBook-Air.local>
2026-02-14 00:31:41 -0700

    added agent.md, updated ci to use local seeds, merged provider attribution changes and fixed bugs

--- integration_tests/models/provider_attribution.sql
+++ integration_tests/models/provider_attribution.sql
@@ -6,8 +6,38 @@
    )
 }}
 
-{%- set tuva_columns -%}
+{# Extension columns not supported for provider_attribution #}
+{%- set tuva_extensions -%}
+{%- endset -%}
+
+{%- set tuva_metadata -%}
+    , data_source
+{%- endset -%}
+
+{%- if var('use_synthetic_data') == true -%}
+  {%- set provider_attribution_relation = ref('provider_attribution_seed') -%}
+{%- else -%}
+  {%- set provider_attribution_relation = source('source_input', 'provider_attribution') -%}
+{%- endif -%}
+
+{%- if execute -%}
+  {%- set provider_attribution_columns = adapter.get_columns_in_relation(provider_attribution_relation) -%}
+  {%- set provider_attribution_column_names = provider_attribution_columns | map(attribute='name') | map('lower') | list -%}
+{%- else -%}
+  {%- set provider_attribution_column_names = [] -%}
+{%- endif -%}
+
+{%- set member_id_expr -%}
+  {%- if 'member_id' in provider_attribution_column_names -%}
+    member_id
+  {%- else -%}
+    person_id
+  {%- endif -%}
+{%- endset -%}
+
+select
       person_id
+    , {{ member_id_expr }} as member_id
     , patient_id
     , year_month
     , payer
@@ -20,26 +50,6 @@
     , custom_attributed_provider_practice
     , custom_attributed_provider_organization
     , custom_attributed_provider_lob
-{%- endset -%}
-
-{# Extension columns not supported for provider_attribution #}
-{%- set tuva_extensions -%}
-{%- endset -%}
-
-{%- set tuva_metadata -%}
-    , data_source
-{%- endset -%}
-
-{% if var('use_synthetic_data') == true -%}
-
-select * from {{ ref('provider_attribution_seed') }}
-
-{%- else -%}
-
-select
-    {{ tuva_columns }}
     {{ tuva_extensions }}
     {{ tuva_metadata }}
-from {{ source('source_input', 'provider_attribution') }}
-
-{%- endif %}
+from {{ provider_attribution_relation }}
