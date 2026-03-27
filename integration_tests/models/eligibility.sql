7b590c12c42f634c10f248dfe1727138b8ce9399
Aaron Neiderhiser <aaronneiderhiser@Aarons-MacBook-Air.local>
2026-03-27 13:23:54 -0600

    Revert "[Breaking Change] 1270 add 9 nullable columns to core  eligibility for cms risk adjustment part cd aca esrd (#1271)"
    This reverts commit 2b73f1913615585b8f5f610d6ba93aa5ac9face2.

--- integration_tests/models/eligibility.sql
+++ integration_tests/models/eligibility.sql
@@ -23,16 +23,8 @@
     , medicare_status_code
     , enrollment_status
     , hospice_flag
-    , snp_type
-    , medicaid_indicator
+    , institutional_snp_flag
     , long_term_institutional_flag
-    , part_d_raf_type
-    , low_income_subsidy_indicator
-    , metal_level
-    , csr_indicator
-    , enrollment_duration_months
-    , esrd_status
-    , transplant_duration_months
     , group_id
     , group_name
     , name_suffix
--- models/data_marts/cms_hcc/staging/cms_hcc__stg_core__eligibility.sql
+++ models/data_marts/cms_hcc/staging/cms_hcc__stg_core__eligibility.sql
@@ -12,10 +12,8 @@ select distinct
     , dual_status_code
     , medicare_status_code
     , enrollment_status
-    , medicaid_indicator
     , long_term_institutional_flag
-    , case when snp_type = 'I-SNP' then 1 else 0 end as institutional_snp_flag
-    , case when snp_type is not null then 1 else 0 end as snp_flag
+    , institutional_snp_flag
     , data_source
     , cast('{{ var('tuva_last_run') }}' as {{ dbt.type_timestamp() }}) as tuva_last_run
 from {{ ref('core__eligibility') }}
