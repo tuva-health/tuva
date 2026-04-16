1f8b21c1ff148b2a4963d758ebf91a613a91bc18
Aaron Neiderhiser <anneiderhiser@gmail.com>
2026-04-16 12:19:33 -0600

    Recover missed PR changes after 1312

--- integration_tests/macros/load_seed.sql
+++
@@ -1,9 +0,0 @@
-{% macro duckdb__load_seed(uri, pattern, compression, headers, null_marker) %}
-{#
-    No-op override for DuckDB in environments without S3/httpfs access.
-    The seed table retains whatever data was loaded from the local CSV file.
-#}
-{% if execute %}
-    {{ log("Skipping S3 seed load (no httpfs): s3://" ~ uri ~ "/" ~ pattern, info=True) }}
-{% endif %}
-{% endmacro %}
