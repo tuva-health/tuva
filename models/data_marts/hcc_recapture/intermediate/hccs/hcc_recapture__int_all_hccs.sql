6cd5dbbcf60680506b6f0cfa02e9336bf9706241
Andreas Martinson <amartinson193@gmail.com>
2026-04-21 16:44:10 -0700

    move chronic hccs

--- models/data_marts/hcc_recapture/intermediate/hccs/hcc_recapture__int_all_hccs.sql
+++ models/data_marts/hcc_recapture/intermediate/hccs/hcc_recapture__int_all_hccs.sql
@@ -11,37 +11,6 @@ with seed_hcc_hierarchy as (
     from {{ ref('cms_hcc__disease_hierarchy_flat') }}
 )
 
-, hcc_diagnosis as (
-    select
-          payment_year
-        , diagnosis_code
-        , cms_hcc_v28 as hcc_code
-        , 'CMS-HCC-V28' as model_version
-    from {{ ref('cms_hcc__icd_10_cm_mappings') }}
-    where cms_hcc_v28_flag = 'Yes'
-
-    union all
-
-    select
-          payment_year
-        , diagnosis_code
-        , cms_hcc_v24 as hcc_code
-        , 'CMS-HCC-V24' as model_version
-    from {{ ref('cms_hcc__icd_10_cm_mappings') }}
-    where cms_hcc_v24_flag = 'Yes'
-)
-
-, chronic_hccs as (
-select distinct
-      diag.hcc_code
-    , diag.model_version
-    , 1 as chronic_flag
-from {{ ref('chronic_conditions__cms_chronic_conditions_hierarchy') }} as hier
-inner join hcc_diagnosis as diag
-  on hier.code = diag.diagnosis_code
-where diag.payment_year = (select max(payment_year) from hcc_diagnosis)
-)
-
 , get_risk_code as (
     select distinct
         person_id
