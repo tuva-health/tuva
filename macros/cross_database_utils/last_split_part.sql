3fe7ad8095cbc64b3db10b5a27be7a193eae8114
Rabee Zyoud <rabee05@hotmail.com>
2026-06-06 00:32:11 +0000

    feat(macros): add last_split_part cross-database macro
    Return the last element of a delimited string. split_part(..., -1) works on
    most adapters but Athena/Trino rejects negative indexes, so provide athena__
    and fabric__ implementations alongside the default.

---
+++ macros/cross_database_utils/last_split_part.sql
@@ -0,0 +1,25 @@
+{#
+    Returns the last element of string_text split on delimiter_text.
+    Equivalent to split_part(..., -1) but works on Athena, whose adapter has
+    no split_part override and so passes the negative index through to
+    Trino verbatim (which rejects it: "Index must be greater than zero").
+#}
+{% macro last_split_part(string_text, delimiter_text) -%}
+    {{ return(adapter.dispatch('last_split_part')(string_text, delimiter_text)) }}
+{%- endmacro %}
+
+{% macro default__last_split_part(string_text, delimiter_text) -%}
+    {{ dbt.split_part(string_text, delimiter_text, -1) }}
+{%- endmacro %}
+
+{% macro athena__last_split_part(string_text, delimiter_text) -%}
+    split_part(
+        {{ string_text }},
+        {{ delimiter_text }},
+        length({{ string_text }}) - length(replace({{ string_text }}, {{ delimiter_text }}, '')) + 1
+    )
+{%- endmacro %}
+
+{% macro fabric__last_split_part(string_text, delimiter_text) -%}
+    reverse(left(reverse({{ string_text }}), charindex({{ delimiter_text }}, reverse({{ string_text }})) - 1))
+{%- endmacro %}
